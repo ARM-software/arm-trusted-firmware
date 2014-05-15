dbff210ec5b0c458512ac0f3341f6ee87c8e6b92
Sandrine Bailleux <sandrine.bailleux@arm.com>
2014-05-15 15:08:02 +0100

    Revert "juno: Initialise architected timer in BL3-1"
    This reverts commit ff7480a34839693fdba599f74e5c7e6e189eebb8.
    because the architected timer is now initialised in architecture
    code and no longer in platform code. See commit 9e86490f8f1.
    
    Change-Id: I0f12e006ba1574c7c0fc03c616503f33958b9596

--- plat/juno/bl31_plat_setup.c
+++ plat/juno/bl31_plat_setup.c
@@ -30,7 +30,6 @@
 
 #include <mhu.h>
 #include <platform.h>
-#include <assert.h>
 #include <arch_helpers.h>
 #include <console.h>
 
@@ -122,21 +121,10 @@ void bl31_early_platform_setup(bl31_args *from_bl2,
  ******************************************************************************/
 void bl31_platform_setup(void)
 {
-	unsigned int counter_base_frequency;
-
 	mhu_secure_init();
 
 	/* Initialize the gic cpu and distributor interfaces */
 	gic_setup();
-
-	/* Read the frequency from Frequency modes table */
-	counter_base_frequency = mmio_read_32(SYS_CNTCTL_BASE + CNTFID_OFF);
-
-	/* The first entry of the frequency modes table must not be 0 */
-	assert(counter_base_frequency != 0);
-
-	/* Program the counter frequency */
-	write_cntfrq_el0(counter_base_frequency);
 
 	/* Topologies are best known to the platform. */
 	plat_setup_topology();
