6e095e51ef1bd64d25262beda096cc1c8b10497a
Dan Handley <dan.handley@arm.com>
2014-05-15 14:11:36 +0100

    Move BL porting functions into platform.h
    Some platform porting functions were in BL specific header files.
    These have been moved to platform.h so that all porting functions
    are in the same place. The functions are now grouped by BL.
    Obsolete BL headers files have been removed.
    
    Also, the weak declarations of the init_blX_mem_layout() functions
    have been moved out the header file and into the source file
    (bl_common.c) using the more succinct #pragma syntax. This
    mitigates the risk of 2 weak definitions being created and the
    wrong one being picked up by the compiler.
    
    Change-Id: Ib19934939fd755f3e5a5a5bceec88da684308a83

--- common/bl_common.c
+++ common/bl_common.c
@@ -74,9 +74,11 @@ void change_security_state(unsigned int target_security_state)
 
 
 /*******************************************************************************
- * The next function is a weak definition. Platform specific
- * code can override it if it wishes to.
+ * The next two functions are weak definitions. Platform specific
+ * code can override them if it wishes to.
  ******************************************************************************/
+#pragma weak init_bl31_mem_layout
+#pragma weak init_bl2_mem_layout
 
 /*******************************************************************************
  * Function that takes a memory layout into which BL2 has been either top or
--- plat/fvp/bl2_plat_setup.c
+++ plat/fvp/bl2_plat_setup.c
@@ -31,7 +31,6 @@
 #include <arch_helpers.h>
 #include <assert.h>
 #include <bl_common.h>
-#include <bl2.h>
 #include <console.h>
 #include <platform.h>
 #include <platform_def.h>
