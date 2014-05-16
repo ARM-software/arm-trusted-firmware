075d385b718860f258fc6ccbc4eb597985d3c69c
Juan Castillo <juan.castillo@arm.com>
2014-05-16 15:33:15 +0100

    Reserve some DDR DRAM for secure use on FVP platforms
    TZC-400 is configured to set the last 16MB of DRAM1 as secure memory and
    the rest of DRAM as non-secure. Non-secure software must not attempt to
    access the 16MB secure area.
    
    Device tree files (sources and binaries) have been updated to match this
    configuration, removing that memory from the Linux physical memory map.
    UEFI earliest commit matching this change updated in documentation.
    
    Replaced magic numbers with #define for memory region definition in the
    platform security initialization function.
    
    Fixes ARM-software/tf-issues#149
    
    Change-Id: Ia5d070244aae6c5288ea0e6c8e89d92859522bfe

--- plat/fvp/bl2_plat_setup.c
+++ plat/fvp/bl2_plat_setup.c
@@ -269,9 +269,9 @@ extern void bl2_plat_get_bl32_meminfo(meminfo_t *bl32_meminfo)
 extern void bl2_plat_get_bl33_meminfo(meminfo_t *bl33_meminfo)
 {
 	bl33_meminfo->total_base = DRAM_BASE;
-	bl33_meminfo->total_size = DRAM_SIZE;
+	bl33_meminfo->total_size = DRAM_SIZE - DRAM1_SEC_SIZE;
 	bl33_meminfo->free_base = DRAM_BASE;
-	bl33_meminfo->free_size = DRAM_SIZE;
+	bl33_meminfo->free_size = DRAM_SIZE - DRAM1_SEC_SIZE;
 	bl33_meminfo->attr = 0;
 	bl33_meminfo->attr = 0;
 }
