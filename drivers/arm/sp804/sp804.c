71d0069d21492d7ba9bc48fbe3c0cd95796fd3e4
Ryan Harkin <ryan.harkin@linaro.org>
2015-03-17 14:52:39 +0000

    sp804: add driver
    Add a driver for the ARM SP804 dual timer.
    
    This driver will only use the first timer, called timer 1 in the
    documentation.
    
    To use this driver, the BSP must provide three constants:
    
    	SP804_BASE	base address of the timer
    	SP804_CLKDIV	clock divider
    	SP804_CLKMULT	clock multiplier
    
    The BSP is responsible for calling sp804_init(). The SP804 driver
    creates a timer_ops_t and calls the generic timer_init().
    
    Signed-off-by: Ryan Harkin <ryan.harkin@linaro.org>

---
+++ drivers/arm/sp804/sp804.c
@@ -0,0 +1,79 @@
+/*
+ * Copyright (c) 2015, ARM Limited and Contributors. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * Neither the name of ARM nor the names of its contributors may be used
+ * to endorse or promote products derived from this software without specific
+ * prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <debug.h>
+#include <mmio.h>
+#include <platform_def.h>
+#include <timer.h>
+
+#define SP804_TIMER1_LOAD	(SP804_BASE + 0x000)
+#define SP804_TIMER1_VALUE	(SP804_BASE + 0x004)
+#define SP804_TIMER1_CONTROL	(SP804_BASE + 0x008)
+#define SP804_TIMER1_BGLOAD	(SP804_BASE + 0x018)
+
+#define TIMER_CTRL_ONESHOT	(1 << 0)
+#define TIMER_CTRL_32BIT	(1 << 1)
+#define TIMER_CTRL_DIV1		(0 << 2)
+#define TIMER_CTRL_DIV16	(1 << 2)
+#define TIMER_CTRL_DIV256	(2 << 2)
+#define TIMER_CTRL_IE		(1 << 5)
+#define TIMER_CTRL_PERIODIC	(1 << 6)
+#define TIMER_CTRL_ENABLE	(1 << 7)
+
+/***********************************************************
+ * The delay implementation for all BL stages
+ ***********************************************************/
+
+uint32_t sp804_get_timer_value(void)
+{
+	return mmio_read_32(SP804_TIMER1_VALUE);
+}
+
+/***********************************************************
+ * delay for the given number of milliseconds
+ ***********************************************************/
+void sp804_init()
+{
+	timer_ops_t ops;
+
+	ops.get_timer_value = sp804_get_timer_value;
+	ops.clkdiv = SP804_CLKDIV;
+	ops.clkmult = SP804_CLKMULT;
+
+	/* disable timer1 */
+	mmio_write_32(SP804_TIMER1_CONTROL, 0);
+	mmio_write_32(SP804_TIMER1_LOAD, 0xffffffff);
+	mmio_write_32(SP804_TIMER1_VALUE, 0xffffffff);
+
+	/* enable as a free running 32-bit counter */
+	mmio_write_32(SP804_TIMER1_CONTROL, TIMER_CTRL_32BIT | TIMER_CTRL_ENABLE);
+
+	timer_init(&ops);
+}
