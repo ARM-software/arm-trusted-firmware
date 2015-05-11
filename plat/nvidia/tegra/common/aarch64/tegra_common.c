412f7223267fcbc9cf89250cd9e6a4ad56fe9f01
Varun Wadekar <vwadekar@nvidia.com>
2015-05-11 10:50:32 +0530

    Support for NVIDIA's Tegra T210 SoCs
    T210 is the latest chip in the Tegra family of SoCs from NVIDIA. It is an
    ARM v8 dual-cluster (A57/A53) SoC, with any one of the clusters being active
    at a given point in time.
    
    This patch adds support to boot the Trusted Firmware on T210 SoCs. The patch
    also adds support to boot secondary CPUs, enter/exit core power states for
    all CPUs in the slow/fast clusters. The support to switch between clusters
    is still not available in this patch and would be available later.
    
    Signed-off-by: Varun Wadekar <vwadekar@nvidia.com>

---
+++ plat/nvidia/tegra/common/aarch64/tegra_common.c
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2015, ARM Limited and Contributors. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * Neither the name of ARM nor the names of its contributors may be used
+ * to endorse or promote products derived from this software without specific
+ * prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <arch.h>
+#include <platform.h>
+#include <tegra_config.h>
+#include <tegra_def.h>
+
+/*******************************************************************************
+ * This variable holds the non-secure image entry address
+ ******************************************************************************/
+extern uint64_t ns_image_entrypoint;
+
+/*******************************************************************************
+ * arm_config holds the characteristics of the Tegra chips. It will be
+ * populated during cold boot by the primary before enabling the MMU & used
+ * thereafter.
+ ******************************************************************************/
+tegra_config_t tegra_config;
+
+int tegra_config_setup(void)
+{
+	tegra_config.gicd_base = TEGRA_GICD_BASE;
+	tegra_config.gicc_base = TEGRA_GICC_BASE;
+
+	return 0;
+}
+
+unsigned long plat_get_ns_image_entrypoint(void)
+{
+	return ns_image_entrypoint;
+}
