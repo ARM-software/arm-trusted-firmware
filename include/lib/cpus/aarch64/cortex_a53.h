6323a61f897a8862f0be44cfa7fe4461820c0952
Varun Wadekar <vwadekar@nvidia.com>
2015-08-21 15:52:51 +0530

    CPUECTLR_EL1 and L2ECTLR_EL1 retention control masks
    This patch adds the timer tick macros to be used by the AdvSIMD and FPU
    retention control and the CPU retention control bits.
    
    Signed-off-by: Varun Wadekar <vwadekar@nvidia.com>

--- include/lib/cpus/aarch64/cortex_a53.h
+++ include/lib/cpus/aarch64/cortex_a53.h
@@ -34,12 +34,27 @@
 /* Cortex-A53 midr for revision 0 */
 #define CORTEX_A53_MIDR 0x410FD030
 
+/* Retention timer tick definitions */
+#define RETENTION_ENTRY_TICKS_2		0x1
+#define RETENTION_ENTRY_TICKS_8		0x2
+#define RETENTION_ENTRY_TICKS_32	0x3
+#define RETENTION_ENTRY_TICKS_64	0x4
+#define RETENTION_ENTRY_TICKS_128	0x5
+#define RETENTION_ENTRY_TICKS_256	0x6
+#define RETENTION_ENTRY_TICKS_512	0x7
+
 /*******************************************************************************
  * CPU Extended Control register specific definitions.
  ******************************************************************************/
 #define CPUECTLR_EL1			S3_1_C15_C2_1	/* Instruction def. */
 
 #define CPUECTLR_SMP_BIT		(1 << 6)
+
+#define CPUECTLR_CPU_RET_CTRL_SHIFT	0
+#define CPUECTLR_CPU_RET_CTRL_MASK	(0x7 << CPUECTLR_CPU_RET_CTRL_SHIFT)
+
+#define CPUECTLR_FPU_RET_CTRL_SHIFT	3
+#define CPUECTLR_FPU_RET_CTRL_MASK	(0x7 << CPUECTLR_FPU_RET_CTRL_SHIFT)
 
 /*******************************************************************************
  * CPU Auxiliary Control register specific definitions.
@@ -55,5 +70,13 @@
 
 #define L2ACTLR_ENABLE_UNIQUECLEAN	(1 << 14)
 #define L2ACTLR_DISABLE_CLEAN_PUSH	(1 << 3)
+
+/*******************************************************************************
+ * L2 Extended Control register specific definitions.
+ ******************************************************************************/
+#define L2ECTLR_EL1			S3_1_C11_C0_3	/* Instruction def. */
+
+#define L2ECTLR_RET_CTRL_SHIFT		0
+#define L2ECTLR_RET_CTRL_MASK		(0x7 << L2ECTLR_RET_CTRL_SHIFT)
 
 #endif /* __CORTEX_A53_H__ */
