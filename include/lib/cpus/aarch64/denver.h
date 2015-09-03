e956e228d464afdb36486d72d47b16903d7ffdc8
Varun Wadekar <vwadekar@nvidia.com>
2015-09-03 17:15:06 +0530

    cpus: Add support for all Denver variants
    This patch adds support for all variants of the Denver CPUs. The
    variants export their cpu_ops to allow all Denver platforms to run
    the Trusted Firmware stack.
    
    Change-Id: I1488813ddfd506ffe363d8a32cda1b575e437035
    Signed-off-by: Varun Wadekar <vwadekar@nvidia.com>

--- include/lib/cpus/aarch64/denver.h
+++ include/lib/cpus/aarch64/denver.h
@@ -31,8 +31,15 @@
 #ifndef __DENVER_H__
 #define __DENVER_H__
 
-/* MIDR for Denver v1.0 */
-#define DENVER_1_0_MIDR			0x4E0F0000
+/* MIDR values for Denver */
+#define DENVER_MIDR_PN0			0x4E0F0000
+#define DENVER_MIDR_PN1			0x4E0F0010
+#define DENVER_MIDR_PN2			0x4E0F0020
+#define DENVER_MIDR_PN3			0x4E0F0030
+#define DENVER_MIDR_PN4			0x4E0F0040
+
+/* Implementer code in the MIDR register */
+#define DENVER_IMPL			0x4E
 
 /* CPU state ids - implementation defined */
 #define DENVER_CPU_STATE_POWER_DOWN	0x3
