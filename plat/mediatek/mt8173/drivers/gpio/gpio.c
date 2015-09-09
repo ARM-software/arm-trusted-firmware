ea39b067671848b47bbd18081f4d93db4330bb04
Jimmy Huang <jimmy.huang@mediatek.com>
2015-09-09 18:34:38 +0800

    mt8173: Add gpio interface to read gpio status
    This patch added gpio_get() function to read gpio status. We also
    further revised/simplified the gpio driver in this patch.
    
    TEST=boot and confirm system shutdown still works on oak-rev3
    
    Change-Id: I855f2a07ee89803b07a67afa5891f0d1df9b0806
    Signed-off-by: Jimmy Huang <jimmy.huang@mediatek.com>

--- plat/mediatek/mt8173/drivers/gpio/gpio.c
+++ plat/mediatek/mt8173/drivers/gpio/gpio.c
@@ -27,172 +27,52 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  */
+#include <assert.h>
 #include <debug.h>
 #include <gpio.h>
 #include <mmio.h>
-#include <mt8173_def.h>
-#include <pmic_wrap_init.h>
 
 enum {
-	MAX_GPIO_REG_BITS = 16,
+	MAX_8173_GPIO = 134,
+	MAX_GPIO_REG_BITS = 16
 };
 
-struct mt_gpio_obj {
-	struct gpio_regs *reg;
-};
-
-static struct mt_gpio_obj gpio_dat = {
-	.reg = (struct gpio_regs *)(GPIO_BASE),
-};
-
-static struct mt_gpio_obj *gpio_obj = &gpio_dat;
-
-struct mt_gpioext_obj {
-	struct gpioext_regs *reg;
-};
-
-static struct mt_gpioext_obj gpioext_dat = {
-	.reg = (struct gpioext_regs *)(GPIOEXT_BASE),
-};
-
-static struct mt_gpioext_obj *gpioext_obj = &gpioext_dat;
-
-static inline struct mt_gpio_obj *mt_get_gpio_obj(void)
+static void pos_bit_calc(uint32_t pin, uint32_t *pos, uint32_t *bit)
 {
-	return gpio_obj;
+	*pos = pin / MAX_GPIO_REG_BITS;
+	*bit = pin % MAX_GPIO_REG_BITS;
 }
 
-static inline struct mt_gpioext_obj *mt_get_gpioext_obj(void)
+int gpio_get(uint32_t pin)
 {
-	return gpioext_obj;
-}
-
-enum {
-	GPIO_PRO_DIR = 0,
-	GPIO_PRO_DOUT,
-	GPIO_PRO_DIN,
-	GPIO_PRO_PULLEN,
-	GPIO_PRO_PULLSEL,
-	GPIO_PRO_MODE,
-	GPIO_PRO_MAX,
-};
-
-static inline int32_t gpioext_write(uint16_t *addr, int64_t data)
-{
-	return pwrap_write((uint32_t)(uintptr_t)addr, data);
-}
-
-static inline int32_t gpioext_set_bits(uint32_t bit, uint16_t *reg)
-{
-	return gpioext_write(reg, bit);
-}
-
-static int32_t mt_set_gpio_chip(uint32_t pin, uint32_t property, uint32_t val)
-{
-	uint32_t pos = 0;
-	uint32_t bit = 0;
-	struct mt_gpio_obj *obj = mt_get_gpio_obj();
+	uint32_t pos;
+	uint32_t bit;
 	uint16_t *reg;
-	uint32_t data = 0;
+	int data;
 
-	if (!obj)
-		return -ERACCESS;
+	assert(pin <= MAX_8173_GPIO);
 
-	if (pin >= GPIO_EXTEND_START)
-		return -ERINVAL;
+	pos_bit_calc(pin, &pos, &bit);
 
-	if (property >= GPIO_PRO_MAX)
-		return -ERINVAL;
+	reg = &mt8173_gpio->din[pos].val;
+	data = mmio_read_32((uintptr_t)reg);
 
-	pos = pin / MAX_GPIO_REG_BITS;
-	bit = pin % MAX_GPIO_REG_BITS;
-	data = 1L << bit;
-
-	switch (property) {
-	case GPIO_PRO_DIR:
-		if (val == GPIO_DIR_IN)
-			reg = &obj->reg->dir[pos].rst;
-		else
-			reg = &obj->reg->dir[pos].set;
-		break;
-	case GPIO_PRO_DOUT:
-		if (val == GPIO_OUT_ZERO)
-			reg = &obj->reg->dout[pos].rst;
-		else
-			reg = &obj->reg->dout[pos].set;
-		break;
-	default:
-		return -ERINVAL;
-	}
-
-	mmio_write_16((uintptr_t)reg, data);
-
-	return RSUCCESS;
+	return (data & (1L << bit)) ? 1 : 0;
 }
 
-static int32_t mt_set_gpio_ext(uint32_t pin, uint32_t property, uint32_t val)
+void gpio_set(uint32_t pin, int output)
 {
-	uint32_t pos = 0;
-	uint32_t bit = 0;
-	struct mt_gpioext_obj *obj = mt_get_gpioext_obj();
+	uint32_t pos;
+	uint32_t bit;
 	uint16_t *reg;
-	uint32_t data = 0;
-	int ret = 0;
 
-	if (!obj)
-		return -ERACCESS;
+	assert(pin <= MAX_8173_GPIO);
 
-	if (pin >= MAX_GPIO_PIN)
-		return -ERINVAL;
+	pos_bit_calc(pin, &pos, &bit);
 
-	if (property >= GPIO_PRO_MAX)
-		return -ERINVAL;
-
-	pin -= GPIO_EXTEND_START;
-	pos = pin / MAX_GPIO_REG_BITS;
-	bit = pin % MAX_GPIO_REG_BITS;
-
-	switch (property) {
-	case GPIO_PRO_DIR:
-		if (val == GPIO_DIR_IN)
-			reg = &obj->reg->dir[pos].rst;
-		else
-			reg = &obj->reg->dir[pos].set;
-		break;
-	case GPIO_PRO_DOUT:
-		if (val == GPIO_OUT_ZERO)
-			reg = &obj->reg->dout[pos].rst;
-		else
-			reg = &obj->reg->dout[pos].set;
-		break;
-	default:
-		return -ERINVAL;
-	}
-	data = (1L << bit);
-	ret = gpioext_set_bits(data, reg);
-
-	return ret ? -ERWRAPPER : RSUCCESS;
-}
-
-static void mt_gpio_pin_decrypt(uint32_t *cipher)
-{
-	if ((*cipher & (0x80000000)) == 0)
-		INFO("Pin %u decrypt warning!\n", *cipher);
-	*cipher &= ~(0x80000000);
-}
-
-int32_t mt_set_gpio_out(uint32_t pin, uint32_t output)
-{
-	uint32_t gp = GPIO_PRO_DOUT;
-
-	mt_gpio_pin_decrypt(&pin);
-
-	return (pin >= GPIO_EXTEND_START) ?
-		mt_set_gpio_ext(pin, gp, output) :
-		mt_set_gpio_chip(pin, gp, output);
-}
-
-void gpio_set(uint32_t gpio, int32_t value)
-{
-	mt_set_gpio_out(gpio, value);
+	if (output == 0)
+		reg = &mt8173_gpio->dout[pos].rst;
+	else
+		reg = &mt8173_gpio->dout[pos].set;
+	mmio_write_16((uintptr_t)reg, 1L << bit);
 }
