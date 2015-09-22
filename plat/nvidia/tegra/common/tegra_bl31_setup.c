7b7082ec002df08e2f69b11e339bba69c4feb273
Varun Wadekar <vwadekar@nvidia.com>
2015-09-22 15:02:41 +0530

    Tegra: Initialize GICD and GICC settings earlier
    This patch initializes the GICD and GICC settings earlier in the
    BL3-1 setup process; before setting up any other hardware block.
    
    Change-Id: Iad619f497f26d3adfcc5f034519416174abc0d73
    Signed-off-by: Varun Wadekar <vwadekar@nvidia.com>

--- plat/nvidia/tegra/common/tegra_bl31_setup.c
+++ plat/nvidia/tegra/common/tegra_bl31_setup.c
@@ -158,6 +158,9 @@ void bl31_platform_setup(void)
 {
 	uint32_t tmp_reg;
 
+	/* Initialize the gic cpu and distributor interfaces */
+	tegra_gic_setup();
+
 	/*
 	 * Initialize delay timer
 	 */
@@ -182,9 +185,6 @@ void bl31_platform_setup(void)
 	/* Set the next EL to be AArch64 */
 	tmp_reg = SCR_RES1_BITS | SCR_RW_BIT;
 	write_scr(tmp_reg);
-
-	/* Initialize the gic cpu and distributor interfaces */
-	tegra_gic_setup();
 }
 
 /*******************************************************************************
