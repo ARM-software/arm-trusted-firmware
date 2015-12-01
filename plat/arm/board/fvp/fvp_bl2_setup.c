540a5ba8d955b6f369322b099f70bb5dc6787a06
Juan Castillo <juan.castillo@arm.com>
2015-12-01 16:10:15 +0000

    Fix SP804 delay timer on FVP
    This patch fixes several issues with the SP804 delay timer on FVP:
    
    * By default, the SP804 dual timer on FVP runs at 32 KHz. In order
      to run the timer at 35 MHz (as specified in the FVP user manual)
      the Overwrite bit in the SP810 control register must be set.
    
    * The CLKMULT and CLKDIV definitions are mixed up:
    
          delta(us) = delta(ticks) * T(us) = delta(ticks) / f(MHz)
    
      From the delay function:
    
          delta_us = (delta * ops->clk_mult) / ops->clk_div;
    
      Matching both expressions:
    
          1 / f(MHz) = ops->clk_mult / ops->clk_div
    
      And consequently:
    
          f(MHz) = ops->clk_div / ops->clk_mult
    
      Which, for a 35 MHz timer, translates to:
    
          ops->clk_div = 35
          ops->clk_mult = 1
    
    * The comment in the delay timer header file has been corrected:
      The ratio of the multiplier and the divider is the clock period
      in microseconds, not the frequency.
    
    Change-Id: Iffd5ce0a5a28fa47c0720c0336d81b678ff8fdf1

--- plat/arm/board/fvp/fvp_bl2_setup.c
+++ plat/arm/board/fvp/fvp_bl2_setup.c
@@ -28,6 +28,7 @@
  * POSSIBILITY OF SUCH DAMAGE.
  */
 
+#include <mmio.h>
 #include <plat_arm.h>
 #include <sp804_delay_timer.h>
 #include <v2m_def.h>
@@ -46,6 +47,10 @@ void bl2_early_platform_setup(meminfo_t *mem_layout)
 void bl2_platform_setup(void)
 {
 	arm_bl2_platform_setup();
+
+	/* Enable the clock override for SP804 timer 0, which means that no
+	 * clock dividers are applied and the raw (35 MHz) clock will be used */
+	mmio_write_32(V2M_SP810_BASE, FVP_SP810_CTRL_TIM0_OV);
 
 	/* Initialize delay timer driver using SP804 dual timer 0 */
 	sp804_timer_init(V2M_SP804_TIMER0_BASE,
