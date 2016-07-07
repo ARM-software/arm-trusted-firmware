a4beaaffedb53f6d8bcc199aedaddc9f5c3eec7c
Soby Mathew <soby.mathew@arm.com>
2016-07-07 10:03:21 +0100

    Define `plat_get_syscnt_freq2()` unconditionally for ARM platforms
    Previously the definition of `plat_get_syscnt_freq2()` in `arm_common.c` was
    conditionally defined based on the ERROR_DEPRECATED flag. This patch makes
    this function available irrespective of the flag and removes the deprecated
    `plat_get_syscnt_freq()` definition.
    
    Change-Id: I250ca787ca1b5e867096c6ba8f2bb444db44c97b

--- plat/arm/common/arm_common.c
+++ plat/arm/common/arm_common.c
@@ -46,8 +46,6 @@ extern const mmap_region_t plat_arm_mmap[];
  * conflicts with the definition in plat/common. */
 #if ERROR_DEPRECATED
 #pragma weak plat_get_syscnt_freq2
-#else
-#pragma weak plat_get_syscnt_freq
 #endif
 
 /*
@@ -187,15 +185,9 @@ const mmap_region_t *plat_arm_get_mmap(void)
 
 #ifdef ARM_SYS_CNTCTL_BASE
 
-#if ERROR_DEPRECATED
 unsigned int plat_get_syscnt_freq2(void)
 {
 	unsigned int counter_base_frequency;
-#else
-unsigned long long plat_get_syscnt_freq(void)
-{
-	unsigned long long counter_base_frequency;
-#endif /* ERROR_DEPRECATED */
 
 	/* Read the frequency from Frequency modes table */
 	counter_base_frequency = mmio_read_32(ARM_SYS_CNTCTL_BASE + CNTFID_OFF);
