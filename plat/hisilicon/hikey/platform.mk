6afb05837600b647e6ef07ccf44c6183bc66983e
Haojian Zhuang <haojian.zhuang@linaro.org>
2016-09-18 13:05:23 +0800

    hikey: support bl2u
    Support firmware updater BL2U in hikey platform. DDR is initialized
    in BL2U since fastboot driver needs to access more memory. And
    fastboot is the protocol to flush images into eMMC of hikey.
    
    Signed-off-by: Haojian Zhuang <haojian.zhuang@linaro.org>

--- plat/hisilicon/hikey/platform.mk
+++ plat/hisilicon/hikey/platform.mk
@@ -30,6 +30,7 @@
 
 CONSOLE_BASE			:=	PL011_UART3_BASE
 CRASH_CONSOLE_BASE		:=	PL011_UART3_BASE
+PLAT_PARTITION_MAX_ENTRIES	:=	12
 PLAT_PL061_MAX_GPIOS		:=	160
 COLD_BOOT_SINGLE_CPU		:=	1
 PROGRAMMABLE_RESET_ADDRESS	:=	1
@@ -38,6 +39,7 @@ PROGRAMMABLE_RESET_ADDRESS	:=	1
 $(eval $(call add_define,CONSOLE_BASE))
 $(eval $(call add_define,CRASH_CONSOLE_BASE))
 $(eval $(call add_define,PLAT_PL061_MAX_GPIOS))
+$(eval $(call add_define,PLAT_PARTITION_MAX_ENTRIES))
 
 ENABLE_PLAT_COMPAT	:=	0
 
@@ -68,3 +70,22 @@ BL1_SOURCES		+=	bl1/tbbr/tbbr_img_desc.c		\
 				plat/hisilicon/hikey/hikey_bl1_setup.c	\
 				plat/hisilicon/hikey/hikey_io_storage.c
 
+BL2U_SOURCES		+=	drivers/arm/pl061/pl061_gpio.c		\
+				drivers/arm/sp804/sp804_delay_timer.c	\
+				drivers/delay_timer/delay_timer.c	\
+				drivers/emmc/emmc.c			\
+				drivers/fastboot/fastboot.c		\
+				drivers/fastboot/usb.c			\
+				drivers/gpio/gpio.c			\
+				drivers/io/io_block.c			\
+				drivers/io/io_fip.c			\
+				drivers/io/io_storage.c			\
+				drivers/partition/gpt.c			\
+				drivers/partition/partition.c		\
+				drivers/synopsys/emmc/dw_mmc.c		\
+				drivers/synopsys/usb/dw_usb.c		\
+				plat/common/aarch64/platform_up_stack.S	\
+				plat/hisilicon/hikey/hikey_bl2u_setup.c	\
+				plat/hisilicon/hikey/hikey_ddr.c	\
+				plat/hisilicon/hikey/hikey_io_storage.c	\
+				plat/hisilicon/hikey/hikey_fastboot.c
