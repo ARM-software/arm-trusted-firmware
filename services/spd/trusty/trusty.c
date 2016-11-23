48c1c39fef53b05d2e73a095b56616a4fc915b65
Sandrine Bailleux <sandrine.bailleux@arm.com>
2016-11-23 10:53:07 +0000

    Fix a coding style issue in trusty.c
    This patch fixes the following coding style error reported
    by the checkpatch.pl script:
    
      Bad function definition - void el3_exit() should probably
      be void el3_exit(void)
    
    There is another one but it's a false positive so there's no
    point in fixing it:
    
      space prohibited after that '&' (ctx:WxW)
      +#define SMC_NR(entity, fn, fastcall, smc64) ((((fastcall) & 0x1) << 31) | \
                                                                ^
    Change-Id: I34de0337c7216dabd16395879f13845a60ee6df0
    
    Signed-off-by: Sandrine Bailleux <sandrine.bailleux@arm.com>

--- services/spd/trusty/trusty.c
+++ services/spd/trusty/trusty.c
@@ -229,7 +229,7 @@ static uint64_t trusty_smc_handler(uint32_t smc_fid,
 
 static int32_t trusty_init(void)
 {
-	void el3_exit();
+	void el3_exit(void);
 	entry_point_info_t *ep_info;
 	struct trusty_cpu_ctx *ctx = get_trusty_ctx();
 	uint32_t cpu = plat_my_core_pos();
