f122d67cd4c3a092cdd3de940a928c9d41c6f50c
Masahiro Yamada <yamada.masahiro@socionext.com>
2017-02-21 21:39:26 +0900

    io: semihosting: re-order callbacks to save declarations
    Define callbacks above the ops structure.  This allows us to remove
    unneeded declaration.
    
    While we are here, remove excessive blank lines.
    
    Signed-off-by: Masahiro Yamada <yamada.masahiro@socionext.com>

--- drivers/io/io_semihosting.c
+++ drivers/io/io_semihosting.c
@@ -33,62 +33,11 @@
 #include <io_storage.h>
 #include <semihosting.h>
 
-
-
 /* Identify the device type as semihosting */
 static io_type_t device_type_sh(void)
 {
 	return IO_TYPE_SEMIHOSTING;
 }
-
-
-/* Semi-hosting functions, device info and handle */
-
-static int sh_dev_open(const uintptr_t dev_spec, io_dev_info_t **dev_info);
-static int sh_file_open(io_dev_info_t *dev_info, const uintptr_t spec,
-		io_entity_t *entity);
-static int sh_file_seek(io_entity_t *entity, int mode, ssize_t offset);
-static int sh_file_len(io_entity_t *entity, size_t *length);
-static int sh_file_read(io_entity_t *entity, uintptr_t buffer, size_t length,
-		size_t *length_read);
-static int sh_file_write(io_entity_t *entity, const uintptr_t buffer,
-		size_t length, size_t *length_written);
-static int sh_file_close(io_entity_t *entity);
-
-static const io_dev_connector_t sh_dev_connector = {
-	.dev_open = sh_dev_open
-};
-
-
-static const io_dev_funcs_t sh_dev_funcs = {
-	.type = device_type_sh,
-	.open = sh_file_open,
-	.seek = sh_file_seek,
-	.size = sh_file_len,
-	.read = sh_file_read,
-	.write = sh_file_write,
-	.close = sh_file_close,
-	.dev_init = NULL,	/* NOP */
-	.dev_close = NULL,	/* NOP */
-};
-
-
-/* No state associated with this device so structure can be const */
-static const io_dev_info_t sh_dev_info = {
-	.funcs = &sh_dev_funcs,
-	.info = (uintptr_t)NULL
-};
-
-
-/* Open a connection to the semi-hosting device */
-static int sh_dev_open(const uintptr_t dev_spec __unused,
-		io_dev_info_t **dev_info)
-{
-	assert(dev_info != NULL);
-	*dev_info = (io_dev_info_t *)&sh_dev_info; /* cast away const */
-	return 0;
-}
-
 
 /* Open a file on the semi-hosting device */
 static int sh_file_open(io_dev_info_t *dev_info __unused,
@@ -110,7 +59,6 @@ static int sh_file_open(io_dev_info_t *dev_info __unused,
 	return result;
 }
 
-
 /* Seek to a particular file offset on the semi-hosting device */
 static int sh_file_seek(io_entity_t *entity, int mode, ssize_t offset)
 {
@@ -124,7 +72,6 @@ static int sh_file_seek(io_entity_t *entity, int mode, ssize_t offset)
 
 	return (sh_result == 0) ? 0 : -ENOENT;
 }
-
 
 /* Return the size of a file on the semi-hosting device */
 static int sh_file_len(io_entity_t *entity, size_t *length)
@@ -144,7 +91,6 @@ static int sh_file_len(io_entity_t *entity, size_t *length)
 
 	return result;
 }
-
 
 /* Read data from a file on the semi-hosting device */
 static int sh_file_read(io_entity_t *entity, uintptr_t buffer, size_t length,
@@ -171,7 +117,6 @@ static int sh_file_read(io_entity_t *entity, uintptr_t buffer, size_t length,
 	return result;
 }
 
-
 /* Write data to a file on the semi-hosting device */
 static int sh_file_write(io_entity_t *entity, const uintptr_t buffer,
 		size_t length, size_t *length_written)
@@ -193,7 +138,6 @@ static int sh_file_write(io_entity_t *entity, const uintptr_t buffer,
 	return (sh_result == 0) ? 0 : -ENOENT;
 }
 
-
 /* Close a file on the semi-hosting device */
 static int sh_file_close(io_entity_t *entity)
 {
@@ -209,6 +153,36 @@ static int sh_file_close(io_entity_t *entity)
 	return (sh_result >= 0) ? 0 : -ENOENT;
 }
 
+static const io_dev_funcs_t sh_dev_funcs = {
+	.type = device_type_sh,
+	.open = sh_file_open,
+	.seek = sh_file_seek,
+	.size = sh_file_len,
+	.read = sh_file_read,
+	.write = sh_file_write,
+	.close = sh_file_close,
+	.dev_init = NULL,	/* NOP */
+	.dev_close = NULL,	/* NOP */
+};
+
+/* No state associated with this device so structure can be const */
+static const io_dev_info_t sh_dev_info = {
+	.funcs = &sh_dev_funcs,
+	.info = (uintptr_t)NULL
+};
+
+/* Open a connection to the semi-hosting device */
+static int sh_dev_open(const uintptr_t dev_spec __unused,
+		io_dev_info_t **dev_info)
+{
+	assert(dev_info != NULL);
+	*dev_info = (io_dev_info_t *)&sh_dev_info; /* cast away const */
+	return 0;
+}
+
+static const io_dev_connector_t sh_dev_connector = {
+	.dev_open = sh_dev_open
+};
 
 /* Exported functions */
 
