3436089d6725e74c836ae6c1f5367eeedc051f48
Anthony Zhou <anzhou@nvidia.com>
2017-04-27 22:00:54 +0800

    Tegra: delay_timer: fix MISRA defects
    Main fixes:
    
    * Include header file for function declarations [Rule 8.4]
    * Move global object into function [Rule 8.9]
    
    Change-Id: I1bc9f3f0ebd4ffc0b8444ac856cd97b0cb56bda4
    Signed-off-by: Anthony Zhou <anzhou@nvidia.com>

--- plat/nvidia/tegra/common/tegra_delay_timer.c
+++ plat/nvidia/tegra/common/tegra_delay_timer.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015, ARM Limited and Contributors. All rights reserved.
+ * Copyright (c) 2015-2017, ARM Limited and Contributors. All rights reserved.
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
@@ -7,17 +7,12 @@
 #include <delay_timer.h>
 #include <mmio.h>
 #include <tegra_def.h>
+#include <tegra_private.h>
 
 static uint32_t tegra_timerus_get_value(void)
 {
 	return mmio_read_32(TEGRA_TMRUS_BASE);
 }
-
-static const timer_ops_t tegra_timer_ops = {
-	.get_timer_value	= tegra_timerus_get_value,
-	.clk_mult		= 1,
-	.clk_div		= 1,
-};
 
 /*
  * Initialise the on-chip free rolling us counter as the delay
@@ -25,5 +20,11 @@ static const timer_ops_t tegra_timer_ops = {
  */
 void tegra_delay_timer_init(void)
 {
+	static const timer_ops_t tegra_timer_ops = {
+		.get_timer_value	= tegra_timerus_get_value,
+		.clk_mult		= 1,
+		.clk_div		= 1,
+	};
+
 	timer_init(&tegra_timer_ops);
 }
