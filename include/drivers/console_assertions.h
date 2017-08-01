9536bae6df5638772a1e8b1c8cf8e321f4ab5452
Julius Werner <jwerner@chromium.org>
2017-07-31 18:15:11 -0700

    Add new function-pointer-based console API
    This patch overhauls the console API to allow for multiple console
    instances of different drivers that are active at the same time. Instead
    of binding to well-known function names (like console_core_init),
    consoles now provide a register function (e.g. console_16550_register())
    that will hook them into the list of active consoles. All console
    operations will be dispatched to all consoles currently in the list.
    
    The new API will be selected by the build-time option MULTI_CONSOLE_API,
    which defaults to ${ERROR_DEPRECATED} for now. The old console API code
    will be retained to stay backwards-compatible to older platforms, but
    should no longer be used for any newly added platforms and can hopefully
    be removed at some point in the future.
    
    The new console API is intended to be used for both normal (bootup) and
    crash use cases, freeing platforms of the need to set up the crash
    console separately. Consoles can be individually configured to be active
    active at boot (until first handoff to EL2), at runtime (after first
    handoff to EL2), and/or after a crash. Console drivers should set a sane
    default upon registration that can be overridden with the
    console_set_scope() call. Code to hook up the crash reporting mechanism
    to this framework will be added with a later patch.
    
    This patch only affects AArch64, but the new API could easily be ported
    to AArch32 as well if desired.
    
    Change-Id: I35c5aa2cb3f719cfddd15565eb13c7cde4162549
    Signed-off-by: Julius Werner <jwerner@chromium.org>

---
+++ include/drivers/console_assertions.h
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2017, ARM Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef __CONSOLE_ASSERTIONS_H__
+#define __CONSOLE_ASSERTIONS_H__
+
+#include <cassert.h>
+
+/*
+ * This file contains some separate assertions about console_t, moved here to
+ * keep them out of the way. Should only be included from <console.h>.
+ */
+CASSERT(CONSOLE_T_NEXT == __builtin_offsetof(console_t, next),
+	assert_console_t_next_offset_mismatch);
+CASSERT(CONSOLE_T_FLAGS == __builtin_offsetof(console_t, flags),
+	assert_console_t_flags_offset_mismatch);
+CASSERT(CONSOLE_T_PUTC == __builtin_offsetof(console_t, putc),
+	assert_console_t_putc_offset_mismatch);
+CASSERT(CONSOLE_T_GETC == __builtin_offsetof(console_t, getc),
+	assert_console_t_getc_offset_mismatch);
+CASSERT(CONSOLE_T_FLUSH == __builtin_offsetof(console_t, flush),
+	assert_console_t_flush_offset_mismatch);
+CASSERT(CONSOLE_T_DRVDATA == sizeof(console_t),
+	assert_console_t_drvdata_offset_mismatch);
+
+#endif /* __CONSOLE_ASSERTIONS_H__ */
+
