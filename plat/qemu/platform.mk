ccdbae7198b4bd9fe5c2e940da34accac245ee78
Jens Wiklander <jens.wiklander@linaro.org>
2017-08-24 13:16:20 +0200

    qemu: replace deprecated plat_psci_common.c
    Change to compile with new plat/common/plat_psci_common.c instead of the old
    deprecated plat/common/aarch64/plat_psci_common.c
    
    Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>

--- plat/qemu/platform.mk
+++ plat/qemu/platform.mk
@@ -72,7 +72,7 @@ BL31_SOURCES		+=	lib/cpus/aarch64/aem_generic.S		\
 				drivers/arm/gic/v2/gicv2_helpers.c	\
 				drivers/arm/gic/v2/gicv2_main.c		\
 				drivers/arm/gic/common/gic_common.c	\
-				plat/common/aarch64/plat_psci_common.c	\
+				plat/common/plat_psci_common.c		\
 				plat/qemu/qemu_pm.c			\
 				plat/qemu/topology.c			\
 				plat/qemu/aarch64/plat_helpers.S	\
