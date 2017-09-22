8e743bcd6a69bce2ac782319951061c69798558d
Jeenu Viswambharan <jeenu.viswambharan@arm.com>
2017-09-22 08:32:10 +0100

    BL31: Introduce Publish and Subscribe framework
    This light-weight framework enables some EL3 components to publish
    events which other EL3 components can subscribe to. Publisher can
    optionally pass opaque data for subscribers. The order in which
    subscribers are called is not defined.
    
    Firmware design updated.
    
    Change-Id: I24a3a70b2b1dedcb1f73cf48313818aebf75ebb6
    Signed-off-by: Jeenu Viswambharan <jeenu.viswambharan@arm.com>

---
+++ include/lib/el3_runtime/pubsub_events.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2017, ARM Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <pubsub.h>
+
+/*
+ * This file defines a list of pubsub events, declared using
+ * REGISTER_PUBSUB_EVENT() macro.
+ */
