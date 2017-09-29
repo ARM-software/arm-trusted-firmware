929a764d0cd89a4921d87503c2a1df0324942bcc
Steven Kao <skao@nvidia.com>
2017-09-29 16:32:34 +0800

    Tegra194: increase MAX_MMAP_REGIONS macro value
    This patch increases the MAX_MMAP_REGIONS value to 30 from 25 to
    allow addition of more MMU mappings.
    
    Change-Id: I5c758c432f5cc77299608e25ba2fd92c3822379d
    Signed-off-by: Steven Kao <skao@nvidia.com>

--- plat/nvidia/tegra/soc/t194/platform_t194.mk
+++ plat/nvidia/tegra/soc/t194/platform_t194.mk
@@ -42,7 +42,7 @@ $(eval $(call add_define,PLATFORM_MAX_CPUS_PER_CLUSTER))
 MAX_XLAT_TABLES				:= 25
 $(eval $(call add_define,MAX_XLAT_TABLES))
 
-MAX_MMAP_REGIONS			:= 25
+MAX_MMAP_REGIONS			:= 30
 $(eval $(call add_define,MAX_MMAP_REGIONS))
 
 # platform files
