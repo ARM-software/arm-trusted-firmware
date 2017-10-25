7191566c696e3f9070db9f2b9b8503a5617fa6db
Varun Wadekar <vwadekar@nvidia.com>
2017-10-25 11:52:07 -0700

    Tegra186: secondary: fix MISRA violations for Rules 8.6, 11.1
    This patch fixes the following MISRA violations:
    
    Rule 8.6: Externally-linked object or function has "no" definition(s).
    Rule 11.1: A cast shall not convert a pointer to a function to
    any other type.
    
    Change-Id: Ic1f6fc14c744e54ff782c6987dab9c9430410f5e
    Signed-off-by: Varun Wadekar <vwadekar@nvidia.com>

---
+++ plat/nvidia/tegra/include/t186/tegra186_private.h
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2017, NVIDIA CORPORATION. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef TEGRA186_PRIVATE_H
+#define TEGRA186_PRIVATE_H
+
+void tegra186_cpu_reset_handler(void);
+uint64_t tegra186_get_cpu_reset_handler_base(void);
+uint64_t tegra186_get_cpu_reset_handler_size(void);
+
+#endif /* TEGRA186_PRIVATE_H */
