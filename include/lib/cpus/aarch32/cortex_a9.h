e3148c2b5339da8201033bcc2e88e27cf46fd48b
Etienne Carriere <etienne.carriere@linaro.org>
2017-11-05 22:56:19 +0100

    ARMv7: introduce Cortex-A9
    As Cortex-A9 needs to manually enable program flow prediction,
    do not reset SCTLR[Z] at entry. Platform should enable it only
    once MMU is enabled.
    
    Change-Id: I34e1ee2da73221903f7767f23bc6fc10ad01e3de
    Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>

---
+++ include/lib/cpus/aarch32/cortex_a9.h
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2017, ARM Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef __CORTEX_A9_H__
+#define __CORTEX_A9_H__
+
+/*******************************************************************************
+ * Cortex-A9 midr with version/revision set to 0
+ ******************************************************************************/
+#define CORTEX_A9_MIDR			0x410FC090
+
+/*******************************************************************************
+ * CPU Auxiliary Control register specific definitions.
+ ******************************************************************************/
+#define CORTEX_A9_ACTLR_SMP_BIT		(1 << 6)
+#define CORTEX_A9_ACTLR_FLZW_BIT	(1 << 3)
+
+/*******************************************************************************
+ * CPU Power Control Register
+ ******************************************************************************/
+#define PCR		p15, 0, c15, c0, 0
+
+#ifndef __ASSEMBLY__
+#include <arch_helpers.h>
+DEFINE_COPROCR_RW_FUNCS(pcr, PCR)
+#endif
+
+#endif /* __CORTEX_A9_H__ */
