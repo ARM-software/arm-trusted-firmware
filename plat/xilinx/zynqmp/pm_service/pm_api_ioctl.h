1818c029252914b7d5c93915789c7cef32ffcfd7
Rajan Vaja <rajan.vaja@xilinx.com>
2018-01-17 02:39:24 -0800

    zynqmp: pm: Implement IOCTL APIs for device control
    Implement ioctl APIs which uses MMIO operations
    to configure devices. Below IOCTLs are supported
    in this patch:
      * Set tap delay bypass
      * Set SGMII mode
      * SD reset
      * Set SD/MMC tap delay
    
    Signed-off-by: Rajan Vaja <rajanv@xilinx.com>
    Signed-off-by: Jolly Shah <jollys@xilinx.com>

--- plat/xilinx/zynqmp/pm_service/pm_api_ioctl.h
+++ plat/xilinx/zynqmp/pm_service/pm_api_ioctl.h
@@ -18,6 +18,10 @@ enum pm_ioctl_id {
 	IOCTL_SET_RPU_OPER_MODE,
 	IOCTL_RPU_BOOT_ADDR_CONFIG,
 	IOCTL_TCM_COMB_CONFIG,
+	IOCTL_SET_TAPDELAY_BYPASS,
+	IOCTL_SET_SGMII_MODE,
+	IOCTL_SD_DLL_RESET,
+	IOCTL_SET_SD_TAPDELAY,
 };
 
 enum rpu_oper_mode {
@@ -33,6 +37,34 @@ enum rpu_boot_mem {
 enum rpu_tcm_comb {
 	PM_RPU_TCM_SPLIT,
 	PM_RPU_TCM_COMB,
+};
+
+enum tap_delay_signal_type {
+	PM_TAPDELAY_NAND_DQS_IN,
+	PM_TAPDELAY_NAND_DQS_OUT,
+	PM_TAPDELAY_QSPI,
+	PM_TAPDELAY_MAX,
+};
+
+enum tap_delay_bypass_ctrl {
+	PM_TAPDELAY_BYPASS_DISABLE,
+	PM_TAPDELAY_BYPASS_ENABLE,
+};
+
+enum sgmii_mode {
+	PM_SGMII_DISABLE,
+	PM_SGMII_ENABLE,
+};
+
+enum tap_delay_type {
+	PM_TAPDELAY_INPUT,
+	PM_TAPDELAY_OUTPUT,
+};
+
+enum dll_reset_type {
+	PM_DLL_RESET_ASSERT,
+	PM_DLL_RESET_RELEASE,
+	PM_DLL_RESET_PULSE,
 };
 
 enum pm_ret_status pm_api_ioctl(enum pm_node_id nid,
