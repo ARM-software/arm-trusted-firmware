19b731e810399145ee085cfe25411f91bbb0625b
Haojian Zhuang <haojian.zhuang@linaro.org>
2018-01-30 10:35:17 +0800

    hikey960: fix invoking driver init in image load driver
    It's unnecessary to call platform driver initialization in image
    load driver. We could make bl2_platform_setup() to executing
    just before SCP_BL2 by setting flag IMAGE_ATTRIB_PLAT_SETUP.
    
    Signed-off-by: Haojian Zhuang <haojian.zhuang@linaro.org>

--- plat/hisilicon/hikey960/hikey960_image_load.c
+++ plat/hisilicon/hikey960/hikey960_image_load.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017, ARM Limited and Contributors. All rights reserved.
+ * Copyright (c) 2017-2018, ARM Limited and Contributors. All rights reserved.
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
@@ -7,8 +7,6 @@
 #include <bl_common.h>
 #include <desc_image_load.h>
 #include <platform.h>
-
-#include "hikey960_private.h"
 
 /*******************************************************************************
  * This function flushes the data structures so that they are visible
@@ -24,10 +22,6 @@ void plat_flush_next_bl_params(void)
  ******************************************************************************/
 bl_load_info_t *plat_get_bl_image_load_info(void)
 {
-	/* Required before loading scp_bl2 */
-	hikey960_init_ufs();
-	hikey960_io_setup();
-
 	return get_bl_load_info_from_mem_params_desc();
 }
 
