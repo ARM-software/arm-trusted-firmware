81bf6aae6855d3afb6c99ac82a75f576f6af6131
Soby Mathew <soby.mathew@arm.com>
2018-02-20 14:48:50 +0000

    ARM Platforms: Don't build BL1 and BL2 if RESET_TO_SP_MIN=1
    Change-Id: Iadb21bb56f2e61d7e6aec9b3b3efd30059521def
    Signed-off-by: Soby Mathew <soby.mathew@arm.com>

--- plat/arm/common/sp_min/arm_sp_min.mk
+++ plat/arm/common/sp_min/arm_sp_min.mk
@@ -5,6 +5,13 @@
 #
 
 # SP MIN source files common to ARM standard platforms
+
+# Skip building BL1 and BL2 if RESET_TO_SP_MIN flag is set.
+ifeq (${RESET_TO_SP_MIN},1)
+    BL1_SOURCES =
+    BL2_SOURCES =
+endif
+
 BL32_SOURCES		+=	plat/arm/common/arm_pm.c			\
 				plat/arm/common/arm_topology.c			\
 				plat/arm/common/sp_min/arm_sp_min_setup.c	\
