92bec97f5c39b16b1599d41337ae4556b6da6c72
Jeenu Viswambharan <jeenu.viswambharan@arm.com>
2018-04-27 15:06:57 +0100

    xlat v1: Provide direct MMU-enabling stubs
    An earlier patch split MMU-enabling function for translation library v2.
    Although we don't intend to introduce the exact same functionality for
    xlat v1, this patch introduces stubs for directly enabling MMU to
    maintain API-compatibility.
    
    Change-Id: Id7d56e124c80af71de999fcda10f1734b50bca97
    Signed-off-by: Jeenu Viswambharan <jeenu.viswambharan@arm.com>

--- lib/xlat_tables/aarch32/xlat_tables.c
+++ lib/xlat_tables/aarch32/xlat_tables.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016-2017, ARM Limited and Contributors. All rights reserved.
+ * Copyright (c) 2016-2018, ARM Limited and Contributors. All rights reserved.
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
@@ -129,4 +129,9 @@ void enable_mmu_secure(unsigned int flags)
 
 	/* Ensure the MMU enable takes effect immediately */
 	isb();
+}
+
+void enable_mmu_direct(unsigned int flags)
+{
+	enable_mmu_secure(flags);
 }
