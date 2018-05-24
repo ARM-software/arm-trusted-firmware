e286b563a6344825375b8094f016cd5fbc5e265e
Bryan O'Donoghue <bryan.odonoghue@linaro.org>
2018-05-24 13:00:57 +0100

    warp7: io_storage: Add initial stub warp7_io_storage.c
    This commit adds warp7_io_storage.c with a stub function
    plat_get_image_source(). Just enough to compile up an image and link
    without any functionality contained therein.
    
    Later patches will add some logic to flesh this out.
    
    Signed-off-by: Bryan O'Donoghue <bryan.odonoghue@linaro.org>

---
+++ plat/imx/imx7/warp7/warp7_io_storage.c
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2018, ARM Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <platform_def.h>
+
+int plat_get_image_source(unsigned int image_id, uintptr_t *dev_handle,
+			  uintptr_t *image_spec)
+{
+	return 0;
+}
