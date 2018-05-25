3cb9c25db9c0adba73d8f2026d57f33dc9a76571
Bryan O'Donoghue <bryan.odonoghue@linaro.org>
2018-05-25 17:13:25 +0100

    warp7: Add stub warp7_bl2_el3_setup.c
    On the i.MX7 the BootROM hands off to the second stage at EL3. The BootROM
    is not ATF compliant though it does have a proprietary root-of-trust
    authentication system that makes it possible to run ATF as an authenticated
    binary.
    
    This patch stubs out the necessary mandatory functions with some comments
    for later binding to the lower-level initialization routines to come.
    
    Signed-off-by: Bryan O'Donoghue <bryan.odonoghue@linaro.org>

---
+++ plat/imx/imx7/warp7/warp7_bl2_el3_setup.c
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2018, ARM Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <arch_helpers.h>
+#include <assert.h>
+#include <bl_common.h>
+#include <debug.h>
+#include <desc_image_load.h>
+#include <platform_def.h>
+#include <xlat_mmu_helpers.h>
+#include <xlat_tables_defs.h>
+
+#include "warp7_private.h"
+
+int bl2_plat_handle_post_image_load(unsigned int image_id)
+{
+	return 0;
+}
+
+void bl2_el3_plat_arch_setup(void)
+{
+	/* Setup the MMU here */
+}
+
+/*
+ * bl2_early_platform_setup()
+ * MMU off
+ */
+void bl2_el3_early_platform_setup(u_register_t arg1, u_register_t arg2,
+				  u_register_t arg3, u_register_t arg4)
+{
+	/* Initialize the AIPS */
+
+	/* Initialize clocks, regulators, pin-muxes etc */
+
+	/* Init UART, storage and friends */
+}
+
+/*
+ * bl2_platform_setup()
+ * MMU on - enabled by bl2_el3_plat_arch_setup()
+ */
+void bl2_platform_setup(void)
+{
+}
