d56e39155376f6eb681dcadbdddf6c861bae8e65
Bryan O'Donoghue <bryan.odonoghue@linaro.org>
2018-05-25 17:18:56 +0100

    warp7: Add a warp7_private.h file
    Internal declarations for the WaRP7 port will go here. For now just include
    sys/types.h.
    
    Signed-off-by: Bryan O'Donoghue <bryan.odonoghue@linaro.org>

---
+++ plat/imx/imx7/warp7/warp7_private.h
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2015-2018, ARM Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef __WARP7_PRIVATE_H__
+#define __WARP7_PRIVATE_H__
+
+#include <sys/types.h>
+
+/*******************************************************************************
+ * Function and variable prototypes
+ ******************************************************************************/
+
+#endif /*__WARP7_PRIVATE_H__ */
