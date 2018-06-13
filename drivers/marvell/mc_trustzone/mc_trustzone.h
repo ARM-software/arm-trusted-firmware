3a9f8eecd7eb4526ec4b6b5d7231038f9e32a3a5
Grzegorz Jaszczyk <jaz@semihalf.com>
2018-06-13 15:27:10 +0200

    drivers: marvell: mc_trustzone: add driver for mc trustzone
    Add simple driver which allows to configure the memory controller trust
    zones. It is responsible for opening mc trustzone window, with
    appropriate base address, size and attributes.
    
    Example of usage in upcoming commits.
    
    Change-Id: I8bea17754d31451b305040ee7de331fb8db0c63f
    Signed-off-by: Grzegorz Jaszczyk <jaz@semihalf.com>
    Reviewed-by: Kostya Porotchkin <kostap@marvell.com>

---
+++ drivers/marvell/mc_trustzone/mc_trustzone.h
@@ -0,0 +1,27 @@
+/*
+ * Copyright (C) 2018 Marvell International Ltd.
+ *
+ * SPDX-License-Identifier:     BSD-3-Clause
+ * https://spdx.org/licenses
+ */
+
+#ifndef _MC_TRUSTZONE_H
+#define _MC_TRUSTZONE_H
+
+#include <addr_map.h>
+
+#define MVEBU_TZ_MAX_WINS	16
+
+#define TZ_VALID		(1 << 0)
+#define TZ_PERM(x)		((x) << 1)
+#define TZ_RZ_ENABLE		(1 << 3)
+
+/* tz attr definitions */
+#define TZ_PERM_RW		(TZ_PERM(0))
+#define TZ_PERM_RO		(TZ_PERM(1))
+#define TZ_PERM_WO		(TZ_PERM(2))
+#define TZ_PERM_ABORT		(TZ_PERM(3))
+
+void tz_enable_win(int ap_index, const struct addr_map_win *win, int win_id);
+
+#endif /* _MC_TRUSTZONE_H */
