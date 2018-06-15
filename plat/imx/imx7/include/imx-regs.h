a84c99fb1ba83c2245fb8dcf6e55440f0b067062
Bryan O'Donoghue <bryan.odonoghue@linaro.org>
2018-06-15 12:50:14 +0100

    imx7: imx-regs: csu: Define SoC-specific CSU features
    This commit
    - Defines MXC_MAX_CSU_REGS. The Central Security Unit - CSU regs
      are used to control the mode peripherals can be accessed in.
    
    - Adds the base address of CSU block
      The CSU block is an offset inside of the AIPS1 block.
    
    Signed-off-by: Bryan O'Donoghue <bryan.odonoghue@linaro.org>

--- plat/imx/imx7/include/imx-regs.h
+++ plat/imx/imx7/include/imx-regs.h
@@ -56,4 +56,8 @@
 /* Define the maximum number of UART blocks on this SoC */
 #define MXC_MAX_UART_NUM		0x07
 
+/* Define the number of CSU registers for this SoC */
+#define MXC_MAX_CSU_REGS		0x40
+#define CSU_BASE			(AIPS1_BASE + 0x3E0000)
+
 #endif /* __REGS_H__ */
