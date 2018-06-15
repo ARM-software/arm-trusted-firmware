67b400705f377fcf7b6c186d5f86559eb075b573
Sumit Garg <sumit.garg@linaro.org>
2018-06-15 14:34:42 +0530

    synquacer: Enable PL011 UART Console
    Signed-off-by: Masahisa Kojima <masahisa.kojima@linaro.org>
    Signed-off-by: Sumit Garg <sumit.garg@linaro.org>

--- plat/socionext/synquacer/sq_bl31_setup.c
+++ plat/socionext/synquacer/sq_bl31_setup.c
@@ -9,11 +9,22 @@
 #include <platform_def.h>
 #include <assert.h>
 #include <bl_common.h>
+#include <pl011.h>
 #include <debug.h>
+
+static console_pl011_t console;
 
 void bl31_early_platform_setup(bl31_params_t *from_bl2,
 				void *plat_params_from_bl2)
 {
+	/* Initialize the console to provide early debug support */
+	(void)console_pl011_register(PLAT_SQ_BOOT_UART_BASE,
+			       PLAT_SQ_BOOT_UART_CLK_IN_HZ,
+			       SQ_CONSOLE_BAUDRATE, &console);
+
+	console_set_scope(&console.console, CONSOLE_FLAG_BOOT |
+			  CONSOLE_FLAG_RUNTIME);
+
 	/* There are no parameters from BL2 if BL31 is a reset vector */
 	assert(from_bl2 == NULL);
 	assert(plat_params_from_bl2 == NULL);
