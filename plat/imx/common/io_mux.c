41964a936b9418b81b5b7211a0859e167dd5a1af
Bryan O'Donoghue <bryan.odonoghue@linaro.org>
2018-06-22 12:48:38 +0100

    imx: io_mux: Add code to write pad function and features
    The io_muxc_set_pad_activity() functions are really just a thin layer
    around a register write. Add in the necessary code to locate the indicated
    register and write the value specified.
    
    Signed-off-by: Bryan O'Donoghue <bryan.odonoghue@linaro.org>

--- plat/imx/common/io_mux.c
+++ plat/imx/common/io_mux.c
@@ -8,10 +8,18 @@
 #include <imx-regs.h>
 #include <io_mux.h>
 
+#include <debug.h>
+
 void io_muxc_set_pad_alt_function(uint32_t pad_mux_offset, uint32_t alt_function)
 {
+	uintptr_t addr = (uintptr_t)(MXC_IO_MUXC_BASE + pad_mux_offset);
+
+	mmio_write_32(addr, alt_function);
 }
 
 void io_muxc_set_pad_features(uint32_t pad_feature_offset, uint32_t pad_features)
 {
+	uintptr_t addr = (uintptr_t)(MXC_IO_MUXC_BASE + pad_feature_offset);
+
+	mmio_write_32(addr, pad_features);
 }
