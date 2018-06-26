4ab2c0a49d57ca30d86bc54e448245c06c1a2eed
Antonio Nino Diaz <antonio.ninodiaz@arm.com>
2018-06-26 21:42:51 +0100

    Document ep_info attribute flags
    The meaning of them wasn't clear in the header file, it was needed to
    search the code for the places where they are used.
    
    Add parentheses to macros in the same header.
    
    Change-Id: I700bc0fd75ccfc0a34f439cb3b2692861bb73b9b
    Signed-off-by: Antonio Nino Diaz <antonio.ninodiaz@arm.com>

--- include/common/ep_info.h
+++ include/common/ep_info.h
@@ -29,33 +29,38 @@
 /* The following are used to set/get image attributes. */
 #define PARAM_EP_SECURITY_MASK		U(0x1)
 
+/* Secure or Non-secure image */
 #define GET_SECURITY_STATE(x) (x & PARAM_EP_SECURITY_MASK)
 #define SET_SECURITY_STATE(x, security) \
 			((x) = ((x) & ~PARAM_EP_SECURITY_MASK) | (security))
 
-#define EP_EE_MASK	U(0x2)
-#define EP_EE_SHIFT	1
-#define EP_EE_LITTLE	U(0x0)
-#define EP_EE_BIG	U(0x2)
-#define EP_GET_EE(x) (x & EP_EE_MASK)
-#define EP_SET_EE(x, ee) ((x) = ((x) & ~EP_EE_MASK) | (ee))
+/* Endianness of the image. */
+#define EP_EE_MASK		U(0x2)
+#define EP_EE_SHIFT		U(1)
+#define EP_EE_LITTLE		U(0x0)
+#define EP_EE_BIG		U(0x2)
+#define EP_GET_EE(x)		((x) & EP_EE_MASK)
+#define EP_SET_EE(x, ee)	((x) = ((x) & ~EP_EE_MASK) | (ee))
 
-#define EP_ST_MASK	U(0x4)
-#define EP_ST_DISABLE	U(0x0)
-#define EP_ST_ENABLE	U(0x4)
-#define EP_GET_ST(x) (x & EP_ST_MASK)
-#define EP_SET_ST(x, ee) ((x) = ((x) & ~EP_ST_MASK) | (ee))
+/* Enable or disable access to the secure timer from secure images. */
+#define EP_ST_MASK		U(0x4)
+#define EP_ST_DISABLE		U(0x0)
+#define EP_ST_ENABLE		U(0x4)
+#define EP_GET_ST(x)		((x) & EP_ST_MASK)
+#define EP_SET_ST(x, ee)	((x) = ((x) & ~EP_ST_MASK) | (ee))
 
-#define EP_EXE_MASK	U(0x8)
-#define NON_EXECUTABLE	U(0x0)
-#define EXECUTABLE	U(0x8)
-#define EP_GET_EXE(x) (x & EP_EXE_MASK)
-#define EP_SET_EXE(x, ee) ((x) = ((x) & ~EP_EXE_MASK) | (ee))
+/* Determine if an image is executable or not. */
+#define EP_EXE_MASK		U(0x8)
+#define NON_EXECUTABLE		U(0x0)
+#define EXECUTABLE		U(0x8)
+#define EP_GET_EXE(x)		((x) & EP_EXE_MASK)
+#define EP_SET_EXE(x, ee)	((x) = ((x) & ~EP_EXE_MASK) | (ee))
 
+/* Flag to indicate the first image that is executed. */
 #define EP_FIRST_EXE_MASK	U(0x10)
 #define EP_FIRST_EXE		U(0x10)
-#define EP_GET_FIRST_EXE(x) ((x) & EP_FIRST_EXE_MASK)
-#define EP_SET_FIRST_EXE(x, ee) ((x) = ((x) & ~EP_FIRST_EXE_MASK) | (ee))
+#define EP_GET_FIRST_EXE(x)	((x) & EP_FIRST_EXE_MASK)
+#define EP_SET_FIRST_EXE(x, ee)	((x) = ((x) & ~EP_FIRST_EXE_MASK) | (ee))
 
 #ifndef __ASSEMBLY__
 
