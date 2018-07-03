14368498da39756b5b0bd088368963dc96a34dad
Ying-Chun Liu (PaulLiu) <paulliu@debian.org>
2018-07-04 02:26:48 +0800

    rpi3: Add support for Trusted Board Boot
    This patch adds support for TBB to rpi3. The ROTPK is generated at build
    time and is included into BL1/BL2. The key and content certificates are
    read over semihosting.
    
    Tested-by: Antonio Nino Diaz <antonio.ninodiaz@arm.com>
    Signed-off-by: Ying-Chun Liu (PaulLiu) <paul.liu@linaro.org>

---
+++ plat/rpi3/rpi3_trusted_boot.c
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2017-2018, ARM Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <platform.h>
+
+extern char rpi3_rotpk_hash[], rpi3_rotpk_hash_end[];
+
+int plat_get_rotpk_info(void *cookie, void **key_ptr, unsigned int *key_len,
+			unsigned int *flags)
+{
+	*key_ptr = rpi3_rotpk_hash;
+	*key_len = rpi3_rotpk_hash_end - rpi3_rotpk_hash;
+	*flags = ROTPK_IS_HASH;
+
+	return 0;
+}
+
+int plat_get_nv_ctr(void *cookie, unsigned int *nv_ctr)
+{
+	*nv_ctr = 0;
+
+	return 0;
+}
+
+int plat_set_nv_ctr(void *cookie, unsigned int nv_ctr)
+{
+	return 1;
+}
