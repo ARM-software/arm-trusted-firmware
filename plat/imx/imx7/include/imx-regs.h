8dac3f36c989f143b527ae081ca364deb1087cf6
Bryan O'Donoghue <bryan.odonoghue@linaro.org>
2018-07-11 15:06:58 +0100

    imx7: caam: Define base-address, job-ring, RTIC and DECO details
    This patch defines the base address of the CAAM in the AIPS3 address range,
    the CAAM_NUM_JOB_RINGS, the number of Real-Time-Integrity-Check (RTIC)
    blocks and the number of descriptor-controllers (DECO) which are
    responsible for executing CAAM descriptors.
    
    Taken in aggregate then this patch specifies the SoC specific semantics of
    the CAAM for the i.MX7 allowing later patches to define shared CAAM
    semantics that ought to be useful for i.MX6, i.MX7, i.MX25 and i.MX8 should
    any of those processors end up doing TF-A ports in the future.
    
    Signed-off-by: Bryan O'Donoghue <bryan.odonoghue@linaro.org>

--- plat/imx/imx7/include/imx-regs.h
+++ plat/imx/imx7/include/imx-regs.h
@@ -83,4 +83,10 @@
 /* Arm optional memory mapped counter module base address */
 #define SYS_CNTCTL_BASE			(AIPS2_BASE + 0x2c0000)
 
+/* Define CAAM AIPS offset */
+#define CAAM_AIPS_BASE			(AIPS3_BASE + 0x100000)
+#define CAAM_NUM_JOB_RINGS		0x03
+#define CAAM_NUM_RTIC			0x04
+#define CAAM_NUM_DECO			0x01
+
 #endif /* __REGS_H__ */
