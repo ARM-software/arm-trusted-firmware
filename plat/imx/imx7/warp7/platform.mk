418c1b879180342c38f5e62bc17fd6bf48771b42
Bryan O'Donoghue <bryan.odonoghue@linaro.org>
2018-07-25 14:47:27 +0100

    warp7: aips: Add AIPS code to the build
    This patch brings in aips.c to the build list. A subsequent patch will call
    into aips_init() to run the code that sets up the requisite AIPS
    permissions.
    
    Signed-off-by: Bryan O'Donoghue <bryan.odonoghue@linaro.org>

--- plat/imx/imx7/warp7/platform.mk
+++ plat/imx/imx7/warp7/platform.mk
@@ -34,6 +34,7 @@ BL2_SOURCES		+=	common/desc_image_load.c			\
 				lib/aarch32/arm32_aeabi_divmod.c		\
 				lib/aarch32/arm32_aeabi_divmod_a32.S		\
 				lib/cpus/aarch32/cortex_a7.S			\
+				plat/imx/common/aips.c				\
 				plat/imx/imx7/warp7/aarch32/warp7_helpers.S	\
 				plat/imx/imx7/warp7/warp7_bl2_el3_setup.c	\
 				plat/imx/imx7/warp7/warp7_bl2_mem_params_desc.c \
