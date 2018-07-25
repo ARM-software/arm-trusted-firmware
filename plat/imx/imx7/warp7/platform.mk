987209ee341ba7f9f526c7c6c948031040774884
Bryan O'Donoghue <bryan.odonoghue@linaro.org>
2018-07-25 14:59:25 +0100

    warp7: clock: Add clock setup code to build
    This commit adds clock.c to the WaRP7 build. Once done we can call into
    clock_init() and get amongst other things an initial UART clock going.
    
    Subsequent patches will add-in clock setup code for USB, eMMC and other
    peripherals we depend on to boot.
    
    Signed-off-by: Bryan O'Donoghue <bryan.odonoghue@linaro.org>

--- plat/imx/imx7/warp7/platform.mk
+++ plat/imx/imx7/warp7/platform.mk
@@ -35,6 +35,7 @@ BL2_SOURCES		+=	common/desc_image_load.c			\
 				lib/aarch32/arm32_aeabi_divmod_a32.S		\
 				lib/cpus/aarch32/cortex_a7.S			\
 				plat/imx/common/aips.c				\
+				plat/imx/common/clock.c				\
 				plat/imx/imx7/warp7/aarch32/warp7_helpers.S	\
 				plat/imx/imx7/warp7/warp7_bl2_el3_setup.c	\
 				plat/imx/imx7/warp7/warp7_bl2_mem_params_desc.c \
