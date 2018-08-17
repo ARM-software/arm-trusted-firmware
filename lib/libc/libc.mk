90f2d452a860adef46dde22ddaf39811a0a43c5d
Antonio Nino Diaz <antonio.ninodiaz@arm.com>
2018-08-17 09:46:43 +0100

    libc: Remove sscanf() and timingsafe_bcmp()
    sscanf() is unused and it doesn't work, so it doesn't make sense to
    keep it.
    
    timingsafe_bcmp() isn't used anywhere.
    
    Change-Id: Ib5d28ff21d0f3ccc36c5c0fb5474b3384105cf80
    Signed-off-by: Antonio Nino Diaz <antonio.ninodiaz@arm.com>

--- lib/libc/libc.mk
+++ lib/libc/libc.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (c) 2016-2017, ARM Limited and Contributors. All rights reserved.
+# Copyright (c) 2016-2018, ARM Limited and Contributors. All rights reserved.
 #
 # SPDX-License-Identifier: BSD-3-Clause
 #
@@ -12,14 +12,12 @@ LIBC_SRCS	:=	$(addprefix lib/libc/,	\
 			printf.c			\
 			putchar.c			\
 			puts.c				\
-			sscanf.c			\
 			strchr.c			\
 			strcmp.c			\
 			strlen.c			\
 			strncmp.c			\
 			strnlen.c			\
-			subr_prf.c			\
-			timingsafe_bcmp.c)
+			subr_prf.c)
 
 INCLUDES	+=	-Iinclude/lib/libc		\
 			-Iinclude/lib/libc/sys
