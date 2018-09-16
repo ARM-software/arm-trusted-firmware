b62b5b923371dd9040903c6b320281ac1f37adcb
Chandni Cherukuri <chandni.cherukuri@arm.com>
2018-09-16 21:06:29 +0530

    plat/arm/sgi: add support for SGI-Clark.Ares platform
    SGI-Clark.Ares platform is a variant of the SGI-Clark platform. It has
    two clusters of four Ares CPUs each. Though very similar to the SGI575
    platform, there are subtle differences. HW_CONFIG and TB_FW_CONFIG dts
    files have been added.
    
    Change-Id: I740a33cbd1c3b1f1984cb56243b46ad379bab3e6
    Signed-off-by: Chandni Cherukuri <chandni.cherukuri@arm.com>

---
+++ plat/arm/board/sgiclarka/include/platform_def.h
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2018, ARM Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef PLATFORM_DEF_H
+#define PLATFORM_DEF_H
+
+#include <sgi_base_platform_def.h>
+
+#define PLAT_ARM_CLUSTER_COUNT		2
+#define CSS_SGI_MAX_CPUS_PER_CLUSTER	4
+#define CSS_SGI_MAX_PE_PER_CPU		1
+
+#define PLAT_CSS_MHU_BASE		0x45400000
+
+#endif /* PLATFORM_DEF_H */
