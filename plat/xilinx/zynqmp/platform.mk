bde25ae28f0ff5db7963d0bd6344afddfd599fc6
Antonio Nino Diaz <antonio.ninodiaz@arm.com>
2018-10-17 16:46:41 +0100

    zynqmp: Remove dependency on arm_helpers.S
    Non-Arm platforms shouldn't use Arm platform code. This patch copies the
    implementation of the functions in arm_helpers.S to zynqmp_helpers.S to
    remove this dependency of zynqmp on Arm platforms.
    
    Change-Id: Ia85f303c4c63bcf0ffa57c7f3ef9d88376729b6b
    Signed-off-by: Antonio Nino Diaz <antonio.ninodiaz@arm.com>

--- plat/xilinx/zynqmp/platform.mk
+++ plat/xilinx/zynqmp/platform.mk
@@ -64,7 +64,6 @@ PLAT_BL_COMMON_SOURCES	:=	lib/xlat_tables/xlat_tables_common.c		\
 				drivers/arm/gic/v2/gicv2_helpers.c		\
 				drivers/cadence/uart/aarch64/cdns_console.S	\
 				drivers/console/aarch64/console.S		\
-				plat/arm/common/aarch64/arm_helpers.S		\
 				plat/arm/common/arm_cci.c			\
 				plat/arm/common/arm_common.c			\
 				plat/arm/common/arm_gicv2.c			\
