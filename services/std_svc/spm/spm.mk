07c13a30d275d5310d71ca5130726ccf2c1cbcc8
Antonio Nino Diaz <antonio.ninodiaz@arm.com>
2018-11-08 09:21:48 +0000

    SPM: Introduce SPRT C host library
    Change-Id: If57ec9cc0791f49d9ade83dff9d24ef9047963a8
    Co-authored-by: Jean-Paul Etienne <jean-paul.etienne@arm.com>
    Signed-off-by: Antonio Nino Diaz <antonio.ninodiaz@arm.com>

--- services/std_svc/spm/spm.mk
+++ services/std_svc/spm/spm.mk
@@ -11,6 +11,8 @@ ifneq (${ARCH},aarch64)
         $(error "Error: SPM is only supported on aarch64.")
 endif
 
+include lib/sprt/sprt_host.mk
+
 SPM_SOURCES	:=	$(addprefix services/std_svc/spm/,	\
 			${ARCH}/spm_helpers.S			\
 			${ARCH}/spm_shim_exceptions.S		\
@@ -18,8 +20,10 @@ SPM_SOURCES	:=	$(addprefix services/std_svc/spm/,	\
 			sp_xlat.c				\
 			spci.c					\
 			spm_main.c				\
-			sprt.c)
+			sprt.c)					\
+			${SPRT_LIB_SOURCES}
 
+INCLUDES	+=	${SPRT_LIB_INCLUDES}
 
 # Force SMC Calling Convention 2 when using SPM
 SMCCC_MAJOR_VERSION	:=	2
