cef2fab952e97826d381b546760c1eef33965474
Ryan Grachek <ryan@edited.us>
2019-01-17 16:07:04 -0600

    hikey960: enable IOMCU DMAC
    There exists a third DMA controller on the hi3660
    SoC called the IOMCU DMAC. This controller is used by
    peripherals like SPI2 and UART3. Initialize changes 4-8
    as non-secure, while 0-3 remain reserved and secure.
    
    Signed-off-by: Ryan Grachek <ryan@edited.us>

--- plat/hisilicon/hikey960/hikey960_bl31_setup.c
+++ plat/hisilicon/hikey960/hikey960_bl31_setup.c
@@ -160,6 +160,20 @@ static void hikey960_edma_init(void)
 	}
 }
 
+static void hikey960_iomcu_dma_init(void)
+{
+	int i;
+	uint32_t non_secure;
+
+	non_secure = IOMCU_DMAC_SEC_CTRL_INTR_SEC | IOMCU_DMAC_SEC_CTRL_GLOBAL_SEC;
+	mmio_write_32(IOMCU_DMAC_SEC_CTRL, non_secure);
+
+	/* channels 0-3 are reserved */
+	for (i = 4; i < IOMCU_DMAC_CHANNEL_NUMS; i++) {
+		mmio_write_32(IOMCU_DMAC_AXI_CONF(i), (1 << 6) | (1 << 18));
+	}
+}
+
 void bl31_platform_setup(void)
 {
 	/* Initialize the GIC driver, cpu and distributor interfaces */
@@ -169,6 +183,7 @@ void bl31_platform_setup(void)
 	gicv2_cpuif_enable();
 
 	hikey960_edma_init();
+	hikey960_iomcu_dma_init();
 
 	hisi_ipc_init();
 }
