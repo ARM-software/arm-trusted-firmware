e77ccb657fe916e645a159e4af779a6fa4b47da2
Florian La Roche <Florian.LaRoche@gmail.com>
2019-01-28 20:39:51 +0100

    rpi3: mark one more function as static
    Signed-off-by: Florian La Roche <Florian.LaRoche@gmail.com>

--- plat/rpi3/rpi3_pm.c
+++ plat/rpi3/rpi3_pm.c
@@ -140,7 +140,7 @@ static int rpi3_pwr_domain_on(u_register_t mpidr)
  * being turned off earlier. The target_state encodes the low power state that
  * each level has woken up from.
  ******************************************************************************/
-void rpi3_pwr_domain_on_finish(const psci_power_state_t *target_state)
+static void rpi3_pwr_domain_on_finish(const psci_power_state_t *target_state)
 {
 	assert(target_state->pwr_domain_state[MPIDR_AFFLVL0] ==
 					PLAT_LOCAL_STATE_OFF);
