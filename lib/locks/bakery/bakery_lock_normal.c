596929b971dc4366a490d5909a265c038502d284
Varun Wadekar <vwadekar@nvidia.com>
2019-01-30 08:26:20 -0800

    locks: linker variables to calculate per-cpu bakery lock size
    This patch introduces explicit linker variables to mark the start and
    end of the per-cpu bakery lock section to help bakery_lock_normal.c
    calculate the size of the section. This patch removes the previously
    used '__PERCPU_BAKERY_LOCK_SIZE__' linker variable to make the code
    uniform across GNU linker and ARM linker.
    
    Change-Id: Ie0c51702cbc0fe8a2076005344a1fcebb48e7cca
    Signed-off-by: Varun Wadekar <vwadekar@nvidia.com>

--- lib/locks/bakery/bakery_lock_normal.c
+++ lib/locks/bakery/bakery_lock_normal.c
@@ -51,7 +51,9 @@ CASSERT((PLAT_PERCPU_BAKERY_LOCK_SIZE & (CACHE_WRITEBACK_GRANULE - 1)) == 0, \
  * Use the linker defined symbol which has evaluated the size reqiurement.
  * This is not as efficient as using a platform defined constant
  */
-IMPORT_SYM(uintptr_t, __PERCPU_BAKERY_LOCK_SIZE__, PERCPU_BAKERY_LOCK_SIZE);
+IMPORT_SYM(uintptr_t, __PERCPU_BAKERY_LOCK_START__, BAKERY_LOCK_START);
+IMPORT_SYM(uintptr_t, __PERCPU_BAKERY_LOCK_END__, BAKERY_LOCK_END);
+#define PERCPU_BAKERY_LOCK_SIZE (BAKERY_LOCK_END - BAKERY_LOCK_START)
 #endif
 
 static inline bakery_lock_t *get_bakery_info(unsigned int cpu_ix,
