10608a63d89ff5b43f227f6f34787bd8cb5131f9
Antonio Nino Diaz <antonio.ninodiaz@arm.com>
2019-02-27 15:11:18 +0000

    trusty: Include file with GIC definitions
    The GIC definitions used in this file have to be provided by the platform
    but platform_def.h wasn't included.
    
    Change-Id: I858cdf7e60c50577c8e798d64ec2cbc4f067f33b
    Signed-off-by: Antonio Nino Diaz <antonio.ninodiaz@arm.com>

--- services/spd/trusty/generic-arm64-smcall.c
+++ services/spd/trusty/generic-arm64-smcall.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016-2018, ARM Limited and Contributors. All rights reserved.
+ * Copyright (c) 2016-2019, ARM Limited and Contributors. All rights reserved.
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
@@ -8,6 +8,7 @@
 
 #include <common/debug.h>
 #include <common/runtime_svc.h>
+#include <platform_def.h>
 
 #include "generic-arm64-smcall.h"
 
