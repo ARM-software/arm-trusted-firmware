f51df47572fb9d5e6808a07ccf5a7e6a5170554d
Masahiro Yamada <yamada.masahiro@socionext.com>
2019-07-23 12:32:58 +0900

    console: add a flag to prepend '\r' in the multi-console framework
    Currently, console drivers prepend '\r' to '\n' by themselves. This is
    common enough to be supported in the framework.
    
    Add a new flag, CONSOLE_FLAG_TRANSLATE_CRLF. A driver can set this
    flag to ask the framework to transform LF into CRLF instead of doing
    it by itself.
    
    Change-Id: I4f5c5887591bc0a8749a105abe62b6562eaf503b
    Signed-off-by: Masahiro Yamada <yamada.masahiro@socionext.com>

--- include/drivers/console.h
+++ include/drivers/console.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013-2018, ARM Limited and Contributors. All rights reserved.
+ * Copyright (c) 2013-2019, ARM Limited and Contributors. All rights reserved.
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
@@ -21,7 +21,8 @@
 #define CONSOLE_FLAG_CRASH		(U(1) << 2)
 /* Bits 3 to 7 reserved for additional scopes in future expansion. */
 #define CONSOLE_FLAG_SCOPE_MASK		((U(1) << 8) - 1)
-/* Bits 8 to 31 reserved for non-scope use in future expansion. */
+/* Bits 8 to 31 for non-scope use. */
+#define CONSOLE_FLAG_TRANSLATE_CRLF	(U(1) << 8)
 
 /* Returned by getc callbacks when receive FIFO is empty. */
 #define ERROR_NO_PENDING_CHAR		(-1)
