dfbdbd0625990267c6742268118ea748e77c6123
Nicolas Le Bayon <nicolas.le.bayon@st.com>
2019-09-10 10:26:50 +0200

    feat(stm32mp1): add NVMEM layout compatibility definition
    Used by driver parsing this node to get information.
    
    Change-Id: I50623a497157adf7b9da6fafe8d79f6ff58c0ebc
    Signed-off-by: Nicolas Le Bayon <nicolas.le.bayon@st.com>

--- plat/st/stm32mp1/stm32mp1_def.h
+++ plat/st/stm32mp1/stm32mp1_def.h
@@ -486,6 +486,7 @@ static inline uint32_t tamp_bkpr(uint32_t idx)
 #define DT_BSEC_COMPAT			"st,stm32mp15-bsec"
 #define DT_DDR_COMPAT			"st,stm32mp1-ddr"
 #define DT_IWDG_COMPAT			"st,stm32mp1-iwdg"
+#define DT_NVMEM_LAYOUT_COMPAT		"st,stm32-nvmem-layout"
 #define DT_PWR_COMPAT			"st,stm32mp1,pwr-reg"
 #define DT_RCC_CLK_COMPAT		"st,stm32mp1-rcc"
 
