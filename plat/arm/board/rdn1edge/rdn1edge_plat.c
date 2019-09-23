c7d4a2178b4f97d7d6eedce28ab63e5833cdd11b
Vijayenthiran Subramaniam <vijayenthiran.subramaniam@arm.com>
2019-09-23 19:32:32 +0530

    plat/arm/sgi: move bl31_platform_setup to board file
    For SGI-575 and RD platforms, move bl31_platform_setup handler to
    individual board files to allow the platforms to perform board specific
    bl31 setup.
    
    Change-Id: Ia44bccc0a7f40a155b33909bcb438a0909b20d42
    Signed-off-by: Vijayenthiran Subramaniam <vijayenthiran.subramaniam@arm.com>

--- plat/arm/board/rdn1edge/rdn1edge_plat.c
+++ plat/arm/board/rdn1edge/rdn1edge_plat.c
@@ -1,10 +1,11 @@
 /*
- * Copyright (c) 2018, ARM Limited and Contributors. All rights reserved.
+ * Copyright (c) 2018-2020, ARM Limited and Contributors. All rights reserved.
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
 #include <plat/common/platform.h>
+#include <sgi_plat.h>
 
 unsigned int plat_arm_sgi_get_platform_id(void)
 {
@@ -15,4 +16,9 @@ unsigned int plat_arm_sgi_get_platform_id(void)
 unsigned int plat_arm_sgi_get_config_id(void)
 {
 	return mmio_read_32(SID_REG_BASE + SID_SYSTEM_CFG_OFFSET);
+}
+
+void bl31_platform_setup(void)
+{
+	sgi_bl31_common_platform_setup();
 }
