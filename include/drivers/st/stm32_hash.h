19e2af7977937b13513f448e0e162df9847b4068
Yann Gautier <yann.gautier@st.com>
2019-10-02 16:33:41 +0200

    crypto: stm32_hash: align stm32_hash_update() prototype
    Use size_t for length parameter in header file, as in .c file.
    
    Change-Id: I310f2a6159cde1c069b4f814f6558c2488c203ec
    Signed-off-by: Yann Gautier <yann.gautier@st.com>

--- include/drivers/st/stm32_hash.h
+++ include/drivers/st/stm32_hash.h
@@ -14,7 +14,7 @@ enum stm32_hash_algo_mode {
 	HASH_SHA256
 };
 
-int stm32_hash_update(const uint8_t *buffer, uint32_t length);
+int stm32_hash_update(const uint8_t *buffer, size_t length);
 int stm32_hash_final(uint8_t *digest);
 int stm32_hash_final_update(const uint8_t *buffer, uint32_t buf_length,
 			    uint8_t *digest);
