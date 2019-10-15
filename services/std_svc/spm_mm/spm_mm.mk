6b1d9e6c3907074284b7d632bd2ca0ee0db5c6e2
Paul Beesley <paul.beesley@arm.com>
2019-10-15 16:13:10 +0000

    spm-mm: Rename source files
    Change-Id: I851be04fc5de8a95ea11270996f8ca33f0fccadb
    Signed-off-by: Paul Beesley <paul.beesley@arm.com>

--- services/std_svc/spm_mm/spm_mm.mk
+++ services/std_svc/spm_mm/spm_mm.mk
@@ -14,9 +14,9 @@ endif
 SPM_SOURCES	:=	$(addprefix services/std_svc/spm_mm/,	\
 			${ARCH}/spm_helpers.S			\
 			${ARCH}/spm_shim_exceptions.S		\
-			spm_main.c				\
-			spm_setup.c				\
-			spm_xlat.c)
+			spm_mm_main.c				\
+			spm_mm_setup.c				\
+			spm_mm_xlat.c)
 
 
 # Let the top-level Makefile know that we intend to include a BL32 image
