18fbfefbbb1e0b86353d2b849b99fae37bbd95f0
Samuel Holland <samuel@sholland.org>
2019-10-20 19:42:15 -0500

    allwinner: Build PMIC bus drivers only in BL31
    These are used by the PMIC setup code, which runs during BL31
    initialization, and the PSCI shutdown code, also a part of BL31.
    They can't be needed before BL31, or it wouldn't be possible to boot.
    Allwinner platforms don't generally build anything but BL31 anyway, but
    this change improves clarity and consistency with allwinner-common.mk.
    
    Signed-off-by: Samuel Holland <samuel@sholland.org>
    Change-Id: I24f1d9ca8b4256e44badf5218d04d8690082babf

--- plat/allwinner/sun50i_a64/platform.mk
+++ plat/allwinner/sun50i_a64/platform.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (c) 2017-2018, ARM Limited and Contributors. All rights reserved.
+# Copyright (c) 2017-2019, ARM Limited and Contributors. All rights reserved.
 #
 # SPDX-License-Identifier: BSD-3-Clause
 #
@@ -7,4 +7,4 @@
 # The differences between the platform are covered by the include files.
 include plat/allwinner/common/allwinner-common.mk
 
-PLAT_BL_COMMON_SOURCES	+=	drivers/allwinner/sunxi_rsb.c
+BL31_SOURCES		+=	drivers/allwinner/sunxi_rsb.c
