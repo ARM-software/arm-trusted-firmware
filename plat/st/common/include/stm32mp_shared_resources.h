47cf5d3f28f4d9bf1912c0470eac01599dd4a7c8
Etienne Carriere <etienne.carriere@st.com>
2019-12-08 08:14:03 +0100

    stm32mp1: introduce shared resources support
    STM32MP1 SoC includes peripheral interfaces that can be assigned to
    the secure world, or that can be opened to the non-secure world.
    
    This change introduces the basics of a driver that manages such
    resources which assignation is done at run time. It currently offers
    API functions that state whether a service exposed to non-secure
    world has permission to access a targeted clock or reset controller.
    
    Change-Id: Iff20028f41586bc501085488c03546ffe31046d8
    Signed-off-by: Etienne Carriere <etienne.carriere@st.com>

---
+++ plat/st/common/include/stm32mp_shared_resources.h
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2017-2020, STMicroelectronics - All Rights Reserved
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef STM32MP_SHARED_RESOURCES_H
+#define STM32MP_SHARED_RESOURCES_H
+
+#include <stdbool.h>
+
+/* Return true if @clock_id is shared by secure and non-secure worlds */
+bool stm32mp_nsec_can_access_clock(unsigned long clock_id);
+
+/* Return true if and only if @reset_id relates to a non-secure peripheral */
+bool stm32mp_nsec_can_access_reset(unsigned int reset_id);
+
+#endif /* STM32MP_SHARED_RESOURCES_H */
