4682461ded0ba15826022ca9e049e19602845e8a
Madhukar Pappireddy <madhukar.pappireddy@arm.com>
2019-12-27 12:02:34 -0600

    fconf: Extract topology node properties from HW_CONFIG dtb
    Create, register( and implicitly invoke) fconf_populate_topology()
    function which extracts the topology related properties from dtb into
    the newly created fconf based configuration structure 'soc_topology'.
    Appropriate libfdt APIs are added to jmptbl.i file for use with USE_ROMLIB
    build feature.
    
    A new property which describes the power domain levels is added to the
    HW_CONFIG device tree source files.
    
    This patch also fixes a minor bug in the common device tree file
    fvp-base-gicv3-psci-dynamiq-common.dtsi
    As this file includes fvp-base-gicv3-psci-common.dtsi, it is necessary
    to delete all previous cluster node definitons because DynamIQ based
    models have upto 8 CPUs in each cluster. If not deleted, the final dts
    would have an inaccurate description of SoC topology, i.e., cluster0
    with 8 or more core nodes and cluster1 with 4 core nodes.
    
    Change-Id: I9eb406da3ba4732008a66c01afec7c9fa8ef59bf
    Signed-off-by: Madhukar Pappireddy <madhukar.pappireddy@arm.com>

--- plat/arm/board/fvp/include/fconf_hw_config_getter.h
+++ plat/arm/board/fvp/include/fconf_hw_config_getter.h
@@ -12,13 +12,24 @@
 /* Hardware Config related getter */
 #define hw_config__gicv3_config_getter(prop) gicv3_config.prop
 
+#define hw_config__topology_getter(prop) soc_topology.prop
+
 struct gicv3_config_t {
 	void *gicd_base;
 	void *gicr_base;
 };
 
+struct hw_topology_t {
+	uint32_t plat_cluster_count;
+	uint32_t cluster_cpu_count;
+	uint32_t plat_cpu_count;
+	uint32_t plat_max_pwr_level;
+};
+
 int fconf_populate_gicv3_config(uintptr_t config);
+int fconf_populate_topology(uintptr_t config);
 
 extern struct gicv3_config_t gicv3_config;
+extern struct hw_topology_t soc_topology;
 
 #endif /* FCONF_HW_CONFIG_GETTER_H */
