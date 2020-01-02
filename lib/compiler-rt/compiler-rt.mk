8c80c86573898eb59127a32b089e753f40e254fb
Lionel Debieve <lionel.debieve@st.com>
2020-01-02 11:09:03 +0100

    compiler_rt: Import aeabi_ldivmode.S file and dependencies
    Import aeabi_ldivmod.S with divmoddi4.c and divdi3.c from the
    LLVM compiler_rt library on master branch as of 30 Oct 2018
    (SVN revision: r345645).
    
    This is to get the __aeabi_ldivmod builtin, which is required
    by a patch using signed long long division.
    
    Change-Id: Iee4c421deb3175142655e19074cd3732edd82227
    Signed-off-by: Lionel Debieve <lionel.debieve@st.com>

--- lib/compiler-rt/compiler-rt.mk
+++ lib/compiler-rt/compiler-rt.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (c) 2017, ARM Limited and Contributors. All rights reserved.
+# Copyright (c) 2017-2019, ARM Limited and Contributors. All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
@@ -29,8 +29,11 @@
 #
 
 ifeq (${ARCH},aarch32)
-COMPILER_RT_SRCS	:=	lib/compiler-rt/builtins/arm/aeabi_uldivmod.S	\
-				lib/compiler-rt/builtins/udivmoddi4.c		\
+COMPILER_RT_SRCS	:=	lib/compiler-rt/builtins/arm/aeabi_ldivmod.S	\
+				lib/compiler-rt/builtins/arm/aeabi_uldivmod.S	\
 				lib/compiler-rt/builtins/ctzdi2.c		\
-				lib/compiler-rt/builtins/lshrdi3.c
+				lib/compiler-rt/builtins/divdi3.c		\
+				lib/compiler-rt/builtins/divmoddi4.c		\
+				lib/compiler-rt/builtins/lshrdi3.c		\
+				lib/compiler-rt/builtins/udivmoddi4.c
 endif
