92c30ac35b298bb4049778ed1c0b95713e44e44c
Tejas Patel <tejas.patel@xilinx.com>
2020-01-10 03:08:40 -0800

    plat: xilinx: Move pm_client.h to common directory
    Move pm_client.h to common directory to avoid duplication
    of function declaration.
    
    Signed-off-by: Tejas Patel <tejas.patel@xilinx.com>
    Signed-off-by: Jolly Shah <jolly.shah@xilinx.com>
    Change-Id: Iea542e681f42db089cccd9b24d286ac8f0a2ce35

--- plat/xilinx/common/include/pm_client.h
+++ plat/xilinx/common/include/pm_client.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019, Xilinx, Inc. All rights reserved.
+ * Copyright (c) 2013-2019, ARM Limited and Contributors. All rights reserved.
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
@@ -17,10 +17,16 @@
 
 /* Functions to be implemented by each PU */
 void pm_client_suspend(const struct pm_proc *proc, unsigned int state);
-void pm_client_wakeup(const struct pm_proc *proc);
 void pm_client_abort_suspend(void);
+void pm_client_wakeup(const struct pm_proc *proc);
 
 /* Global variables to be set in pm_client.c */
 extern const struct pm_proc *primary_proc;
+
+#ifndef VERSAL_PLATFORM
+enum pm_ret_status set_ocm_retention(void);
+enum pm_ret_status pm_set_suspend_mode(uint32_t mode);
+const struct pm_proc *pm_get_proc_by_node(enum pm_node_id nid);
+#endif
 
 #endif /* PM_CLIENT_H */
