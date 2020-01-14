3a36f70ba02e88ed20083c1278a739c6c870aae0
Jacky Bai <ping.bai@nxp.com>
2020-01-14 16:05:59 +0800

    fix(imx8mq): fix imx8mq build break due to hab
    Add the HAB secure boot support for the i.MX8MQ to
    fix the build break.
    
    Signed-off-by: Jacky Bai <ping.bai@nxp.com>
    Change-Id: I806de2dc42806e008355cc185065e774570362f0

--- plat/imx/imx8m/imx8mq/platform.mk
+++ plat/imx/imx8m/imx8mq/platform.mk
@@ -29,6 +29,7 @@ BL31_SOURCES		+=	plat/imx/common/imx8_helpers.S			\
 				plat/imx/imx8m/imx8mq/imx8mq_bl31_setup.c	\
 				plat/imx/imx8m/imx8mq/imx8mq_psci.c		\
 				plat/imx/imx8m/gpc_common.c			\
+				plat/imx/imx8m/imx_hab.c			\
 				plat/imx/imx8m/imx_aipstz.c			\
 				plat/imx/imx8m/imx8m_caam.c			\
 				plat/imx/imx8m/imx8m_ccm.c			\
