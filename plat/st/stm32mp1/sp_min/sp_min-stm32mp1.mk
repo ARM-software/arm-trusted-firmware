7b3a46f0e41786d635a57fa7797c8bf14573ce10
Etienne Carriere <etienne.carriere@st.com>
2020-04-10 11:32:54 +0200

    plat/stm32mp1: sp_min relies on etzpc driver
    Use ETZPC driver to configure secure aware interfaces to assign
    them to non-secure world. Sp_min also configures BootROM resources
    and SYSRAM to assign both to secure world only.
    
    Define stm32mp15 SoC identifiers for the platform specific DECPROT
    instances.
    
    Change-Id: I3bec9f47b04bcba3929e4df886ddb1d5ff843089
    Signed-off-by: Etienne Carriere <etienne.carriere@st.com>

--- plat/st/stm32mp1/sp_min/sp_min-stm32mp1.mk
+++ plat/st/stm32mp1/sp_min/sp_min-stm32mp1.mk
@@ -6,10 +6,12 @@
 
 SP_MIN_WITH_SECURE_FIQ	:=	1
 
-BL32_SOURCES		+=	plat/common/aarch32/platform_mp_stack.S		\
+BL32_SOURCES		+=	drivers/st/etzpc/etzpc.c			\
+				plat/common/aarch32/platform_mp_stack.S		\
 				plat/st/stm32mp1/sp_min/sp_min_setup.c		\
 				plat/st/stm32mp1/stm32mp1_pm.c			\
 				plat/st/stm32mp1/stm32mp1_topology.c
+
 # Generic GIC v2
 BL32_SOURCES		+=	drivers/arm/gic/common/gic_common.c	\
 				drivers/arm/gic/v2/gicv2_helpers.c	\
