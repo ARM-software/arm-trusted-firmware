68450c9437bde3a20de65347aed7c2124ae4e021
Etienne Carriere <etienne.carriere@st.com>
2020-05-13 14:22:01 +0200

    stm32mp1: shared resources: peripheral registering
    Define helper functions stm32mp_register_secure_periph() and
    stm32mp_register_non_secure_periph() for platform drivers to
    register a shared resource assigned to respectively secure
    or non-secure world.
    
    Some resources are related to clock resources. When a resource is
    registered as secure, ensure its clock dependencies are also
    registered as secure. Registering a non-secure resource does not
    mandate its clock dependencies are also registered as non-secure.
    
    Change-Id: I74975be8976b8d3bf18dcc807541a072803af6e3
    Signed-off-by: Etienne Carriere <etienne.carriere@st.com>

--- plat/st/common/include/stm32mp_shared_resources.h
+++ plat/st/common/include/stm32mp_shared_resources.h
@@ -9,13 +9,22 @@
 
 #include <stdbool.h>
 
+#ifdef STM32MP_SHARED_RESOURCES
+enum stm32mp_shres;
+
 /* Return true if @clock_id is shared by secure and non-secure worlds */
 bool stm32mp_nsec_can_access_clock(unsigned long clock_id);
 
 /* Return true if and only if @reset_id relates to a non-secure peripheral */
 bool stm32mp_nsec_can_access_reset(unsigned int reset_id);
 
+/* Register a shared resource assigned to the secure world */
+void stm32mp_register_secure_periph(enum stm32mp_shres id);
+
+/* Register a shared resource assigned to the non-secure world */
+void stm32mp_register_non_secure_periph(enum stm32mp_shres id);
+
 /* Consolidate peripheral states and lock against new peripheral registering */
 void stm32mp_lock_periph_registering(void);
-
+#endif /* STM32MP_SHARED_RESOURCES */
 #endif /* STM32MP_SHARED_RESOURCES_H */
