4e5005254e7e4e8eba1e20fb4893ca077e3a1aad
Leonardo Sandoval <leonardo.sandoval@linaro.org>
2020-06-29 18:09:24 -0500

    fiptool: return zero status on help and help <command>
    Querying the 'fiptool' for help or help <command> should return 0
    return status (success) and not 1 (failure). In the other hand, if tool is
    executed with any other command (not help) where command's parameters are
    either missing or wrong, then the tool should return non-zero (failure). Now,
    the 'usage' function caller is the one that passes the return status.
    
    Change-Id: Id5eea91037cd810fb1e34a42e8199ef504f5daa4
    Signed-off-by: Leonardo Sandoval <leonardo.sandoval@linaro.org>

--- tools/fiptool/fiptool.h
+++ tools/fiptool/fiptool.h
@@ -48,7 +48,7 @@ typedef struct image {
 typedef struct cmd {
 	char              *name;
 	int              (*handler)(int, char **);
-	void             (*usage)(void);
+	void             (*usage)(int);
 } cmd_t;
 
 #endif /* FIPTOOL_H */
