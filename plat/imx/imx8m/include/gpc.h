8c72a7ab20355b48070caa834211ef3474f899a2
Jacky Bai <ping.bai@nxp.com>
2020-08-11 15:56:29 +0800

    plat: imx8m: Fix the macro define error
    the 'always_on' member should be initialized from 'on'.
    
    Signed-off-by: Jacky Bai <ping.bai@nxp.com>
    Change-Id: I5746ff40075b4fcda2ac7d04a8d7f1269af17e91

--- plat/imx/imx8m/include/gpc.h
+++ plat/imx/imx8m/include/gpc.h
@@ -32,7 +32,7 @@
 		.pwr_req = name##_PWR_REQ,		\
 		.pgc_offset = name##_PGC,		\
 		.need_sync = false,			\
-		.always_on = true,			\
+		.always_on = (on),			\
 	}
 
 #define IMX_MIX_DOMAIN(name, on)			\
@@ -42,7 +42,7 @@
 		.adb400_sync = name##_ADB400_SYNC,	\
 		.adb400_ack = name##_ADB400_ACK,	\
 		.need_sync = true,			\
-		.always_on = true,			\
+		.always_on = (on),			\
 	}
 
 struct imx_pwr_domain {
