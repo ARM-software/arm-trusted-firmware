60f995fd98c9f5e81a0fe5b02999c5472f772c2e
Aditya Angadi <aditya.angadi@arm.com>
2020-11-18 08:32:30 +0530

    plat/arm/sgi: refactor header file inclusions
    Upcoming RD platforms have deviations in various definitions of
    platform macros from that of the exisiting platforms. In preparation
    for adding support for those upcoming RD platforms, refactor the
    header file inclusion to allow newer platforms to use a different
    set of platform macros.
    
    Change-Id: Ic80283ddadafaa7f766f300652cb0d4e507efdb6
    Signed-off-by: Aditya Angadi <aditya.angadi@arm.com>

--- plat/arm/css/sgi/include/sgi_base_platform_def.h
+++ plat/arm/css/sgi/include/sgi_base_platform_def.h
@@ -9,12 +9,9 @@
 
 #include <lib/utils_def.h>
 #include <lib/xlat_tables/xlat_tables_defs.h>
-#include <plat/arm/board/common/board_css_def.h>
-#include <plat/arm/board/common/v2m_def.h>
 #include <plat/arm/common/arm_def.h>
 #include <plat/arm/common/arm_spm_def.h>
 #include <plat/arm/css/common/css_def.h>
-#include <plat/arm/soc/common/soc_css_def.h>
 #include <plat/common/common_def.h>
 
 #define PLATFORM_CORE_COUNT		(CSS_SGI_CHIP_COUNT *		\
---
+++ plat/arm/css/sgi/include/sgi_soc_platform_def.h
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2020, ARM Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef SGI_SOC_PLATFORM_DEF_H
+#define SGI_SOC_PLATFORM_DEF_H
+
+#include <sgi_base_platform_def.h>
+#include <plat/arm/board/common/board_css_def.h>
+#include <plat/arm/board/common/v2m_def.h>
+#include <plat/arm/soc/common/soc_css_def.h>
+
+#endif /* SGI_SOC_PLATFORM_DEF_H */
