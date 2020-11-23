f2afaad0718b3a3e2b5f63ea3229933eecc6d7c2
Siva Durga Prasad Paladugu <siva.durga.paladugu@xilinx.com>
2020-11-22 22:10:12 -0800

    zynqmp: pm_api_clock: Copy only the valid bytes
    This patches copies only the valid part of string and
    avoids filling junk at the end.
    
    Signed-off-by: Siva Durga Prasad Paladugu <siva.durga.paladugu@xilinx.com>
    Signed-off-by: Rajan Vaja <rajan.vaja@xilinx.com>
    Change-Id: If23772f31f9cf7f5042e8bfc474fbfe77dcd90e7

--- plat/xilinx/zynqmp/pm_service/pm_api_clock.c
+++ plat/xilinx/zynqmp/pm_service/pm_api_clock.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018-2019, ARM Limited and Contributors. All rights reserved.
+ * Copyright (c) 2018-2020, ARM Limited and Contributors. All rights reserved.
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
@@ -2435,7 +2435,8 @@ enum pm_ret_status pm_api_clock_get_num_clocks(unsigned int *nclocks)
 enum pm_ret_status pm_api_clock_get_name(unsigned int clock_id, char *name)
 {
 	if (clock_id == CLK_MAX)
-		memcpy(name, END_OF_CLK, CLK_NAME_LEN);
+		memcpy(name, END_OF_CLK, sizeof(END_OF_CLK) > CLK_NAME_LEN ?
+					 CLK_NAME_LEN : sizeof(END_OF_CLK));
 	else if (!pm_clock_valid(clock_id))
 		memset(name, 0, CLK_NAME_LEN);
 	else if (clock_id < CLK_MAX_OUTPUT_CLK)
