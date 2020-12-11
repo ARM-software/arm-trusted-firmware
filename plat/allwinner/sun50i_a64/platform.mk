8fa5592b841ec8de48325eb10b35c97449f9e660
Andre Przywara <andre.przywara@arm.com>
2020-12-11 21:29:31 +0000

    allwinner: A64: Limit FDT checks to reduce code size
    The upcoming refactoring to support the new H616 SoCs will push the A64
    build over the edge, by using more than the 48KB of SRAM available.
    
    To reduce the code size, set some libfdt options that aim to reduce
    sanity checks (for saving code space):
    - ASSUME_LATEST: only allow v17 DTBs (as created by dtc)
    - ASSUME_NO_ROLLBACK: don't prepare for failed DT additions
    - ASSUME_LIBFDT_ORDER: assume sane ordering, as done by dtc
    
    Change-Id: I12c93ec09e7587c5ae71e54947f817c32ce5fd6d
    Signed-off-by: Andre Przywara <andre.przywara@arm.com>

--- plat/allwinner/sun50i_a64/platform.mk
+++ plat/allwinner/sun50i_a64/platform.mk
@@ -9,3 +9,6 @@ include plat/allwinner/common/allwinner-common.mk
 
 BL31_SOURCES		+=	drivers/allwinner/axp/axp803.c		\
 				drivers/allwinner/sunxi_rsb.c
+
+FDT_ASSUME_MASK := "(ASSUME_LATEST | ASSUME_NO_ROLLBACK | ASSUME_LIBFDT_ORDER)"
+$(eval $(call add_define,FDT_ASSUME_MASK))
