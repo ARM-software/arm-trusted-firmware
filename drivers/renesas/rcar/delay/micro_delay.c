ee0dbc41045c12164d90c3ca79b7c40fa5da9175
Biju Das <biju.das.jz@bp.renesas.com>
2020-12-13 19:59:26 +0000

    drivers: renesas: rcar: delay: Fix checkpatch warnings
    Fix checkpatch warnings.
    
    There are no functional changes.
    
    Signed-off-by: Biju Das <biju.das.jz@bp.renesas.com>
    Reviewed-by: Lad Prabhakar <prabhakar.mahadev-lad.rj@bp.renesas.com>
    Change-Id: Iec7dd019bd38e84eccd8cc17189745fdef1911bb

--- drivers/renesas/rcar/delay/micro_delay.c
+++ drivers/renesas/rcar/delay/micro_delay.c
@@ -1,18 +1,19 @@
 /*
- * Copyright (c) 2018, Renesas Electronics Corporation. All rights reserved.
+ * Copyright (c) 2018-2020, Renesas Electronics Corporation. All rights reserved.
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
 #include <arch.h>
 #include <arch_helpers.h>
+
 #include "micro_delay.h"
 
 #define RCAR_CONV_MICROSEC		1000000U
 
 void
 #if IMAGE_BL31
-	__attribute__ ((section (".system_ram")))
+	__attribute__ ((section(".system_ram")))
 #endif
 	rcar_micro_delay(uint64_t micro_sec)
 {
