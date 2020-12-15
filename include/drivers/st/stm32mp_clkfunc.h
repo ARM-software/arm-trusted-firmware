812daf916c9c977a4f6d7d745d22b90c8492fc71
Lionel Debieve <lionel.debieve@st.com>
2020-12-15 10:35:59 +0100

    feat(st): update the security based on new compatible
    From the new binding, the RCC become secured based on the new
    compatible. This must be done only from the secure OS initialisation.
    
    Signed-off-by: Lionel Debieve <lionel.debieve@st.com>
    Change-Id: I7f0a62f22bfcca638ddaefc9563df00f89f01653

--- include/drivers/st/stm32mp_clkfunc.h
+++ include/drivers/st/stm32mp_clkfunc.h
@@ -23,7 +23,7 @@ int fdt_rcc_read_uint32_array(const char *prop_name, uint32_t count,
 			      uint32_t *array);
 int fdt_rcc_subnode_offset(const char *name);
 const fdt32_t *fdt_rcc_read_prop(const char *prop_name, int *lenp);
-bool fdt_get_rcc_secure_status(void);
+bool fdt_get_rcc_secure_state(void);
 
 int fdt_get_clock_id(int node);
 unsigned long fdt_get_uart_clock_freq(uintptr_t instance);
