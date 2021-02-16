513ba5c97372dafb7bca60659e494e211413eedc
Thomas Abraham <thomas.abraham@arm.com>
2021-02-16 12:24:13 +0530

    plat/sgi: refactor DMC-620 error handling SMC function id
    The macros defining the SMC function ids for DMC-620 error handling are
    listed in the sgi_base_platform_def.h header file. But these macros are
    not applicable for all platforms supported under plat/sgi. So move these
    macro definitions to sgi_ras.c file in which these are consumed. While
    at it, remove the AArch32 and error injection function ids as these are
    unused.
    
    Signed-off-by: Thomas Abraham <thomas.abraham@arm.com>
    Change-Id: I249b54bf4c1b1694188a1e3b297345b942f16bc9

--- plat/arm/css/sgi/include/sgi_ras.h
+++ plat/arm/css/sgi/include/sgi_ras.h
@@ -1,11 +1,15 @@
 /*
- * Copyright (c) 2018, ARM Limited and Contributors. All rights reserved.
+ * Copyright (c) 2018-2021, ARM Limited and Contributors. All rights reserved.
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
 #ifndef SGI_RAS_H
 #define SGI_RAS_H
+
+/* Platform specific SMC FID's used for DMC-620 RAS error handling */
+#define SP_DMC_ERROR_OVERFLOW_EVENT_AARCH64	0xC4000043
+#define SP_DMC_ERROR_ECC_EVENT_AARCH64		0xC4000044
 
 /*
  * Mapping the RAS interrupt with SDEI event number and the event
