d77583549f89af29b3582867fcf3eabdbc41bb84
Venkatesh Yadav Abbarapu <venkatesh.abbarapu@xilinx.com>
2021-02-19 01:40:14 -0700

    plat: xilinx: common: Rename the IPI CRC checksum macro
    Rename the macro ZYNQMP_IPI_CRC_CHECK to IPI_CRC_CHECK and
    move the related defines to the common include.
    
    Signed-off-by: Venkatesh Yadav Abbarapu <venkatesh.abbarapu@xilinx.com>
    Change-Id: I6d30b081ac607572a0b23e10ca8031bc90489e58

--- plat/xilinx/zynqmp/include/plat_pm_common.h
+++ plat/xilinx/zynqmp/include/plat_pm_common.h
@@ -16,17 +16,6 @@
 #include <common/debug.h>
 #include "pm_defs.h"
 
-#if ZYNQMP_IPI_CRC_CHECK
-#define PAYLOAD_ARG_CNT         8U
-#define IPI_W0_TO_W6_SIZE       28U
-#define PAYLOAD_CRC_POS         7U
-#define CRC_INIT_VALUE          0x4F4EU
-#define CRC_ORDER               16U
-#define CRC_POLYNOM             0x8005U
-#else
-#define PAYLOAD_ARG_CNT         6U
-#endif
-#define PAYLOAD_ARG_SIZE	4U	/* size in bytes */
 
 #define ZYNQMP_TZ_VERSION_MAJOR		1
 #define ZYNQMP_TZ_VERSION_MINOR		0
