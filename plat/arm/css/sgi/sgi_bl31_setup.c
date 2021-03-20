fe5d5bbfe6bd0f386f92bdc419a7e04d885d5b43
Aditya Angadi <aditya.angadi@arm.com>
2021-03-20 12:15:04 +0530

    feat(board/rdn2): add support for variant 1 of rd-n2 platform
    Add board support for RD-N2 Cfg1 variant of RD-N2 platform. It is a
    variant of RD-N2 platform with a reduced interconnect mesh size (3x3)
    and core count (8-cores). Its platform variant id is 1.
    
    Change-Id: I34ad35c5a5c1e9b69a658fb92ed00e5bc5fe72f3
    Signed-off-by: Aditya Angadi <aditya.angadi@arm.com>

--- plat/arm/css/sgi/sgi_bl31_setup.c
+++ plat/arm/css/sgi/sgi_bl31_setup.c
@@ -75,7 +75,8 @@ scmi_channel_plat_info_t *plat_css_get_scmi_info(int channel_id)
 {
 	if (sgi_plat_info.platform_id == RD_N1E1_EDGE_SID_VER_PART_NUM ||
 		sgi_plat_info.platform_id == RD_V1_SID_VER_PART_NUM ||
-		sgi_plat_info.platform_id == RD_N2_SID_VER_PART_NUM) {
+		sgi_plat_info.platform_id == RD_N2_SID_VER_PART_NUM ||
+		sgi_plat_info.platform_id == RD_N2_CFG1_SID_VER_PART_NUM) {
 		if (channel_id >= ARRAY_SIZE(plat_rd_scmi_info))
 			panic();
 		return &plat_rd_scmi_info[channel_id];
