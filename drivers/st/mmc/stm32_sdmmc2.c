d5b4d5d2e62e57acdcb2dbbcd4fe208bde92dc4c
Yann Gautier <yann.gautier@st.com>
2021-05-05 14:28:22 +0200

    feat(st-sdmmc2): set FIFO size to 1024 on STM32MP25
    On STM32MP25, a new version of the SDMMC2 IP is embedded (v3.0).
    The size of the FIFO is 1024 in this new IP version.
    
    Signed-off-by: Yann Gautier <yann.gautier@st.com>
    Change-Id: Ie6b1fb215fc77b24b7c342d4cd69248a96039a4d

--- drivers/st/mmc/stm32_sdmmc2.c
+++ drivers/st/mmc/stm32_sdmmc2.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018-2023, STMicroelectronics - All Rights Reserved
+ * Copyright (c) 2018-2024, STMicroelectronics - All Rights Reserved
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
@@ -129,7 +129,11 @@
 #define DT_SDMMC2_COMPAT		"st,stm32-sdmmc2"
 #endif
 
+#if STM32MP13 || STM32MP15
 #define SDMMC_FIFO_SIZE			64U
+#else
+#define SDMMC_FIFO_SIZE			1024U
+#endif
 
 #define STM32MP_MMC_INIT_FREQ			U(400000)	/*400 KHz*/
 #define STM32MP_SD_NORMAL_SPEED_MAX_FREQ	U(25000000)	/*25 MHz*/
