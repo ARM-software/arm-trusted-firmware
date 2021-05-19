4d585fe52feb231d5e73ec50a505122d5e9bf450
Andre Przywara <andre.przywara@arm.com>
2021-05-19 09:37:21 +0100

    feat(libfdt): also allow changing base address
    For platforms where we don't know the number of cores at compile time,
    the size of the GIC redistributor frame is then also undetermined, since
    it depends on this number of cores.
    On top of this the GICR base address can also change, when an unknown
    number of ITS frames (including zero) take up space between the
    distributor and redistributor.
    
    So while those two adjustments are done for independent reasons, the
    code for doing so is very similar, so we should utilise the existing
    fdt_adjust_gic_redist() function.
    
    Add an (optional) gicr_base parameters to the prototype, so callers can
    choose to also adjust this base address later, if needed.
    
    Change-Id: Id39c0ba83e7401fdff1944e86950bb7121f210e8
    Signed-off-by: Andre Przywara <andre.przywara@arm.com>

--- include/common/fdt_fixup.h
+++ include/common/fdt_fixup.h
@@ -7,13 +7,15 @@
 #ifndef FDT_FIXUP_H
 #define FDT_FIXUP_H
 
+#define INVALID_BASE_ADDR	((uintptr_t)~0UL)
+
 int dt_add_psci_node(void *fdt);
 int dt_add_psci_cpu_enable_methods(void *fdt);
 int fdt_add_reserved_memory(void *dtb, const char *node_name,
 			    uintptr_t base, size_t size);
 int fdt_add_cpus_node(void *dtb, unsigned int afflv0,
 		      unsigned int afflv1, unsigned int afflv2);
-int fdt_adjust_gic_redist(void *dtb, unsigned int nr_cores,
+int fdt_adjust_gic_redist(void *dtb, unsigned int nr_cores, uintptr_t gicr_base,
 			  unsigned int gicr_frame_size);
 
 #endif /* FDT_FIXUP_H */
