bfe8a12eea3d51c07570cce65ea7a290db0ab9ce
Pascal Paillet <p.paillet@st.com>
2021-07-06 18:08:32 +0200

    feat(st-clock): add function to restore generic timer rate
    Add a function to restore the CPU generic timer rate from STGEN content.
    After wake-up from LPLV-Stop2, STGEN content is not lost, but generic timer
    has been reset.
    
    Signed-off-by: Pascal Paillet <p.paillet@st.com>
    Change-Id: I6f91dbd051f76383e9ff1d6bb86225d373dbf33a

--- drivers/st/clk/stm32mp_clkfunc.c
+++ drivers/st/clk/stm32mp_clkfunc.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017-2023, STMicroelectronics - All Rights Reserved
+ * Copyright (c) 2017-2024, STMicroelectronics - All Rights Reserved
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
@@ -360,6 +360,18 @@ void stm32mp_stgen_config(unsigned long rate)
 
 	/* Need to update timer with new frequency */
 	generic_delay_timer_init();
+}
+
+/*******************************************************************************
+ * This function restores CPU generic timer rate from the STGEN clock rate.
+ ******************************************************************************/
+void stm32mp_stgen_restore_rate(void)
+{
+	unsigned long rate;
+
+	rate = mmio_read_32(STGEN_BASE + CNTFID_OFF);
+
+	write_cntfrq_el0((u_register_t)rate);
 }
 
 /*******************************************************************************
