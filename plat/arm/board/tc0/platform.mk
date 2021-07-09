b5863cab9adb3fed0c1e4dfb92cf906794e7bdb4
Arunachalam Ganapathy <arunachalam.ganapathy@arm.com>
2021-07-09 13:43:01 +0100

    fix(plat/tc0): enable AMU extension
    Recent changes to enable SVE for the secure world have disabled AMU
    extension by default in the reset value of CPTR_EL3 register. So the
    platform has to enable this extension explicitly.
    
    Signed-off-by: Arunachalam Ganapathy <arunachalam.ganapathy@arm.com>
    Change-Id: I7d930d96ec22d7c3db961411370564bece0ce272

--- plat/arm/board/tc0/platform.mk
+++ plat/arm/board/tc0/platform.mk
@@ -114,6 +114,8 @@ override CTX_INCLUDE_PAUTH_REGS	:= 1
 
 override ENABLE_SPE_FOR_LOWER_ELS	:= 0
 
+override ENABLE_AMU := 1
+
 include plat/arm/common/arm_common.mk
 include plat/arm/css/common/css_common.mk
 include plat/arm/soc/common/soc_css.mk
