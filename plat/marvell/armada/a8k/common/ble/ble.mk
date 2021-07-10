559ab2df4a35cd82b2a67a0bebeb3028544a6766
Pali Rohár <pali@kernel.org>
2021-07-10 17:05:53 +0200

    fix(plat/marvell/a8k): Correctly set include directories for individual targets
    Do not set all include directories, including those for external targets
    in one PLAT_INCLUDES variable.
    
    Instead split them into variables:
    * $(PLAT_INCLUDES) for all TF-A BL images
    * BLE target specific $(PLAT_INCLUDES) only for Marvell BLE image
    * $(MV_DDR_INCLUDES) for targets in external Marvell mv-ddr-marvell tree
    
    Include directory $(CURDIR)/drivers/marvell is required by TF-A BL
    images, so move it from ble.mk to a8k_common.mk.
    
    Include directory $(MV_DDR_PATH) is needed only by Marvell BLE image, so
    move it into BLE target specific $(PLAT_INCLUDES) variable.
    
    And remaining include directories specified in ble.mk are needed only
    for building external dependences from Marvell mv-ddr tree, so move them
    into $(MV_DDR_INCLUDES) variable and correctly use it in $(MV_DDR_LIB)
    target.
    
    Signed-off-by: Pali Rohár <pali@kernel.org>
    Change-Id: I331f7de675dca2bc70733d56b768f00d56ae4a67

--- plat/marvell/armada/a8k/common/ble/ble.mk
+++ plat/marvell/armada/a8k/common/ble/ble.mk
@@ -16,17 +16,18 @@ BLE_SOURCES		+= 	$(BLE_PATH)/ble_main.c				\
 				$(PLAT_MARVELL)/common/plat_delay_timer.c	\
 				$(PLAT_MARVELL)/common/marvell_console.c
 
-PLAT_INCLUDES		+= 	-I$(MV_DDR_PATH)				\
-				-I$(CURDIR)/include				\
+MV_DDR_INCLUDES		:=	-I$(CURDIR)/include				\
 				-I$(CURDIR)/include/arch/aarch64		\
 				-I$(CURDIR)/include/lib/libc			\
-				-I$(CURDIR)/include/lib/libc/aarch64		\
-				-I$(CURDIR)/drivers/marvell
+				-I$(CURDIR)/include/lib/libc/aarch64
 
 BLE_LINKERFILE		:=	$(BLE_PATH)/ble.ld.S
+
+BLE_OBJS := $(addprefix $(BUILD_PLAT)/ble/,$(call SOURCES_TO_OBJS,$(BLE_SOURCES)))
+$(BLE_OBJS): PLAT_INCLUDES += -I$(MV_DDR_PATH)
 
 $(MV_DDR_LIB): FORCE
 	$(if $(value MV_DDR_PATH),,$(error "Platform '$(PLAT)' for BLE requires MV_DDR_PATH. Please set MV_DDR_PATH to point to the right directory"))
 	$(if $(wildcard $(value MV_DDR_PATH)/*),,$(error "'MV_DDR_PATH=$(value MV_DDR_PATH)' was specified, but '$(value MV_DDR_PATH)' directory does not exist"))
 	$(if $(shell git -C $(value MV_DDR_PATH) rev-parse --show-cdup 2>&1),$(error "'MV_DDR_PATH=$(value MV_DDR_PATH)' was specified, but '$(value MV_DDR_PATH)' does not contain valid mv-ddr-marvell git repository"))
-	@+make -C $(MV_DDR_PATH) --no-print-directory PLAT_INCLUDES="$(PLAT_INCLUDES)" PLATFORM=$(PLAT) ARCH=AARCH64 OBJ_DIR=$(BUILD_PLAT)/ble
+	@+make -C $(MV_DDR_PATH) --no-print-directory PLAT_INCLUDES="$(MV_DDR_INCLUDES)" PLATFORM=$(PLAT) ARCH=AARCH64 OBJ_DIR=$(BUILD_PLAT)/ble
