c77ab18ec7c8e0f3d953177b835e004a9b53515f
Toshiyuki Ogasahara <toshiyuki.ogasahara.bo@hitachi.com>
2021-07-13 14:20:57 +0900

    fix(plat/rcar): fix cache maintenance process of reading cert header
    Move calling inv_dcache_range from before io_read to after that.
    
    Signed-off-by: Hideyuki Nitta <hideyuki.nitta.jf@hitachi.com>
    Signed-off-by: Toshiyuki Ogasahara <toshiyuki.ogasahara.bo@hitachi.com>
    Signed-off-by: Yoshifumi Hosoya <yoshifumi.hosoya.wj@renesas.com>
    Change-Id: Ic0dabf3eb94eeeb04099ed5127cdfda79bbac9b3

--- drivers/renesas/common/io/io_rcar.c
+++ drivers/renesas/common/io/io_rcar.c
@@ -423,14 +423,16 @@ static int32_t rcar_dev_init(io_dev_info_t *dev_info, const uintptr_t name)
 		WARN("Firmware Image Package header failed to seek\n");
 		goto error;
 	}
-#if RCAR_BL2_DCACHE == 1
-	inv_dcache_range((uint64_t) header, sizeof(header));
-#endif
+
 	rc = io_read(handle, (uintptr_t) &header, sizeof(header), &cnt);
 	if (rc != IO_SUCCESS) {
 		WARN("Firmware Image Package header failed to read\n");
 		goto error;
 	}
+
+#if RCAR_BL2_DCACHE == 1
+	inv_dcache_range((uint64_t) header, sizeof(header));
+#endif
 
 	rcar_image_number = header[0];
 	for (i = 0; i < rcar_image_number + 2; i++) {
@@ -449,16 +451,18 @@ static int32_t rcar_dev_init(io_dev_info_t *dev_info, const uintptr_t name)
 		WARN("Firmware Image Package header failed to seek cert\n");
 		goto error;
 	}
-#if RCAR_BL2_DCACHE == 1
-	inv_dcache_range(RCAR_SDRAM_certESS,
-			 RCAR_CERT_SIZE * (2 + rcar_image_number));
-#endif
+
 	rc = io_read(handle, RCAR_SDRAM_certESS,
 		     RCAR_CERT_SIZE * (2 + rcar_image_number), &cnt);
 	if (rc != IO_SUCCESS) {
 		WARN("cert file read error.\n");
 		goto error;
 	}
+
+#if RCAR_BL2_DCACHE == 1
+	inv_dcache_range(RCAR_SDRAM_certESS,
+			 RCAR_CERT_SIZE * (2 + rcar_image_number));
+#endif
 
 	rcar_cert_load = RCAR_CERT_LOAD;
 error:
