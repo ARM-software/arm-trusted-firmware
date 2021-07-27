47d6f5ff16d1f2ad009d630a381054b10fa0a06f
Varun Wadekar <vwadekar@nvidia.com>
2021-07-27 02:32:29 -0700

    feat(cpus): workaround for Cortex A78 AE erratum 1941500
    Cortex A78 AE erratum 1941500 is a Cat B erratum that applies
    to revisions <= r0p1. It is still open.
    
    This erratum is avoided by by setting CPUECTLR_EL1[8] to 1.
    There is a small performance cost (<0.5%) for setting this
    bit.
    
    SDEN is available at https://developer.arm.com/documentation/SDEN1707912/0900
    
    Change-Id: I2d72666468b146714a0340ba114ccf0f5165b39c
    Signed-off-by: Varun Wadekar <vwadekar@nvidia.com>

--- include/lib/cpus/aarch64/cortex_a78_ae.h
+++ include/lib/cpus/aarch64/cortex_a78_ae.h
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2019-2020, ARM Limited. All rights reserved.
+ * Copyright (c) 2021, NVIDIA Corporation. All rights reserved.
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
@@ -10,5 +11,11 @@
 #include <cortex_a78.h>
 
 #define CORTEX_A78_AE_MIDR U(0x410FD420)
+
+/*******************************************************************************
+ * CPU Extended Control register specific definitions.
+ ******************************************************************************/
+#define CORTEX_A78_AE_CPUECTLR_EL1			CORTEX_A78_CPUECTLR_EL1
+#define CORTEX_A78_AE_CPUECTLR_EL1_BIT_8		CORTEX_A78_CPUECTLR_EL1_BIT_8
 
 #endif /* CORTEX_A78_AE_H */
