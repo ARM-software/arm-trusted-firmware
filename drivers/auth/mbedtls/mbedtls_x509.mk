92b80d49101bee5cd61f54de1a1c17044bcca2a4
removedporn <86824510+removedporn@users.noreply.github.com>
2021-08-18 21:59:48 +0800

    Delete drivers directory

--- drivers/auth/mbedtls/mbedtls_x509.mk
+++
@@ -1,9 +0,0 @@
-#
-# Copyright (c) 2015, ARM Limited and Contributors. All rights reserved.
-#
-# SPDX-License-Identifier: BSD-3-Clause
-#
-
-include drivers/auth/mbedtls/mbedtls_common.mk
-
-MBEDTLS_SOURCES	+=	drivers/auth/mbedtls/mbedtls_x509_parser.c
--- drivers/renesas/common/ddr/ddr_a/boot_init_dram_regdef.h
+++
@@ -1,8 +0,0 @@
-/*
- * Copyright (c) 2015-2019, Renesas Electronics Corporation
- * All rights reserved.
- *
- * SPDX-License-Identifier: BSD-3-Clause
- */
-
-#include "../ddr_regs.h"
