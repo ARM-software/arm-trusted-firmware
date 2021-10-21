8bbb1d80a58dbdf96fcabbdebbfbd21d2d5344a4
Jiafei Pan <Jiafei.Pan@nxp.com>
2021-10-21 15:02:08 +0800

    feat(cpu/cortex_a53): add L1PCTL macro definiton for CPUACTLR_EL1
    Add L1PCTL field definiton in register CPUACTLR_EL1.
    
    Signed-off-by: Jiafei Pan <Jiafei.Pan@nxp.com>
    Change-Id: Iebfb240ac58aa8f3dc870804bf4390dfbdfa9b95

--- include/lib/cpus/aarch64/cortex_a53.h
+++ include/lib/cpus/aarch64/cortex_a53.h
@@ -52,6 +52,8 @@
 #define CORTEX_A53_CPUACTLR_EL1_L1RADIS			(ULL(3) << CORTEX_A53_CPUACTLR_EL1_L1RADIS_SHIFT)
 #define CORTEX_A53_CPUACTLR_EL1_DTAH_SHIFT		U(24)
 #define CORTEX_A53_CPUACTLR_EL1_DTAH			(ULL(1) << CORTEX_A53_CPUACTLR_EL1_DTAH_SHIFT)
+#define CORTEX_A53_CPUACTLR_EL1_L1PCTL_SHIFT		U(13)
+#define CORTEX_A53_CPUACTLR_EL1_L1PCTL			(ULL(7) << CORTEX_A53_CPUACTLR_EL1_L1PCTL_SHIFT)
 
 /*******************************************************************************
  * L2 Auxiliary Control register specific definitions.
