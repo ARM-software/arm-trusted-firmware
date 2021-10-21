3ccd7e45a2c3ff9fa7794f0284c9d0298e7cb982
Jiafei Pan <Jiafei.Pan@nxp.com>
2021-10-21 17:03:04 +0800

    fix(nxp/common/setup): increase soc name maximum length
    Increate SoC name length as it is not enough for some
    SoC personalities.
    
    Signed-off-by: Biwen Li <biwen.li@nxp.com>
    Signed-off-by: Jiafei Pan <Jiafei.Pan@nxp.com>
    Change-Id: I2142b4b5162dd3c9ab3afefcdc859063836d8bcc

--- plat/nxp/common/setup/include/plat_common.h
+++ plat/nxp/common/setup/include/plat_common.h
@@ -131,9 +131,11 @@ void ls_setup_page_tables(uintptr_t total_base,
 #endif
 );
 
+#define SOC_NAME_MAX_LEN	(20)
+
 /* Structure to define SoC personality */
 struct soc_type {
-	char name[10];
+	char name[SOC_NAME_MAX_LEN];
 	uint32_t version;
 	uint8_t num_clusters;
 	uint8_t cores_per_cluster;
