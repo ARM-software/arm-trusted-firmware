635e6b108e773daf37c00f46e6fbb1cae4e78f96
jason-ch chen <jason-ch.chen@mediatek.corp-partner.google.com>
2021-11-16 10:18:46 +0800

    feat(mt8186): add Vcore DVFS driver
    Add Vcore DVFS to SPM driver.
    
    TEST=build pass
    BUG=b:202871018
    
    Signed-off-by: Jason-ch Chen <jason-ch.chen@mediatek.com>
    Change-Id: I52b241b2cdb792be74390cbaa09a728ddbe6593a

--- plat/mediatek/mt8186/plat_sip_calls.c
+++ plat/mediatek/mt8186/plat_sip_calls.c
@@ -1,11 +1,14 @@
 /*
- * Copyright (c) 2021, MediaTek Inc. All rights reserved.
+ * Copyright (c) 2021-2022, MediaTek Inc. All rights reserved.
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
 #include <common/debug.h>
 #include <common/runtime_svc.h>
+#include <mt_spm_vcorefs.h>
+#include <mtk_sip_svc.h>
+#include "plat_sip_calls.h"
 
 uintptr_t mediatek_plat_sip_handler(uint32_t smc_fid,
 				u_register_t x1,
@@ -16,7 +19,14 @@ uintptr_t mediatek_plat_sip_handler(uint32_t smc_fid,
 				void *handle,
 				u_register_t flags)
 {
+	uint64_t ret;
+
 	switch (smc_fid) {
+	case MTK_SIP_VCORE_CONTROL_ARCH32:
+	case MTK_SIP_VCORE_CONTROL_ARCH64:
+		ret = spm_vcorefs_args(x1, x2, x3, (uint64_t *)&x4);
+		SMC_RET2(handle, ret, x4);
+		break;
 	default:
 		ERROR("%s: unhandled SMC (0x%x)\n", __func__, smc_fid);
 		break;
