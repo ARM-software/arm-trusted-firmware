9b8c431e2b2d656da7f8c4158e3d32e104446fec
Chandni Cherukuri <chandni.cherukuri@arm.com>
2021-11-30 20:35:35 +0530

    feat(morello): configure DMC-Bing mode
    Based on the SCC configuration value obtained from the SDS
    platform information structure configure DMC-Bing Server or
    Client mode after zeroing out the memory.
    
    Signed-off-by: Chandni Cherukuri <chandni.cherukuri@arm.com>
    Change-Id: I0555fa06c9c1906264848f4e32ca413b4742cdee

--- plat/arm/board/morello/morello_bl31_setup.c
+++ plat/arm/board/morello/morello_bl31_setup.c
@@ -23,12 +23,14 @@
  *	- Remote DDR size in bytes, DDR memory in slave board
  *	- slave_count
  *	- multichip mode
+ *	- scc configuration
  */
 struct morello_plat_info {
 	uint64_t local_ddr_size;
 	uint64_t remote_ddr_size;
 	uint8_t slave_count;
 	bool multichip_mode;
+	uint32_t scc_config;
 } __packed;
 
 /* Compile time assertion to ensure the size of structure is 18 bytes */
@@ -79,6 +81,8 @@ static void dmc_ecc_setup(struct morello_plat_info *plat_info)
 {
 	uint64_t dram2_size;
 	uint32_t val;
+	uint64_t tag_mem_base;
+	uint64_t usable_mem_size;
 
 	INFO("Total DIMM size: %uGB\n",
 			(uint32_t)(plat_info->local_ddr_size / 0x40000000));
@@ -113,6 +117,63 @@ static void dmc_ecc_setup(struct morello_plat_info *plat_info)
 			MORELLO_DMC_MEMC_STATUS_MASK) !=
 			MORELLO_DMC_MEMC_CMD_CONFIG) {
 		continue;
+	}
+
+	/* Configure Bing client/server mode based on SCC configuration */
+	if (plat_info->scc_config & MORELLO_SCC_CLIENT_MODE_MASK) {
+		INFO("Configuring DMC Bing in client mode\n");
+		usable_mem_size = plat_info->local_ddr_size -
+			(plat_info->local_ddr_size / 128ULL);
+
+		/* Linear DDR address */
+		tag_mem_base = usable_mem_size;
+		tag_mem_base = tag_mem_base / 4;
+
+		/* Reverse translation */
+		if (tag_mem_base < ARM_DRAM1_BASE) {
+			tag_mem_base += ARM_DRAM1_BASE;
+		} else {
+			tag_mem_base = tag_mem_base - ARM_DRAM1_BASE +
+				ARM_DRAM2_BASE;
+		}
+
+		mmio_write_32(MORELLO_DMC0_CAP_CTRL_REG, 0x1);
+		mmio_write_32(MORELLO_DMC1_CAP_CTRL_REG, 0x1);
+		mmio_write_32(MORELLO_DMC0_TAG_CACHE_CFG, 0x1);
+		mmio_write_32(MORELLO_DMC1_TAG_CACHE_CFG, 0x1);
+
+		if (plat_info->scc_config & MORELLO_SCC_C1_TAG_CACHE_EN_MASK) {
+			mmio_setbits_32(MORELLO_DMC0_TAG_CACHE_CFG, 0x2);
+			mmio_setbits_32(MORELLO_DMC1_TAG_CACHE_CFG, 0x2);
+			INFO("C1 Tag Cache Enabled\n");
+		}
+
+		if (plat_info->scc_config & MORELLO_SCC_C2_TAG_CACHE_EN_MASK) {
+			mmio_setbits_32(MORELLO_DMC0_TAG_CACHE_CFG, 0x4);
+			mmio_setbits_32(MORELLO_DMC1_TAG_CACHE_CFG, 0x4);
+			INFO("C2 Tag Cache Enabled\n");
+		}
+
+		mmio_write_32(MORELLO_DMC0_MEM_ADDR_CTL,
+				(uint32_t)tag_mem_base);
+		mmio_write_32(MORELLO_DMC1_MEM_ADDR_CTL,
+				(uint32_t)tag_mem_base);
+		mmio_write_32(MORELLO_DMC0_MEM_ADDR_CTL2,
+				(uint32_t)(tag_mem_base >> 32));
+		mmio_write_32(MORELLO_DMC1_MEM_ADDR_CTL2,
+				(uint32_t)(tag_mem_base >> 32));
+
+		mmio_setbits_32(MORELLO_DMC0_MEM_ACCESS_CTL,
+				MORELLO_DMC_MEM_ACCESS_DIS);
+		mmio_setbits_32(MORELLO_DMC1_MEM_ACCESS_CTL,
+				MORELLO_DMC_MEM_ACCESS_DIS);
+
+		INFO("Tag base set to 0x%lx\n", tag_mem_base);
+		plat_info->local_ddr_size = usable_mem_size;
+	} else {
+		INFO("Configuring DMC Bing in server mode\n");
+		mmio_write_32(MORELLO_DMC0_CAP_CTRL_REG, 0x0);
+		mmio_write_32(MORELLO_DMC1_CAP_CTRL_REG, 0x0);
 	}
 
 	INFO("Enabling ECC on DMCs\n");
