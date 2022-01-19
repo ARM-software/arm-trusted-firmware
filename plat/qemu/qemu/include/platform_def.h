0e6977eee178a6436e4a7e1503ea854989316ff4
Jens Wiklander <jens.wiklander@linaro.org>
2022-01-19 22:01:07 +0100

    feat(qemu): increase size of bl31
    Increases the SRAM to a full 1MB and also increase BL31 size to have
    room to spare for debugging.
    
    Signed-off-by: Jens Wiklander <jens.wiklander@linaro.org>
    Change-Id: I584f9d409a1f653a3dfc7cf2b95706ada367c70e

--- plat/qemu/qemu/include/platform_def.h
+++ plat/qemu/qemu/include/platform_def.h
@@ -84,7 +84,7 @@
 #define NS_DRAM0_SIZE			ULL(0xc0000000)
 
 #define SEC_SRAM_BASE			0x0e000000
-#define SEC_SRAM_SIZE			0x00060000
+#define SEC_SRAM_SIZE			0x00100000
 
 #define SEC_DRAM_BASE			0x0e100000
 #define SEC_DRAM_SIZE			0x00f00000
@@ -146,7 +146,7 @@
  * Put BL3-1 at the top of the Trusted SRAM. BL31_BASE is calculated using the
  * current BL3-1 debug size plus a little space for growth.
  */
-#define BL31_BASE			(BL31_LIMIT - 0x20000)
+#define BL31_BASE			(BL31_LIMIT - 0x60000)
 #define BL31_LIMIT			(BL_RAM_BASE + BL_RAM_SIZE)
 #define BL31_PROGBITS_LIMIT		BL1_RW_BASE
 
