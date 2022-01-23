2b2b565717cc0299e75e8806004d1a3548e9fbf7
Samuel Holland <samuel@sholland.org>
2022-01-23 17:12:26 -0600

    feat(fdt): add the ability to supply idle state information
    Some platforms require extra firmware to implement CPU_SUSPEND, or only
    have working CPU_SUSPEND in certain configurations. On these platforms,
    CPU idle states should only be listed in the devicetree when they are
    actually available. Add a function BL31 can use to dynamically supply
    this idle state information.
    
    Change-Id: I64fcc288303faba8abec4f59efd13a04220d54dc
    Signed-off-by: Samuel Holland <samuel@sholland.org>

--- include/common/fdt_fixup.h
+++ include/common/fdt_fixup.h
@@ -7,7 +7,21 @@
 #ifndef FDT_FIXUP_H
 #define FDT_FIXUP_H
 
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+
 #define INVALID_BASE_ADDR	((uintptr_t)~0UL)
+
+struct psci_cpu_idle_state {
+	const char *name;
+	uint32_t power_state;
+	bool local_timer_stop;
+	uint32_t entry_latency_us;
+	uint32_t exit_latency_us;
+	uint32_t min_residency_us;
+	uint32_t wakeup_latency_us;
+};
 
 int dt_add_psci_node(void *fdt);
 int dt_add_psci_cpu_enable_methods(void *fdt);
@@ -15,6 +29,7 @@ int fdt_add_reserved_memory(void *dtb, const char *node_name,
 			    uintptr_t base, size_t size);
 int fdt_add_cpus_node(void *dtb, unsigned int afflv0,
 		      unsigned int afflv1, unsigned int afflv2);
+int fdt_add_cpu_idle_states(void *dtb, const struct psci_cpu_idle_state *state);
 int fdt_adjust_gic_redist(void *dtb, unsigned int nr_cores, uintptr_t gicr_base,
 			  unsigned int gicr_frame_size);
 
