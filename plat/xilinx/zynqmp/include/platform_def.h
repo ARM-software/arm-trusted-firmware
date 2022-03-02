4c4b9615b1d9512a4a89aa08e722547cc491a07b
Venkatesh Yadav Abbarapu <venkatesh.abbarapu@xilinx.com>
2022-03-01 22:10:05 -0700

    feat(zynqmp): increase the max xlat tables when debug build is enabled
    Update the MAX_XLAT_TABLES as the memory map has been
    added for the dtb to accomodate in DDR address.
    
    Signed-off-by: Venkatesh Yadav Abbarapu <venkatesh.abbarapu@xilinx.com>
    Change-Id: I46225673f40f123cdab38efefb038604da119b58

--- plat/xilinx/zynqmp/include/platform_def.h
+++ plat/xilinx/zynqmp/include/platform_def.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014-2020, ARM Limited and Contributors. All rights reserved.
+ * Copyright (c) 2014-2022, ARM Limited and Contributors. All rights reserved.
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
@@ -91,10 +91,11 @@
 #define PLAT_VIRT_ADDR_SPACE_SIZE	(1ULL << 32)
 #if (BL31_LIMIT < PLAT_DDR_LOWMEM_MAX)
 #define MAX_MMAP_REGIONS		8
+#define MAX_XLAT_TABLES			6
 #else
 #define MAX_MMAP_REGIONS		7
-#endif
 #define MAX_XLAT_TABLES			5
+#endif
 
 #define CACHE_WRITEBACK_SHIFT   6
 #define CACHE_WRITEBACK_GRANULE (1 << CACHE_WRITEBACK_SHIFT)
