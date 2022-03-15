39f0b86a76534d0b7c71dd0c8b34f1a74480386b
Manish V Badarkhe <Manish.Badarkhe@arm.com>
2022-03-15 16:05:58 +0000

    feat(fvp): update HW_CONFIG DT loading mechanism
    Currently, HW-config is loaded into non-secure memory, which mean
    a malicious NS-agent could tamper with it. Ideally, this shouldn't
    be an issue since no software runs in non-secure world at this time
    (non-secure world has not been started yet).
    
    It does not provide a guarantee though since malicious external
    NS-agents can take control of this memory region for update/corruption
    after BL2 loads it and before BL31/BL32/SP_MIN consumes it. The threat
    is mapped to Threat ID#3 (Bypass authentication scenario) in threat
    model [1].
    
    Hence modified the code as below -
    1. BL2 loads the HW_CONFIG into secure memory
    2. BL2 makes a copy of the HW_CONFIG in the non-secure memory at an
       address provided by the newly added property(ns-load-address) in
       the 'hw-config' node of the FW_CONFIG
    3. SP_MIN receives the FW_CONFIG address from BL2 via arg1 so that
       it can retrieve details (address and size) of HW_CONFIG from
       FW_CONFIG
    4. A secure and non-secure HW_CONFIG address will eventually be used
       by BL31/SP_MIN/BL32 and BL33 components respectively
    5. BL31/SP_MIN dynamically maps the Secure HW_CONFIG region and reads
       information from it to local variables (structures) and then
       unmaps it
    6. Reduce HW_CONFIG maximum size from 16MB to 1MB; it appears
       sufficient, and it will also create a free space for any future
       components to be added to memory
    
    [1]: https://trustedfirmware-a.readthedocs.io/en/latest/threat_model/threat_model.html
    
    Change-Id: I1d431f3e640ded60616604b1c33aa638b9a1e55e
    Signed-off-by: Manish V Badarkhe <Manish.Badarkhe@arm.com>

--- plat/arm/board/fvp/fvp_bl2_setup.c
+++ plat/arm/board/fvp/fvp_bl2_setup.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013-2021, Arm Limited and Contributors. All rights reserved.
+ * Copyright (c) 2013-2022, Arm Limited and Contributors. All rights reserved.
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
@@ -40,17 +40,23 @@ void bl2_platform_setup(void)
 struct bl_params *plat_get_next_bl_params(void)
 {
 	struct bl_params *arm_bl_params;
+	const struct dyn_cfg_dtb_info_t *hw_config_info __unused;
+	bl_mem_params_node_t *param_node __unused;
 
 	arm_bl_params = arm_get_next_bl_params();
 
-#if __aarch64__ && !BL2_AT_EL3
+#if !BL2_AT_EL3 && !EL3_PAYLOAD_BASE
 	const struct dyn_cfg_dtb_info_t *fw_config_info;
-	bl_mem_params_node_t *param_node;
-	uintptr_t fw_config_base = 0U;
+	uintptr_t fw_config_base = 0UL;
 	entry_point_info_t *ep_info;
 
+#if __aarch64__
 	/* Get BL31 image node */
 	param_node = get_bl_mem_params_node(BL31_IMAGE_ID);
+#else /* aarch32 */
+	/* Get SP_MIN image node */
+	param_node = get_bl_mem_params_node(BL32_IMAGE_ID);
+#endif /* __aarch64__ */
 	assert(param_node != NULL);
 
 	/* get fw_config load address */
@@ -58,15 +64,41 @@ struct bl_params *plat_get_next_bl_params(void)
 	assert(fw_config_info != NULL);
 
 	fw_config_base = fw_config_info->config_addr;
-	assert(fw_config_base != 0U);
+	assert(fw_config_base != 0UL);
 
 	/*
-	 * Get the entry point info of BL31 image and override
+	 * Get the entry point info of next executable image and override
 	 * arg1 of entry point info with fw_config base address
 	 */
 	ep_info = &param_node->ep_info;
 	ep_info->args.arg1 = (uint32_t)fw_config_base;
-#endif /* __aarch64__ && !BL2_AT_EL3 */
+
+	/* grab NS HW config address */
+	hw_config_info = FCONF_GET_PROPERTY(dyn_cfg, dtb, HW_CONFIG_ID);
+
+	/* To retrieve actual size of the HW_CONFIG */
+	param_node = get_bl_mem_params_node(HW_CONFIG_ID);
+	assert(param_node != NULL);
+
+	/* Copy HW config from Secure address to NS address */
+	memcpy((void *)hw_config_info->ns_config_addr,
+	       (void *)hw_config_info->config_addr,
+	       (size_t)param_node->image_info.image_size);
+
+	/*
+	 * Ensure HW-config device tree committed to memory, as there is
+	 * a possibility to use HW-config without cache and MMU enabled
+	 * at BL33
+	 */
+	flush_dcache_range(hw_config_info->ns_config_addr,
+			   param_node->image_info.image_size);
+
+	param_node = get_bl_mem_params_node(BL33_IMAGE_ID);
+	assert(param_node != NULL);
+
+	/* Update BL33's ep info with NS HW config address  */
+	param_node->ep_info.args.arg1 = hw_config_info->ns_config_addr;
+#endif /* !BL2_AT_EL3 && !EL3_PAYLOAD_BASE */
 
 	return arm_bl_params;
 }
