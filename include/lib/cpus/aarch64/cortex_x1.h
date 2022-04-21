6e8eca78e5db966e10e2fa2737e9be4d5af51fa9
Okash Khawaja <okash@google.com>
2022-04-21 10:59:34 +0100

    feat(cpu): add support for Cortex-X1
    This patch adds basic CPU library code to support Cortex-X1 CPU in TF-A.
    Follow-up patches will add selected errata workarounds for this CPU.
    
    Signed-off-by: Okash Khawaja <okash@google.com>
    Change-Id: I4a3d50a98bf55a555bfaefeed5c7b88a35e3bc21

---
+++ include/lib/cpus/aarch64/cortex_x1.h
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2022, Google LLC. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef CORTEX_X1_H
+#define CORTEX_X1_H
+
+/* Cortex-X1 MIDR for r1p0 */
+#define CORTEX_X1_MIDR			U(0x411fd440)
+
+/*******************************************************************************
+ * CPU Extended Control register specific definitions.
+ ******************************************************************************/
+#define CORTEX_X1_CPUECTLR_EL1		S3_0_C15_C1_4
+
+/*******************************************************************************
+ * CPU Power Control register specific definitions
+ ******************************************************************************/
+#define CORTEX_X1_CPUPWRCTLR_EL1	S3_0_C15_C2_7
+#define CORTEX_X1_CORE_PWRDN_EN_MASK	U(0x1)
+
+#endif /* CORTEX_X1_H */
