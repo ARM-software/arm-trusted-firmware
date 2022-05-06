2deff904a953c6a87331ab6830ab80e3889d9e23
Yann Gautier <yann.gautier@st.com>
2022-05-06 09:50:43 +0200

    fix(st): fix NULL pointer dereference issues
    The get_bl_mem_params_node() function could return NULL. Add asserts to
    check the return value is not NULL.
    This corrects coverity issues:
    	pager_mem_params = get_bl_mem_params_node(BL32_EXTRA1_IMAGE_ID);
    >>>     CID 378360:    (NULL_RETURNS)
    >>>     Dereferencing "pager_mem_params", which is known to be "NULL".
    
    	paged_mem_params = get_bl_mem_params_node(BL32_EXTRA2_IMAGE_ID);
    >>>     CID 378360:    (NULL_RETURNS)
    >>>     Dereferencing "paged_mem_params", which is known to be "NULL".
    
    	tos_fw_mem_params = get_bl_mem_params_node(TOS_FW_CONFIG_ID);
    >>>     CID 378360:    (NULL_RETURNS)
    >>>     Dereferencing "tos_fw_mem_params", which is known to be "NULL".
    
    
    Do the same for other occurrences of get_bl_mem_params_node() return not
    checked, in the functions plat_get_bl_image_load_info() and
    bl2_plat_handle_pre_image_load().
    
    Signed-off-by: Yann Gautier <yann.gautier@st.com>
    Change-Id: I79165b1628fcee3da330f2db4ee5e1dafcb1b21f

--- plat/st/stm32mp1/plat_image_load.c
+++ plat/st/stm32mp1/plat_image_load.c
@@ -1,13 +1,13 @@
 /*
- * Copyright (c) 2016-2021, ARM Limited and Contributors. All rights reserved.
+ * Copyright (c) 2016-2022, ARM Limited and Contributors. All rights reserved.
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
-#include <platform_def.h>
-
 #include <common/desc_image_load.h>
 #include <plat/common/platform.h>
+
+#include <platform_def.h>
 
 /*******************************************************************************
  * This function flushes the data structures so that they are visible
@@ -26,6 +26,8 @@ bl_load_info_t *plat_get_bl_image_load_info(void)
 #if STM32MP_USE_STM32IMAGE
 	bl_mem_params_node_t *bl33 = get_bl_mem_params_node(BL33_IMAGE_ID);
 	uint32_t ddr_ns_size = stm32mp_get_ddr_ns_size();
+
+	assert(bl33 != NULL);
 
 	/* Max size is non-secure DDR end address minus image_base */
 	bl33->image_info.image_max_size = STM32MP_DDR_BASE + ddr_ns_size -
