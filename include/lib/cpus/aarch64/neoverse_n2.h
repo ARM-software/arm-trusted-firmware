884d515625aa09b22245c32db2fcc9222c7f34fd
Daniel Boulby <daniel.boulby@arm.com>
2022-07-06 14:33:13 +0100

    fix(cpus): workaround for Neoverse-N2 erratum 2388450
    Neoverse-N2 erratum 2388450 is a cat B erratum that applies to
    revision r0p0 and is fixed in r0p1. The workaround is to set
    bit[40] of CPUACTLR2_EL1 to disable folding of demand requests into
    older prefetches with L2 miss requests outstanding.
    
    SDEN can be found here:
    https://developer.arm.com/documentation/SDEN1982442/latest
    
    Change-Id: I6dd949c79cea8dbad322e569aa5de86cf8cf9639
    Signed-off-by: Daniel Boulby <daniel.boulby@arm.com>

--- include/lib/cpus/aarch64/neoverse_n2.h
+++ include/lib/cpus/aarch64/neoverse_n2.h
@@ -38,6 +38,7 @@
  ******************************************************************************/
 #define NEOVERSE_N2_CPUACTLR2_EL1			S3_0_C15_C1_1
 #define NEOVERSE_N2_CPUACTLR2_EL1_BIT_2			(ULL(1) << 2)
+#define NEOVERSE_N2_CPUACTLR2_EL1_BIT_40		(ULL(1) << 40)
 
 /*******************************************************************************
  * CPU Auxiliary Control register 5 specific definitions.
