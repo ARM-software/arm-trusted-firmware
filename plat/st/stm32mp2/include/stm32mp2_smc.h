f55b136abcdd2d6ba3bd5613eee8fff943de0fd1
Gatien Chevallier <gatien.chevallier@foss.st.com>
2022-07-27 17:57:35 +0200

    feat(stm32mp2): add common SMC runtime services
    Implement the common SMC runtime services for stm32mp2 platforms.
    
    Signed-off-by: Gatien Chevallier <gatien.chevallier@foss.st.com>
    Signed-off-by: Maxime Méré <maxime.mere@foss.st.com>
    Change-Id: I214e4b2bfba439572c079bbc9ffb62bc87793ce9

---
+++ plat/st/stm32mp2/include/stm32mp2_smc.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2024, STMicroelectronics - All Rights Reserved
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef STM32MP2_SMC_H
+#define STM32MP2_SMC_H
+
+#define STM32_COMMON_SIP_NUM_CALLS			1U
+
+#endif /* STM32MP2_SMC_H */
