c58b9a8e12a0f0a9542d8ccec33f4ec2be44123d
Rupinderjit Singh <rupinderjit.singh@arm.com>
2022-08-23 11:55:27 +0100

    refactor(cpu): update IP names of Makalu CPU lib
       * ASM files are renamed to have public IP names in their filename.
       * updated other files to include ASM filename changes.
    
    Signed-off-by: Rupinderjit Singh <rupinderjit.singh@arm.com>
    Change-Id: Ie899c512b11fd7c4312e3a808bb6b9d2376cdb8c

--- plat/arm/board/fvp/platform.mk
+++ plat/arm/board/fvp/platform.mk
@@ -135,9 +135,9 @@ else
 					lib/cpus/aarch64/neoverse_demeter.S	\
 					lib/cpus/aarch64/cortex_a78_ae.S	\
 					lib/cpus/aarch64/cortex_a510.S		\
-					lib/cpus/aarch64/cortex_a710.S	\
-					lib/cpus/aarch64/cortex_makalu.S	\
-					lib/cpus/aarch64/cortex_makalu_elp_arm.S \
+					lib/cpus/aarch64/cortex_a710.S		\
+					lib/cpus/aarch64/cortex_a715.S		\
+					lib/cpus/aarch64/cortex_x3.S 		\
 					lib/cpus/aarch64/cortex_a65.S		\
 					lib/cpus/aarch64/cortex_a65ae.S		\
 					lib/cpus/aarch64/cortex_a78c.S		\
