24b5b53a5922de40e53f0a7ecf65d3d0acc30a0d
Rajan Vaja <rajan.vaja@xilinx.com>
2022-08-31 12:54:34 +0200

    fix(xilinx): update define for ZynqMP specific functions
    Instead of exclude code for Versal, define only for ZynqMP.
    For new platforms this code should be excluded so instead of
    excluding for all platform, define only for ZynqMP.
    
    Signed-off-by: Rajan Vaja <rajan.vaja@xilinx.com>
    Signed-off-by: Michal Simek <michal.simek@amd.com>
    Change-Id: I45798dadc0f374c5794f517f7d0158675a75caa9

--- plat/xilinx/common/include/pm_client.h
+++ plat/xilinx/common/include/pm_client.h
@@ -1,5 +1,7 @@
 /*
  * Copyright (c) 2013-2019, ARM Limited and Contributors. All rights reserved.
+ * Copyright (c) 2020-2022, Xilinx, Inc. All rights reserved.
+ * Copyright (c) 2022, Advanced Micro Devices, Inc. All rights reserved.
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
@@ -23,9 +25,9 @@ void pm_client_wakeup(const struct pm_proc *proc);
 /* Global variables to be set in pm_client.c */
 extern const struct pm_proc *primary_proc;
 
-#ifndef VERSAL_PLATFORM
+#if defined(PLAT_zynqmp)
 enum pm_ret_status pm_set_suspend_mode(uint32_t mode);
 const struct pm_proc *pm_get_proc_by_node(enum pm_node_id nid);
-#endif
+#endif /* PLAT_zynqmp */
 
 #endif /* PM_CLIENT_H */
