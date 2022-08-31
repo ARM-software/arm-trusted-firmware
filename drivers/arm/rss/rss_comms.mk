4348497acedef2c932df79af4a4f648658625b72
Sandrine Bailleux <sandrine.bailleux@arm.com>
2022-08-31 13:53:10 +0200

    build(rss): introduce rss_comms.mk makefile
    Provide a new makefile as a convenience for platform makefiles to pull
    in the list of source files and headers for the RSS communication
    driver.
    
    Change-Id: I188a1a8f4e77318cdc87c3155b280090c46ce813
    Signed-off-by: Sandrine Bailleux <sandrine.bailleux@arm.com>

---
+++ drivers/arm/rss/rss_comms.mk
@@ -0,0 +1,20 @@
+#
+# Copyright (c) 2022, Arm Limited. All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+#
+
+RSS_COMMS_SOURCES	:=	$(addprefix drivers/arm/rss/,			\
+					rss_comms.c				\
+					rss_comms_protocol.c			\
+					rss_comms_protocol_embed.c		\
+					rss_comms_protocol_pointer_access.c	\
+				)
+
+RSS_COMMS_SOURCES	+=	$(addprefix drivers/arm/mhu/,			\
+					mhu_v2_x.c				\
+					mhu_wrapper_v2_x.c			\
+				)
+
+PLAT_INCLUDES		+=	-Idrivers/arm/rss		\
+				-Idrivers/arm/mhu
