5a25a70fb67f56be5f3ebed3d4234bb3b8120181
Sona Mathew <SonaRebecca.Mathew@arm.com>
2023-01-11 12:55:30 -0600

    fix(cpus): workaround for Cortex-A78 erratum 2779479
    Cortex-A78 erratum 2779479 is a Cat B erratum that applies to
    all revisions <= r1p2 and is still open.
    
    The workaround is to set the CPUACTLR3_EL1[47] bit to 1. Setting this
    bit might have a small impact on power and negligible impact
    on performance.
    
    SDEN documentation:
    https://developer.arm.com/documentation/SDEN1401784/latest
    
    Change-Id: I3779fd1eff3017c5961ffa101b357918070b3b36
    Signed-off-by: Sona Mathew <SonaRebecca.Mathew@arm.com>
    (cherry picked from commit 7d1700c4d475358539c9a84cb325183c86a06f33)

--- include/lib/cpus/aarch64/cortex_a78.h
+++ include/lib/cpus/aarch64/cortex_a78.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019-2022, ARM Limited. All rights reserved.
+ * Copyright (c) 2019-2023, ARM Limited. All rights reserved.
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
@@ -39,6 +39,8 @@
 #define CORTEX_A78_ACTLR2_EL1_BIT_1			(ULL(1) << 1)
 #define CORTEX_A78_ACTLR2_EL1_BIT_2			(ULL(1) << 2)
 #define CORTEX_A78_ACTLR2_EL1_BIT_40			(ULL(1) << 40)
+
+#define CORTEX_A78_ACTLR3_EL1				S3_0_C15_C1_2
 
 /*******************************************************************************
  * CPU Activity Monitor Unit register specific definitions.
