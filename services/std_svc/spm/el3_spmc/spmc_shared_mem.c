d781959f81923bc3a59e77abd44df2fcc61f044e
Demi Marie Obenour <demiobenour@gmail.com>
2023-01-11 14:16:37 -0500

    fix(el3-spmc): only call spmc_shm_check_obj() on complete objects
    When called on incomplete objects, it might fail or access uninitialized
    memory. This allows simplifying spmc_shm_check_obj().
    
    Change-Id: I7c11f15d4c8ebe8cd15e7d8c37a0d0f3daa83675
    Signed-off-by: Demi Marie Obenour <demiobenour@gmail.com>

--- services/std_svc/spm/el3_spmc/spmc_shared_mem.c
+++ services/std_svc/spm/el3_spmc/spmc_shared_mem.c
@@ -878,14 +878,6 @@ static int spmc_shmem_check_obj(struct spmc_shmem_obj *obj,
 			return -EINVAL;
 		}
 
-		if (obj->desc_filled < obj->desc_size) {
-			/*
-			 * The whole descriptor has not yet been received.
-			 * Skip final checks.
-			 */
-			return 0;
-		}
-
 		/*
 		 * The offset provided to the composite memory region descriptor
 		 * should be consistent across endpoint descriptors. Store the
@@ -1044,11 +1036,6 @@ static long spmc_ffa_fill_desc(struct mailbox *mbox,
 	}
 
 	obj->desc_filled += fragment_length;
-	ret = spmc_shmem_check_obj(obj, ffa_version);
-	if (ret != 0) {
-		ret = FFA_ERROR_INVALID_PARAMETER;
-		goto err_bad_desc;
-	}
 
 	handle_low = (uint32_t)obj->desc.handle;
 	handle_high = obj->desc.handle >> 32;
@@ -1060,6 +1047,12 @@ static long spmc_ffa_fill_desc(struct mailbox *mbox,
 	}
 
 	/* The full descriptor has been received, perform any final checks. */
+
+	ret = spmc_shmem_check_obj(obj, ffa_version);
+	if (ret != 0) {
+		ret = FFA_ERROR_INVALID_PARAMETER;
+		goto err_bad_desc;
+	}
 
 	/*
 	 * If a partition ID resides in the secure world validate that the
