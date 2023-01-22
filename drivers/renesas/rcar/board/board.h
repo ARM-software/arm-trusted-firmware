281edfee02bc72d81aa4972d60216647f932f3df
Marek Vasut <marek.vasut+renesas@gmail.com>
2023-01-22 19:28:43 +0100

    fix(rcar3): update Draak and Eagle board IDs
    The Draak board ID is 7, Eagle is 3, update them to match
    HW documentation. Drop the U suffix from Ebisu board ID.
    
    Signed-off-by: Marek Vasut <marek.vasut+renesas@gmail.com>
    Change-Id: Idd191614a6f1f8dbbc13b8e0bc1f089090f9d20b

--- drivers/renesas/rcar/board/board.h
+++ drivers/renesas/rcar/board/board.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015-2018, Renesas Electronics Corporation. All rights
+ * Copyright (c) 2015-2023, Renesas Electronics Corporation. All rights
  * reserved.
  *
  * SPDX-License-Identifier: BSD-3-Clause
@@ -11,13 +11,13 @@
 #define BOARD_SALVATOR_X		(0x00)
 #define BOARD_KRIEK			(0x01)
 #define BOARD_STARTER_KIT		(0x02)
+#define BOARD_EAGLE			(0x03)
 #define BOARD_SALVATOR_XS		(0x04)
+#define BOARD_DRAAK			(0x07)
 #define BOARD_EBISU			(0x08)
 #define BOARD_STARTER_KIT_PRE		(0x0B)
-#define BOARD_EBISU_4D			(0x0DU)
-#define BOARD_DRAAK			(0x0EU)
-#define BOARD_EAGLE			(0x0FU)
-#define BOARD_UNKNOWN			(BOARD_EAGLE + 1U)
+#define BOARD_EBISU_4D			(0x0D)
+#define BOARD_UNKNOWN			(BOARD_EBISU_4D + 1U)
 
 #define BOARD_REV_UNKNOWN		(0xFF)
 
