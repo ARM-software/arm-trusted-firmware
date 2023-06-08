91291633a1c99736803f39edb21cad95a3517ee8
Akshay Belsare <akshay.belsare@amd.com>
2023-06-08 11:19:54 +0530

    fix(zynqmp): type cast addresses to fix overflow issue
    Type cast the build time base and size argument to unsigned integer
    and the limit derived from these two as unsigned long to avoid
    size overflow issue during build.
    
    For zynqmp platform, calculating the limit without typecasting results
    in build error as follows
    
    make -j DEBUG=0 RESET_TO_BL31=1 PLAT=zynqmp \
    ZYNQMP_ATF_MEM_BASE=0x70000000 ZYNQMP_ATF_MEM_SIZE=0x10000000 \
    XILINX_OF_BOARD_DTB_ADDR=0x100000 bl31
    
    plat/xilinx/zynqmp/include/platform_def.h:51:62:
    error: integer overflow in expression of type 'int' results
    				in '-2147483648' [-Werror=overflow]
     51 | # define BL31_LIMIT   (ZYNQMP_ATF_MEM_BASE + ZYNQMP_ATF_MEM_SIZE)
    
    Change-Id: Id093a50e748884d4fba65626e94f361f6c23cecc
    Signed-off-by: Michal Simek <michal.simek@amd.com>
    Signed-off-by: Akshay Belsare <akshay.belsare@amd.com>

--- plat/xilinx/zynqmp/include/platform_def.h
+++ plat/xilinx/zynqmp/include/platform_def.h
@@ -47,10 +47,10 @@
 # define BL31_LIMIT			U(0x80000)
 #endif
 #else
-# define BL31_BASE			(ZYNQMP_ATF_MEM_BASE)
-# define BL31_LIMIT			(ZYNQMP_ATF_MEM_BASE + ZYNQMP_ATF_MEM_SIZE)
+# define BL31_BASE			U(ZYNQMP_ATF_MEM_BASE)
+# define BL31_LIMIT			(UL(ZYNQMP_ATF_MEM_BASE) + U(ZYNQMP_ATF_MEM_SIZE))
 # ifdef ZYNQMP_ATF_MEM_PROGBITS_SIZE
-#  define BL31_PROGBITS_LIMIT		(ZYNQMP_ATF_MEM_BASE + ZYNQMP_ATF_MEM_PROGBITS_SIZE)
+#  define BL31_PROGBITS_LIMIT		(UL(ZYNQMP_ATF_MEM_BASE) + U(ZYNQMP_ATF_MEM_PROGBITS_SIZE))
 # endif
 #endif
 
@@ -61,8 +61,8 @@
 # define BL32_BASE			U(0x60000000)
 # define BL32_LIMIT			U(0x80000000)
 #else
-# define BL32_BASE			(ZYNQMP_BL32_MEM_BASE)
-# define BL32_LIMIT			(ZYNQMP_BL32_MEM_BASE + ZYNQMP_BL32_MEM_SIZE)
+# define BL32_BASE			U(ZYNQMP_BL32_MEM_BASE)
+# define BL32_LIMIT			(UL(ZYNQMP_BL32_MEM_BASE) + U(ZYNQMP_BL32_MEM_SIZE))
 #endif
 
 /*******************************************************************************
@@ -71,7 +71,7 @@
 #ifndef PRELOADED_BL33_BASE
 # define PLAT_ARM_NS_IMAGE_BASE	U(0x8000000)
 #else
-# define PLAT_ARM_NS_IMAGE_BASE	PRELOADED_BL33_BASE
+# define PLAT_ARM_NS_IMAGE_BASE	U(PRELOADED_BL33_BASE)
 #endif
 
 /*******************************************************************************
