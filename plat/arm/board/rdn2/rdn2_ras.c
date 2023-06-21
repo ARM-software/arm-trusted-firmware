e80274880bf694fd0b0e869a6ceb67e95e547544
Omkar Anand Kulkarni <omkar.kulkarni@arm.com>
2023-06-21 13:11:04 +0530

    feat(rdn2): enable Neoverse N2 CPU error handling support
    Defines N2 CPU RAS error for RD-N2 platform variants. Enables N2 CPU
    error handling on RD-N2 platform variants.
    
    Signed-off-by: Omkar Anand Kulkarni <omkar.kulkarni@arm.com>
    Change-Id: If9378064c41e0d14e6c789c71f8def594f89e220

--- plat/arm/board/rdn2/rdn2_ras.c
+++ plat/arm/board/rdn2/rdn2_ras.c
@@ -14,6 +14,9 @@ struct sgi_ras_ev_map plat_ras_map[] = {
 
 	/* Non Secure base RAM ECC UE interrupt */
 	{SGI_SDEI_DS_EVENT_0, NS_RAM_ECC_UE_INT, SGI_RAS_INTR_TYPE_SPI},
+
+	/* CPU 1-bit ECC CE error interrupt */
+	{SGI_SDEI_DS_EVENT_1, PLAT_CORE_FAULT_IRQ, SGI_RAS_INTR_TYPE_PPI}
 };
 
 /* RAS error record list definition, used by the common RAS framework. */
@@ -21,11 +24,15 @@ struct err_record_info plat_err_records[] = {
 	/* Base element RAM Non-secure error record. */
 	ERR_RECORD_MEMMAP_V1(SOC_NS_RAM_ERR_REC_BASE, 4, NULL,
 				&sgi_ras_sram_intr_handler, 0),
+	ERR_RECORD_SYSREG_V1(0, 1, NULL, &sgi_ras_cpu_intr_handler, 0),
 };
 
 /* RAS error interrupt list definition, used by the common RAS framework. */
 struct ras_interrupt plat_ras_interrupts[] = {
 	{
+		.intr_number = PLAT_CORE_FAULT_IRQ,
+		.err_record = &plat_err_records[1],
+	}, {
 		.intr_number = NS_RAM_ECC_CE_INT,
 		.err_record = &plat_err_records[0],
 	}, {
