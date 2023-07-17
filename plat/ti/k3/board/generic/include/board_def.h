e9868458e6de2ffb3c08e2fafa444a812b895337
Andrew Davis <afd@ti.com>
2023-07-17 12:31:53 -0500

    feat(ti): remove extra core counts in cluster 2 and 3
    No K3 SoC supported by this TARGET_BOARD has any cluster
    2 or 3 cores. Remove these to save some memory.
    
    Signed-off-by: Andrew Davis <afd@ti.com>
    Change-Id: I27868a2f3aac25fa0fdec56847e273d88f0d9a87

--- plat/ti/k3/board/generic/include/board_def.h
+++ plat/ti/k3/board/generic/include/board_def.h
@@ -12,8 +12,8 @@
 /* The ports must be in order and contiguous */
 #define K3_CLUSTER0_CORE_COUNT		U(2)
 #define K3_CLUSTER1_CORE_COUNT		U(2)
-#define K3_CLUSTER2_CORE_COUNT		U(2)
-#define K3_CLUSTER3_CORE_COUNT		U(2)
+#define K3_CLUSTER2_CORE_COUNT		U(0)
+#define K3_CLUSTER3_CORE_COUNT		U(0)
 
 #define PLAT_PROC_START_ID		U(32)
 #define PLAT_PROC_DEVICE_START_ID	U(202)
