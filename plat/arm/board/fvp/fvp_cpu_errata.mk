a24c8006ea39be65e156283407fa45f7c7592f6e
Sona Mathew <sonarebecca.mathew@arm.com>
2023-09-26 17:49:51 -0500

    refactor(errata-abi): workaround platforms non-arm interconnect
    The workarounds for these below mentioned errata are not implemented
    in EL3, but the flags can be enabled/disabled at a platform level
    based on arm/non-arm interconnect IP flag. The ABI helps assist the
    Kernel in the process of mitigation for the following errata:
    
    Cortex-A715:   erratum 2701951
    Neoverse V2:   erratum 2719103
    Cortex-A710:   erratum 2701952
    Cortex-X2:     erratum 2701952
    Neoverse N2:   erratum 2728475
    Neoverse V1:   erratum 2701953
    Cortex-A78:    erratum 2712571
    Cortex-A78AE:  erratum 2712574
    Cortex-A78C:   erratum 2712575
    
    Change-Id: Ie86b7212d731a79e2a0c07649e69234e733cd78d
    Signed-off-by: Sona Mathew <sonarebecca.mathew@arm.com>
    (cherry picked from commit aceb9c9e54a02a8aef6dc808081c5feb296e7c8e)

--- plat/arm/board/fvp/fvp_cpu_errata.mk
+++ plat/arm/board/fvp/fvp_cpu_errata.mk
@@ -9,6 +9,7 @@
 # for platforms that need to enable errata based on non-arm interconnect IP.
 
 ifeq (${ERRATA_ABI_SUPPORT}, 1)
+ifeq (${ERRATA_NON_ARM_INTERCONNECT}, 1)
 ifeq (${CTX_INCLUDE_AARCH32_REGS}, 0)
 CORTEX_A78_H_INC	:= 1
 NEOVERSE_N2_H_INC	:= 1
@@ -19,7 +20,6 @@ CORTEX_A715_H_INC 	:= 1
 CORTEX_A78C_H_INC	:= 1
 CORTEX_X2_H_INC		:= 1
 $(eval $(call add_define, CORTEX_A78_H_INC))
-$(eval $(call add_define, NEOVERSE_N1_H_INC))
 $(eval $(call add_define, NEOVERSE_N2_H_INC))
 $(eval $(call add_define, NEOVERSE_V1_H_INC))
 $(eval $(call add_define, CORTEX_A78_AE_H_INC))
@@ -27,5 +27,6 @@ $(eval $(call add_define, CORTEX_A710_H_INC))
 $(eval $(call add_define, CORTEX_A715_H_INC))
 $(eval $(call add_define, CORTEX_A78C_H_INC))
 $(eval $(call add_define, CORTEX_X2_H_INC))
+endif
 endif
 endif
