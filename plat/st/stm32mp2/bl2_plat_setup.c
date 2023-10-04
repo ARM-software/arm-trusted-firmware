0ff523ae7bcebc674907b521b7ce57829f3a58e9
Christophe Kerello <christophe.kerello@foss.st.com>
2023-10-04 09:50:39 +0200

    fix(stm32mp2): flush data cache at the end of BL2
    Explicitly flush .bss and .data areas from cache. We must be sure
    data is correct there.
    
    Change-Id: I916c96b64abcc0ca47151c7510bb262569dd86ea
    Signed-off-by: Christophe Kerello <christophe.kerello@foss.st.com>

--- plat/st/stm32mp2/bl2_plat_setup.c
+++ plat/st/stm32mp2/bl2_plat_setup.c
@@ -35,6 +35,11 @@
 
 #define BOOT_CTX_ADDR	0x0e000020UL
 
+IMPORT_SYM(uintptr_t, __BSS_START__, BSS_START)
+IMPORT_SYM(uintptr_t, __BSS_END__, BSS_END)
+IMPORT_SYM(uintptr_t, __DATA_START__, DATA_START)
+IMPORT_SYM(uintptr_t, __DATA_END__, DATA_END)
+
 static void print_reset_reason(void)
 {
 	uint32_t rstsr = mmio_read_32(stm32mp_rcc_base() + RCC_C1BOOTRSTSCLRR);
@@ -424,4 +429,16 @@ int bl2_plat_handle_post_image_load(unsigned int image_id)
 	}
 
 	return err;
+}
+
+void bl2_plat_prepare_exit(void)
+{
+	/*
+	 * The cache has just been disabled by the caller function.
+	 * The flush of the data sections should then be done at the beginning
+	 * of this function before accessing data that have not been flushed.
+	 * This is the case for the storage data, or the timer ops for example.
+	 */
+	flush_dcache_range(BSS_START, BSS_END - BSS_START);
+	flush_dcache_range(DATA_START, DATA_END - DATA_START);
 }
