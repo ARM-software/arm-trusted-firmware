033254ddb516b1fc9faabe65a2d50b254c507f28
Yann Gautier <yann.gautier@st.com>
2023-10-24 15:40:42 +0200

    docs(changelog): changelog for lts-v2.8.10 release
    Generated with the command:
    npm run release -- --release-as 2.8.10
    
    Signed-off-by: Yann Gautier <yann.gautier@st.com>
    Change-Id: Ibe2581783fed3f96150601cffdd6e606bd9a2d50

--- docs/conf.py
+++ docs/conf.py
@@ -14,8 +14,8 @@
 
 project = "Trusted Firmware-A"
 author = "Trusted Firmware-A contributors"
-version = "2.9.0"
-release = "2.9.0"
+version = "2.8.10"
+release = "2.8.10"
 
 # -- General configuration ---------------------------------------------------
 
