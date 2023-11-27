b7591e16fc3ef8cf68fca2b1eaa4add4d47feaf7
Diederik de Haas <didi.trustedfirmware@cknow.org>
2023-11-27 10:21:37 +0100

    fix(rk3328): apply ERRATA_A53_1530924 erratum
    Apply erratum ERRATA_A53_1530924.
    
    Signed-off-by: Olivier Deprez <olivier.deprez@arm.com>
    Change-Id: Ib4130fd9d4cd16b12322f44e91196607fcb6bf6b

--- plat/rockchip/rk3328/platform.mk
+++ plat/rockchip/rk3328/platform.mk
@@ -65,6 +65,7 @@ include lib/libfdt/libfdt.mk
 
 # Enable workarounds for selected Cortex-A53 errata
 ERRATA_A53_855873	:=	1
+ERRATA_A53_1530924	:=      1
 
 $(eval $(call add_define,PLAT_EXTRA_LD_SCRIPT))
 $(eval $(call add_define,PLAT_SKIP_OPTEE_S_EL1_INT_REGISTER))
