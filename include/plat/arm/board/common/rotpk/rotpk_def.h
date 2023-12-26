0f0fd499dedd799e19279f0aa1f4f686085a944a
Rohit Mathew <Rohit.Mathew@arm.com>
2023-12-26 22:33:03 +0000

    fix(rotpk): move rotpk definitions out of arm_def.h
    The file arm_def.h currently contains common definitions used by ARM
    platforms. However, some platforms may have their own definitions,
    allowing them to avoid a direct dependency on arm_def.h. For a clean
    platform port of arm_def.h, none of the source files should directly
    include arm_def.h; instead, they should include the platform header
    which would indirectly include the required definitions.
    
    Presently, the rotpk module has a source file that directly includes
    arm_def.h. This could lead to compilation issues if the platform
    incorporating the rotpk module has a separate implementation of some or
    all of the definitions in arm_def.h file. To address this, move the
    relevant definitions out of arm_def.h and into rotpk_def.h.
    
    Signed-off-by: Rohit Mathew <Rohit.Mathew@arm.com>
    Change-Id: I9e8b0d319391f9a167af5c69a7b2d42ac488e7b4

---
+++ include/plat/arm/board/common/rotpk/rotpk_def.h
@@ -0,0 +1,24 @@
+
+/*
+ * Copyright (c) 2024, ARM Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef ROTPK_DEF_H
+#define ROTPK_DEF_H
+
+/*
+ * Definitions related to ROTPK
+ */
+
+/*
+ * Root of trust key lengths
+ */
+#ifndef ARM_ROTPK_HEADER_LEN
+#define ARM_ROTPK_HEADER_LEN		19
+#endif
+#ifndef ARM_ROTPK_HASH_LEN
+#define ARM_ROTPK_HASH_LEN		32
+#endif
+#endif /* ROTPK_DEF_H */
--- include/plat/arm/common/arm_def.h
+++ include/plat/arm/common/arm_def.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015-2023, Arm Limited and Contributors. All rights reserved.
+ * Copyright (c) 2015-2024, Arm Limited and Contributors. All rights reserved.
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
@@ -12,6 +12,7 @@
 #include <drivers/arm/gic_common.h>
 #include <lib/utils_def.h>
 #include <lib/xlat_tables/xlat_tables_defs.h>
+#include <plat/arm/board/common/rotpk/rotpk_def.h>
 #include <plat/arm/common/smccc_def.h>
 #include <plat/common/common_def.h>
 
@@ -19,11 +20,6 @@
  * Definitions common to all ARM standard platforms
  *****************************************************************************/
 
-/*
- * Root of trust key lengths
- */
-#define ARM_ROTPK_HEADER_LEN		19
-#define ARM_ROTPK_HASH_LEN		32
 
 /* Special value used to verify platform parameters from BL2 to BL31 */
 #define ARM_BL31_PLAT_PARAM_VAL		ULL(0x0f1e2d3c4b5a6978)
