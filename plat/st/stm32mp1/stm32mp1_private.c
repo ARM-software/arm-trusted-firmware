4b1826c82071b4c919c97942bd59bf22c3e34149
Maxime Méré <maxime.mere@foss.st.com>
2024-01-16 15:40:49 +0100

    refactor(st): change suffix for SYSCFG functions
    This patch replaces the suffix "stm32mp1_" in the SYSCFG drivers
    with "stm32mp_". By using a common suffix for function names, we can
    avoid issues or platform compilation flags when a driver
    needs to access SYSCFG across different platforms.
    
    Signed-off-by: Maxime Méré <maxime.mere@foss.st.com>
    Change-Id: I24407852c085abd843ef4cdef235c022a5e57a85

--- plat/st/stm32mp1/stm32mp1_private.c
+++ plat/st/stm32mp1/stm32mp1_private.c
@@ -280,7 +280,7 @@ void stm32mp1_deconfigure_uart_pins(void)
 uint32_t stm32mp_get_chip_version(void)
 {
 #if STM32MP13
-	return stm32mp1_syscfg_get_chip_version();
+	return stm32mp_syscfg_get_chip_version();
 #endif
 #if STM32MP15
 	uint32_t version = 0U;
@@ -297,7 +297,7 @@ uint32_t stm32mp_get_chip_version(void)
 uint32_t stm32mp_get_chip_dev_id(void)
 {
 #if STM32MP13
-	return stm32mp1_syscfg_get_chip_dev_id();
+	return stm32mp_syscfg_get_chip_dev_id();
 #endif
 #if STM32MP15
 	uint32_t dev_id;
