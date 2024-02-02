89c58a5087f12f0e965ce8fdf946038d5799d07d
Jagdish Gediya <jagdish.gediya@arm.com>
2024-02-02 06:01:44 +0000

    feat(tc): setup ni-tower non-secure access for TC3
    NI-Tower's component's registers are need to be accessed from
    kernel NI-PMU driver so enable NS access to it.
    
    Change-Id: I83a8b3a1d2778baf767ff93263e246d127ef8114
    Signed-off-by: Jagdish Gediya <jagdish.gediya@arm.com>
    Signed-off-by: Leo Yan <leo.yan@arm.com>

--- include/plat/arm/common/plat_arm.h
+++ include/plat/arm/common/plat_arm.h
@@ -362,6 +362,7 @@ void plat_arm_interconnect_enter_coherency(void);
 void plat_arm_interconnect_exit_coherency(void);
 void plat_arm_program_trusted_mailbox(uintptr_t address);
 bool plat_arm_bl1_fwu_needed(void);
+int plat_arm_ni_setup(uintptr_t global_cfg);
 __dead2 void plat_arm_error_handler(int err);
 __dead2 void plat_arm_system_reset(void);
 
