e8e5c775929d90df3395701bfef3f50591d1c28e
Yann Gautier <yann.gautier@st.com>
2024-02-08 19:33:22 +0100

    fix(build): update versions
    The versions in Makefile, docs/conf.py and pyproject.toml were not
    properly updated, due to scripts issues (previous patches).
    
    Signed-off-by: Yann Gautier <yann.gautier@st.com>
    Change-Id: Ia80bd4b28fbb7402f4ae139512fcd176bad057ec

--- docs/conf.py
+++ docs/conf.py
@@ -14,8 +14,8 @@
 
 project = "Trusted Firmware-A"
 author = "Trusted Firmware-A contributors"
-version = "2.10.0"
-release = "2.10.0"
+version = "2.10.1"
+release = "2.10.1"
 
 # -- General configuration ---------------------------------------------------
 
