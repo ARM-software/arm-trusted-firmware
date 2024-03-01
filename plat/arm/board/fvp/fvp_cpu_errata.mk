1ba369a5e0a982bf9ecccc71d17274763ea4e6a5
Sona Mathew <sonarebecca.mathew@arm.com>
2024-02-29 18:17:41 -0600

    chore: rearrange the fvp_cpu_errata.mk file
    Change-Id: I3959bdf5852c5714f2238f61493a931b3c857a20
    Signed-off-by: Sona Mathew <sonarebecca.mathew@arm.com>

--- plat/arm/board/fvp/fvp_cpu_errata.mk
+++ plat/arm/board/fvp/fvp_cpu_errata.mk
@@ -11,22 +11,20 @@
 ifeq (${ERRATA_ABI_SUPPORT}, 1)
 ifeq (${ERRATA_NON_ARM_INTERCONNECT}, 1)
 ifeq (${CTX_INCLUDE_AARCH32_REGS}, 0)
+CORTEX_A710_H_INC	:= 1
 CORTEX_A78_H_INC	:= 1
+CORTEX_A78_AE_H_INC	:= 1
+CORTEX_A78C_H_INC	:= 1
+CORTEX_X3_H_INC		:= 1
 NEOVERSE_N2_H_INC	:= 1
 NEOVERSE_V1_H_INC	:= 1
-CORTEX_A78_AE_H_INC	:= 1
-CORTEX_A710_H_INC	:= 1
-CORTEX_X3_H_INC		:= 1
-CORTEX_A78C_H_INC	:= 1
-CORTEX_X2_H_INC		:= 1
+$(eval $(call add_define, CORTEX_A710_H_INC))
 $(eval $(call add_define, CORTEX_A78_H_INC))
+$(eval $(call add_define, CORTEX_A78_AE_H_INC))
+$(eval $(call add_define, CORTEX_A78C_H_INC))
+$(eval $(call add_define, CORTEX_X3_H_INC))
 $(eval $(call add_define, NEOVERSE_N2_H_INC))
 $(eval $(call add_define, NEOVERSE_V1_H_INC))
-$(eval $(call add_define, CORTEX_A78_AE_H_INC))
-$(eval $(call add_define, CORTEX_A710_H_INC))
-$(eval $(call add_define, CORTEX_X3_H_INC))
-$(eval $(call add_define, CORTEX_A78C_H_INC))
-$(eval $(call add_define, CORTEX_X2_H_INC))
 endif
 endif
 endif
