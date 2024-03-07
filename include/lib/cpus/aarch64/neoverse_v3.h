328d304d27b03a71007ad087fd12566c69583e1f
Sona Mathew <sonarebecca.mathew@arm.com>
2024-03-07 14:41:20 -0600

    chore: rename Poseidon to Neoverse V3
    Rename Neoverse Poseidon to Neoverse V3, make changes
    to related build flags, macros, file names etc.
    
    Change-Id: I9e40ba8f80b7390703d543787e6cd2ab6301e891
    Signed-off-by: Sona Mathew <sonarebecca.mathew@arm.com>

--- include/lib/cpus/aarch64/neoverse_v3.h
+++ include/lib/cpus/aarch64/neoverse_v3.h
@@ -4,25 +4,25 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
-#ifndef NEOVERSE_POSEIDON_H
-#define NEOVERSE_POSEIDON_H
+#ifndef NEOVERSE_V3_H
+#define NEOVERSE_V3_H
 
 
-#define NEOVERSE_POSEIDON_VNAE_MIDR				U(0x410FD830)
-#define NEOVERSE_POSEIDON_V_MIDR				U(0x410FD840)
+#define NEOVERSE_V3_VNAE_MIDR				U(0x410FD830)
+#define NEOVERSE_V3_MIDR				U(0x410FD840)
 
-/* Neoverse Poseidon loop count for CVE-2022-23960 mitigation */
-#define NEOVERSE_POSEIDON_BHB_LOOP_COUNT			U(132)
+/* Neoverse V3 loop count for CVE-2022-23960 mitigation */
+#define NEOVERSE_V3_BHB_LOOP_COUNT			U(132)
 
 /*******************************************************************************
  * CPU Extended Control register specific definitions.
  ******************************************************************************/
-#define NEOVERSE_POSEIDON_CPUECTLR_EL1				S3_0_C15_C1_4
+#define NEOVERSE_V3_CPUECTLR_EL1				S3_0_C15_C1_4
 
 /*******************************************************************************
  * CPU Power Control register specific definitions
  ******************************************************************************/
-#define NEOVERSE_POSEIDON_CPUPWRCTLR_EL1			S3_0_C15_C2_7
-#define NEOVERSE_POSEIDON_CPUPWRCTLR_EL1_CORE_PWRDN_BIT		U(1)
+#define NEOVERSE_V3_CPUPWRCTLR_EL1			S3_0_C15_C2_7
+#define NEOVERSE_V3_CPUPWRCTLR_EL1_CORE_PWRDN_BIT		U(1)
 
-#endif /* NEOVERSE_POSEIDON_H */
+#endif /* NEOVERSE_V3_H */
