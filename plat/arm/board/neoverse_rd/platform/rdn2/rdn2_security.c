947e78728e4fff5baeaaf479ed35a07a8049c3cb
Rohit Mathew <rohit.mathew@arm.com>
2024-03-19 17:00:31 +0000

    refactor(neoverse-rd): define naming convention for RoS macros
    As part of the refactoring for the second generation platforms,
    introduce a naming convention for macros within nrd_ros_def2.h and
    nrd_ros_fw_def2.h.  All macros, except those related to page table
    entries, must adhere to the format NRD_ROS_<name>. Page table entry
    macros are handled separately and are not part of this patch.
    
    Signed-off-by: Rohit Mathew <rohit.mathew@arm.com>
    Change-Id: Ifcdc30b1c80b9848b793de2013095fc98d57bec6

--- plat/arm/board/neoverse_rd/platform/rdn2/rdn2_security.c
+++ plat/arm/board/neoverse_rd/platform/rdn2/rdn2_security.c
@@ -25,20 +25,20 @@ static const arm_tzc_regions_info_t tzc_regions[] = {
 static const arm_tzc_regions_info_t tzc_regions_mc[][NRD_CHIP_COUNT - 1] = {
 	{
 		/* TZC memory regions for second chip */
-		NRD_PLAT_TZC_NS_REMOTE_REGIONS_DEF(1),
+		NRD_ROS_TZC_NS_REMOTE_REGIONS_DEF(1),
 		{}
 	},
 #if NRD_CHIP_COUNT > 2
 	{
 		/* TZC memory regions for third chip */
-		NRD_PLAT_TZC_NS_REMOTE_REGIONS_DEF(2),
+		NRD_ROS_TZC_NS_REMOTE_REGIONS_DEF(2),
 		{}
 	},
 #endif
 #if NRD_CHIP_COUNT > 3
 	{
 		/* TZC memory regions for fourth chip */
-		NRD_PLAT_TZC_NS_REMOTE_REGIONS_DEF(3),
+		NRD_ROS_TZC_NS_REMOTE_REGIONS_DEF(3),
 		{}
 	},
 #endif
