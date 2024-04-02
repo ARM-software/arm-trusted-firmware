fed9368529e5bc2c9111ac5a743688166661fd8f
Rohit Mathew <rohit.mathew@arm.com>
2024-04-02 16:28:36 +0100

    feat(neoverse-rd): enable AMU if supported by the platform
    Set build-option ENABLE_FEAT_AMU to 2 for RD-V1 and RD-V1-MC so that AMU
    is enabled if the feature is implemented on these platforms. This would
    ensure that lower ELs could access system registers relevant to AMU
    without causing a trap to EL3.
    
    Signed-off-by: Rohit Mathew <rohit.mathew@arm.com>
    Change-Id: I4d34db0c102c786f34103746064e22600d28da33

--- plat/arm/board/neoverse_rd/platform/rdv1mc/platform.mk
+++ plat/arm/board/neoverse_rd/platform/rdv1mc/platform.mk
@@ -69,7 +69,7 @@ NT_FW_CONFIG		:=	${BUILD_PLAT}/fdts/${PLAT}_nt_fw_config.dtb
 $(eval $(call TOOL_ADD_PAYLOAD,${NT_FW_CONFIG},--nt-fw-config,${NT_FW_CONFIG}))
 
 override CTX_INCLUDE_AARCH32_REGS	:= 0
-override ENABLE_FEAT_AMU		:= 1
+override ENABLE_FEAT_AMU		:= 2
 
 ifneq ($(NRD_PLATFORM_VARIANT),0)
  $(error "NRD_PLATFORM_VARIANT for RD-V1-MC should always be 0, \
