9b5d75b17f131aa80930128b247c63d4ba177469
Bipin Ravi <bipin.ravi@arm.com>
2024-04-10 15:33:21 -0500

    fix(cpus): workaround for Cortex-X4 erratum 2740089
    Cortex-X4 erratum 2740089 is a Cat B erratum that applies to
    all revisions <=r0p1 and is fixed in r0p2. The workaround is to
    insert a dsb before the isb in the power down sequence.
    
    SDEN documentation:
    https://developer.arm.com/documentation/SDEN2432808/latest
    
    Change-Id: I1d0fa4dd383437044a4467591f65a4a8514cabdc
    Signed-off-by: Bipin Ravi <bipin.ravi@arm.com>
    (cherry picked from commit c833ca66a6fecbc54e038164e466be677559ec4e)

--- lib/cpus/cpu-ops.mk
+++ lib/cpus/cpu-ops.mk
@@ -824,6 +824,10 @@ CPU_FLAG_LIST += ERRATA_X3_2779509
 # cpu and is fixed in r0p1.
 CPU_FLAG_LIST += ERRATA_X4_2701112
 
+# Flag to apply erratum 2740089 workaround during powerdown. This erratum
+# applies to all revisions <= r0p1 of the Cortex-X4 cpu, it is fixed in r0p2.
+CPU_FLAG_LIST += ERRATA_X4_2740089
+
 # Flag to apply erratum 1922240 workaround during reset. This erratum applies
 # to revision r0p0 of the Cortex-A510 cpu and is fixed in r0p1.
 CPU_FLAG_LIST += ERRATA_A510_1922240
