813f6677443b9bee094e0f52283f7e078e214e69
Govindraj Raja <govindraj.raja@arm.com>
2024-05-06 12:52:17 -0500

    chore: rename hermes to neoverse-n3
    Rename hermes cpu to Neoverse-N3
    
    Change-Id: I912d4c824c5004a8c1909c68fef77f1f5e202b8a
    Signed-off-by: Govindraj Raja <govindraj.raja@arm.com>
    (cherry picked from commit ba6b69494b2c0bf56b94a370e6f25e124c01abe7)

--- include/lib/cpus/aarch64/neoverse_n3.h
+++ include/lib/cpus/aarch64/neoverse_n3.h
@@ -1,23 +1,23 @@
 /*
- * Copyright (c) 2023, Arm Limited. All rights reserved.
+ * Copyright (c) 2023-2024, Arm Limited. All rights reserved.
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
-#ifndef NEOVERSE_HERMES_H
-#define NEOVERSE_HERMES_H
+#ifndef NEOVERSE_N3_H
+#define NEOVERSE_N3_H
 
-#define NEOVERSE_HERMES_MIDR				U(0x410FD8E0)
+#define NEOVERSE_N3_MIDR				U(0x410FD8E0)
 
 /*******************************************************************************
  * CPU Extended Control register specific definitions
  ******************************************************************************/
-#define NEOVERSE_HERMES_CPUECTLR_EL1			S3_0_C15_C1_4
+#define NEOVERSE_N3_CPUECTLR_EL1			S3_0_C15_C1_4
 
 /*******************************************************************************
  * CPU Power Control register specific definitions
  ******************************************************************************/
-#define NEOVERSE_HERMES_CPUPWRCTLR_EL1			S3_0_C15_C2_7
-#define NEOVERSE_HERMES_CPUPWRCTLR_EL1_CORE_PWRDN_BIT	U(1)
+#define NEOVERSE_N3_CPUPWRCTLR_EL1			S3_0_C15_C2_7
+#define NEOVERSE_N3_CPUPWRCTLR_EL1_CORE_PWRDN_BIT	U(1)
 
-#endif /* NEOVERSE_HERMES_H */
+#endif /* NEOVERSE_N3_H */
