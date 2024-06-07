jolivain/trusted-firmware-a

feat(rdfremont): fetch attestation key and token from RSE
Use the delegated attestation driver to fetch platform attestation token
and Realm attestation key from RSE over the AP-RSE comms interface.

Signed-off-by: Rohit Mathew <Rohit.Mathew@arm.com>
Signed-off-by: Vivek Gautam <vivek.gautam@arm.com>
Change-Id: Id0cfd82ef79598cd8368ba017c145bf34d502e65

plat/arm/board/neoverse_rd/platform/rdfremont/rdfremont_plat_attest_token.c
@@ -4,13 +4,25 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
+#include <errno.h>
 #include <stdint.h>
 
+#include <common/debug.h>
+#include <drivers/delay_timer.h>
 #include <plat/common/platform.h>
+#include <psa/error.h>
+#include <rse_delegated_attestation.h>
 
-/* Placeholder implementation of the API to retrieve attestation token */
 int plat_rmmd_get_cca_attest_token(uintptr_t buf, size_t *len,
 			       uintptr_t hash, size_t hash_size)
 {
+	psa_status_t ret;
+
+	ret = delegated_attestation_get_token(buf, len, hash, hash_size);
+	if (ret != PSA_SUCCESS) {
+		ERROR("Unable to fetch CCA attestation token\n");
+		return -1;
+	}
+
 	return 0;
 }

plat/arm/board/neoverse_rd/platform/rdfremont/rdfremont_realm_attest_key.c
@@ -4,13 +4,25 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
+#include <errno.h>
 #include <stdint.h>
 
+#include <common/debug.h>
+#include <drivers/delay_timer.h>
 #include <plat/common/platform.h>
+#include <psa/error.h>
+#include <rse_delegated_attestation.h>
 
-/* Placeholder implementation of the API to retrieve attestation key */
 int plat_rmmd_get_cca_realm_attest_key(uintptr_t buf, size_t *len,
 				       unsigned int type)
 {
+	psa_status_t ret;
+
+	ret = delegated_attestation_get_key(buf, len, type);
+	if (ret != PSA_SUCCESS) {
+		ERROR("Unable to fetch CCA attestation key\n");
+		return -1;
+	}
+
 	return 0;
 }

ARM-software/arm-trusted-firmware

plat/arm/board/neoverse_rd/platform/rdfremont/rdfremont_plat_attest_token.c
@@ -4,11 +4,23 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
+#include <errno.h>
 #include <stdint.h>
 
-/* Placeholder implementation of the API to retrieve attestation token */
+#include <cca_attestation.h>
+#include <common/debug.h>
+#include <psa/error.h>
+
 int plat_rmmd_get_cca_attest_token(uintptr_t buf, size_t *len,
 				   uintptr_t hash, size_t hash_size)
 {
+	psa_status_t ret;
+
+	ret = cca_attestation_get_plat_token(buf, len, hash, hash_size);
+	if (ret != PSA_SUCCESS) {
+		ERROR("Unable to fetch CCA attestation token\n");
+		return -1;
+	}
+
 	return 0;
 }

plat/arm/board/neoverse_rd/platform/rdfremont/rdfremont_realm_attest_key.c
@@ -4,11 +4,23 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
+#include <errno.h>
 #include <stdint.h>
 
-/* Placeholder implementation of the API to retrieve attestation key */
+#include <cca_attestation.h>
+#include <common/debug.h>
+#include <psa/error.h>
+
 int plat_rmmd_get_cca_realm_attest_key(uintptr_t buf, size_t *len,
 				       unsigned int type)
 {
+	psa_status_t ret;
+
+	ret = cca_attestation_get_realm_key(buf, len, type);
+	if (ret != PSA_SUCCESS) {
+		ERROR("Unable to fetch CCA attestation key\n");
+		return -1;
+	}
+
 	return 0;
 }