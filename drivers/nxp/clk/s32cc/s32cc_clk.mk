086ee20fe7ccb9dcbf6e9ee1ce529ae98e6cf977
Ghennadi Procopciuc <ghennadi.procopciuc@nxp.com>
2024-06-11 18:52:17 +0300

    feat(nxp-clk): add minimal set of S32CC clock ids
    The clock IDs are organized into categories, which are determined based
    on the first 2 MSB bits for each ID. Currently, there are two big
    categories: hardware and software-defined clocks.
    
    The first category refers to clock IDs understood by the S32CC PLL muxes
    and MC_CGM module muxes and is immutable. The last category of the
    clocks includes software-defined IDs for clocks to allow an easy
    representation of the hierarchy.
    
    Change-Id: Idc079feb3ca5f92d8bf337ef09efad006e267088
    Signed-off-by: Ghennadi Procopciuc <ghennadi.procopciuc@nxp.com>

--- drivers/nxp/clk/s32cc/s32cc_clk.mk
+++ drivers/nxp/clk/s32cc/s32cc_clk.mk
@@ -4,6 +4,9 @@
 # SPDX-License-Identifier: BSD-3-Clause
 #
 
+PLAT_INCLUDES		+= \
+	-I${PLAT_DRIVERS_INCLUDE_PATH}/clk/s32cc \
+
 CLK_SOURCES		:= \
 	${PLAT_DRIVERS_PATH}/clk/s32cc/s32cc_clk_drv.c \
 
