6d5319afecf62f931fe03c12f2dbc398e959c7f0
Madhukar Pappireddy <madhukar.pappireddy@arm.com>
2024-06-17 15:22:36 -0500

    feat(simd): add routines to save, restore sve state
    This adds assembly routines to save and restore SVE registers. In order
    to share between FPU and SVE the code to save and restore FPCR and
    FPSR, the patch converts code for those registers into macro.
    Since we will be using simd_ctx_t to save and restore FPU also, we use
    offsets in simd_ctx_t for FPSR and FPCR. Since simd_ctx_t has the same
    structure at the beginning as fp_regs_t, those offsets should be the
    same as CTX_FP_* offsets, when SVE is not enabled. Note that the code
    also saves and restores FPEXC32 reg along with FPSR and FPCR.
    
    Signed-off-by: Madhukar Pappireddy <madhukar.pappireddy@arm.com>
    Signed-off-by: Okash Khawaja <okash@google.com>
    Change-Id: I120c02359794aa6bb6376a464a9afe98bd84ae60

--- include/lib/extensions/sve.h
+++ include/lib/extensions/sve.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017-2023, Arm Limited and Contributors. All rights reserved.
+ * Copyright (c) 2017-2024, Arm Limited and Contributors. All rights reserved.
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
@@ -10,6 +10,7 @@
 #include <context.h>
 
 #if (ENABLE_SME_FOR_NS || ENABLE_SVE_FOR_NS)
+
 void sve_init_el2_unused(void);
 void sve_enable_per_world(per_world_context_t *per_world_ctx);
 void sve_disable_per_world(per_world_context_t *per_world_ctx);
@@ -24,5 +25,10 @@ static inline void sve_disable_per_world(per_world_context_t *per_world_ctx)
 {
 }
 #endif /* ( ENABLE_SME_FOR_NS | ENABLE_SVE_FOR_NS ) */
+
+#if CTX_INCLUDE_SVE_REGS
+void sve_context_save(simd_regs_t *regs);
+void sve_context_restore(simd_regs_t *regs);
+#endif
 
 #endif /* SVE_H */
