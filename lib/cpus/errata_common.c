c4c6eae4e00a8a152155501c60c53f071862271b
Arvind Ram Prakash <arvind.ramprakash@arm.com>
2024-08-05 16:11:42 -0500

    feat(cm): add errata 2938996, 2726228
    This patch checks if the Errata 2938996(Cortex-A520) , 2726228(Cortex-X4)
    applies to cores and if affected applies the errata workaround which
    disables TRBE.
    
    Signed-off-by: Arvind Ram Prakash <arvind.ramprakash@arm.com>
    Change-Id: I53b037839820c8b3a869f393588302a365d5b97c
    (cherry picked from commit 721249b0c0cce9fbe60175af6ee895e2bb7a6d10)
    Signed-off-by: Govindraj Raja <govindraj.raja@arm.com>

---
+++ lib/cpus/errata_common.c
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2024, Arm Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+/* Runtime C routines for errata workarounds and common routines */
+
+#include <arch.h>
+#include <arch_helpers.h>
+#include <cortex_a520.h>
+#include <cortex_x4.h>
+#include <lib/cpus/cpu_ops.h>
+#include <lib/cpus/errata.h>
+
+#if ERRATA_A520_2938996 || ERRATA_X4_2726228
+unsigned int check_if_affected_core(void)
+{
+	uint32_t midr_val = read_midr();
+	long rev_var  = cpu_get_rev_var();
+
+	if (EXTRACT_PARTNUM(midr_val) == EXTRACT_PARTNUM(CORTEX_A520_MIDR)) {
+		return check_erratum_cortex_a520_2938996(rev_var);
+	} else if (EXTRACT_PARTNUM(midr_val) == EXTRACT_PARTNUM(CORTEX_X4_MIDR)) {
+		return check_erratum_cortex_x4_2726228(rev_var);
+	}
+
+	return ERRATA_NOT_APPLIES;
+}
+#endif
