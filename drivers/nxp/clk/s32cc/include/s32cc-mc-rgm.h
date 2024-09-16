11a7c54072f651512948446e432421ba7ee57469
Ghennadi Procopciuc <ghennadi.procopciuc@nxp.com>
2024-09-16 16:20:08 +0300

    feat(nxp-clk): add partition reset utilities
    MC_RGM is a hardware block involved in resetting peripherals and
    partitions. Here, the accessories for partition reset are added.
    
    Change-Id: If00755fe0e93ba2e4841f95ed5ae3c87db20bebf
    Signed-off-by: Ghennadi Procopciuc <ghennadi.procopciuc@nxp.com>

--- drivers/nxp/clk/s32cc/include/s32cc-mc-rgm.h
+++ drivers/nxp/clk/s32cc/include/s32cc-mc-rgm.h
@@ -8,5 +8,7 @@
 #include <stdint.h>
 
 void mc_rgm_periph_reset(uintptr_t rgm, uint32_t part, uint32_t value);
+void mc_rgm_release_part(uintptr_t rgm, uint32_t part);
+void mc_rgm_wait_part_deassert(uintptr_t rgm, uint32_t part);
 
 #endif /* MC_RGM_H */
