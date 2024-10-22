d51c8e4c654a27e7487b7d15a64cec6f5eeab4e5
Maheedhar Bollapalli <maheedharsai.bollapalli@amd.com>
2024-10-22 06:53:27 +0000

    fix(versal-net): typecast operands to match data type
    This corrects the MISRA violation C2012-10.3:
    The value of an expression shall not be assigned to an object with a
    narrower essential type or of a different essential type category.
    The condition is explicitly checked against 0U, appending 'U' and
    typecasting for unsigned comparison.
    
    Change-Id: Ie2d32d5554d251cde8a9c8b7c7a85666ea505a15
    Signed-off-by: Maheedhar Bollapalli <maheedharsai.bollapalli@amd.com>

--- plat/xilinx/versal_net/bl31_versal_net_setup.c
+++ plat/xilinx/versal_net/bl31_versal_net_setup.c
@@ -55,7 +55,7 @@ static inline void bl31_set_default_config(void)
 	bl32_image_ep_info.pc = BL32_BASE;
 	bl32_image_ep_info.spsr = arm_get_spsr_for_bl32_entry();
 	bl33_image_ep_info.pc = plat_get_ns_image_entrypoint();
-	bl33_image_ep_info.spsr = SPSR_64(MODE_EL2, MODE_SP_ELX,
+	bl33_image_ep_info.spsr = (uint32_t)SPSR_64(MODE_EL2, MODE_SP_ELX,
 					DISABLE_ALL_EXCEPTIONS);
 }
 
--- plat/xilinx/versal_net/plat_psci_pm.c
+++ plat/xilinx/versal_net/plat_psci_pm.c
@@ -245,7 +245,7 @@ static int32_t versal_net_validate_power_state(unsigned int power_state,
 {
 	VERBOSE("%s: power_state: 0x%x\n", __func__, power_state);
 
-	int32_t pstate = psci_get_pstate_type(power_state);
+	uint32_t pstate = psci_get_pstate_type(power_state);
 
 	assert(req_state != NULL);
 
