3e6cabb33c2857adeab9df9ef8cdffddd727e43a
Dhruva Gole <d-gole@ti.com>
2024-12-11 13:18:37 +0530

    feat(ti): Introduce basic support for the AM62L
    The AM62L is a lite, low power and performance optimized family of
    application processors that are built for Linux application development.
    
    Some highlights of AM62L SoC are:
     - Single to Dual 64-bit Arm® Cortex®-A53 microprocessor subsystem
     - 16-bit DDR Subsystem that supports LPDDR4, DDR4 memory types.
     - Multiple low power modes support, ex: Deep sleep and RTC+DDR
     - Mailbox transport layer for TI SCI
    
    For more information check out our Technical Reference Manual (TRM)
    which is loacted here:
    
    	https://www.ti.com/lit/pdf/sprujb4
    
    Change-Id: I9d7c707b5b220c5ec13bd2de67f872b3da3c308a
    Signed-off-by: Dhruva Gole <d-gole@ti.com>

---
+++ plat/ti/k3/board/am62l/include/board_def.h
@@ -0,0 +1,23 @@
+/*
+ * Copyright (C) 2024 Texas Instruments Incorporated - https://www.ti.com
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef BOARD_DEF_H
+#define BOARD_DEF_H
+
+#include <lib/utils_def.h>
+
+/* The ports must be in order and contiguous */
+#define K3_CLUSTER0_CORE_COUNT		U(2)
+#define K3_CLUSTER1_CORE_COUNT		U(0)
+#define K3_CLUSTER2_CORE_COUNT		U(0)
+#define K3_CLUSTER3_CORE_COUNT		U(0)
+
+#define PLAT_PROC_START_ID		U(32)
+#define PLAT_PROC_DEVICE_START_ID	U(135)
+#define PLAT_CLUSTER_DEVICE_START_ID	U(134)
+#define PLAT_BOARD_DEVICE_ID		U(157)
+
+#endif /* BOARD_DEF_H */
