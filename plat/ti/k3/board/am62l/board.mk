9adfeb19e3491db6b2172022ac5f83fd2465f2ad
Dhruva Gole <d-gole@ti.com>
2024-12-11 13:18:37 +0530

    FROMLIST: feat(ti): Introduce basic support for the AM62L
    The AM62L is a lite, low power and performance optimized family of
    application processors that are built for Linux application development.
    
    Some highlights of AM62L SoC are:
     - Single to Dual 64-bit Arm® Cortex®-A53 microprocessor subsystem
     - 16-bit DDR Subsystem that supports LPDDR4, DDR4 memory types.
     - Multiple low power modes support, ex: Deep sleep and RTC+DDR
     - Mailbox transport layer for TI SCI
    
    For more information check out our Technical Reference Manual (TRM)
    which is loacted here:
    
    	https://www.ti.com/lit/pdf/sprujb4
    
    Link:
    https://review.trustedfirmware.org/c/TF-A/trusted-firmware-a/+/33969/6
    Change-Id: I9d7c707b5b220c5ec13bd2de67f872b3da3c308a
    Signed-off-by: Dhruva Gole <d-gole@ti.com>

---
+++ plat/ti/k3/board/am62l/board.mk
@@ -0,0 +1,21 @@
+#
+# Copyright (c) 2024, Texas Instruments Inc. All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+#
+
+# We dont have system level coherency capability
+USE_COHERENT_MEM	:=	0
+K3_TI_SCI_MAILBOX	:=	1
+$(eval $(call add_define,K3_TI_SCI_MAILBOX))
+
+K3_TI_SCI_TRANSPORT    =      ${PLAT_PATH}/common/drivers/mailbox/mailbox.c
+
+BL32_BASE ?= 0x80200000
+$(eval $(call add_define,BL32_BASE))
+
+PRELOADED_BL33_BASE ?= 0x82000000
+$(eval $(call add_define,PRELOADED_BL33_BASE))
+
+K3_HW_CONFIG_BASE ?= 0x88000000
+$(eval $(call add_define,K3_HW_CONFIG_BASE))
