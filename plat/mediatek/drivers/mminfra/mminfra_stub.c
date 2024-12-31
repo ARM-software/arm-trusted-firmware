4794746eec6191563a97fec55c06027e42abe9bb
Yong Wu <yong.wu@mediatek.com>
2024-12-31 09:58:28 +0800

    feat(mt8196): add Mediatek MMinfra stub implementation
    Implement stub functions for the MMinfra (Multimedia Infrastructure)
    driver to ensure that the build can pass when a prebuilt library is
    not available.
    
    Change-Id: Iadac654950c868d3743b13a1d6f7ab5d1015fb86
    Signed-off-by: Yong Wu <yong.wu@mediatek.com>

---
+++ plat/mediatek/drivers/mminfra/mminfra_stub.c
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2025, MediaTek Inc. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <drivers/mminfra_public.h>
+
+int mminfra_get_if_in_use(void)
+{
+	return 0;
+}
+
+int mminfra_put(void)
+{
+	return 0;
+}
