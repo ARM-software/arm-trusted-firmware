ae6c7c97d4e0f491854b34628e0fa1038668f8e4
Govindraj Raja <govindraj.raja@arm.com>
2025-01-21 18:02:51 -0600

    fix(cpus): workaround for Cortex-X2 erratum 3701772
    Cortex-X2 erratum 3701772 that applies to r0p0, r1p0, r2p0, r2p1
    is still Open.
    
    The workaround is for EL3 software that performs context save/restore
    on a change of Security state to use a value of SCR_EL3.NS when
    accessing ICH_VMCR_EL2 that reflects the Security state that owns the
    data being saved or restored.
    
    SDEN documentation:
    https://developer.arm.com/documentation/SDEN-1775100/latest/
    
    Change-Id: I2ffc5e7d7467f1bcff8b895fea52a1daa7d14495
    Signed-off-by: Govindraj Raja <govindraj.raja@arm.com>

--- lib/cpus/cpu-ops.mk
+++ lib/cpus/cpu-ops.mk
@@ -786,6 +786,11 @@ CPU_FLAG_LIST += ERRATA_X2_2768515
 # to revisions r0p0, r1p0, r2p0, r2p1 of the Cortex-X2 cpu and it is still open.
 CPU_FLAG_LIST += ERRATA_X2_2778471
 
+# Flag to apply erratum 3701772 workaround during context save/restore of
+# ICH_VMCR_EL2 reg. This erratum applies to revisions r0p0, r1p0, r2p0 and r2p1
+# of the Cortex-X2 cpu and is still open.
+CPU_FLAG_LIST += ERRATA_X2_3701772
+
 # Flag to apply erratum 2070301 workaround on reset. This erratum applies
 # to revisions r0p0, r1p0, r1p1 and r1p2 of the Cortex-X3 cpu and is
 # still open.
