a72f9ec81b2019387e20e6654b1a89b6f28769fe
Hari Nagalla <hnagalla@ti.com>
2025-01-28 15:30:48 -0600

    PENDING: feat(ti): Add support for bl1 builds
    Add build support for bl1. bl1 uses a device tree based lpddr4
    configuration.
    
    Signed-off-by: Hari Nagalla <hnagalla@ti.com>

--- plat/ti/k3/board/am62l/board.mk
+++ plat/ti/k3/board/am62l/board.mk
@@ -8,6 +8,67 @@
 USE_COHERENT_MEM	:=	0
 K3_TI_SCI_MAILBOX	:=	1
 $(eval $(call add_define,K3_TI_SCI_MAILBOX))
+DDR_16BIT	:=	1
+$(eval $(call add_define,DDR_16BIT))
+
+K3_SPL_IMG_OFFSET	:=	0x80000
+$(eval $(call add_define,K3_SPL_IMG_OFFSET))
+
+ifeq (${IMAGE_BL1}, 1)
+override ENABLE_PIE := 0
+endif
+
+DTB_FILE_NAME ?= k3-am62l-ddr.dtb
+
+$(eval $(call MAKE_LIB_DIRS))
+
+include lib/libfdt/libfdt.mk
+
+define add_tfcflag
+	TF_CFLAGS_aarch64	+= -Wno-address-of-packed-member
+endef
+
+define add_asflag
+	ASFLAGS 		+= -DBL1_DTB_PATH=\"${BUILD_PLAT}/fdts/$(DTB_FILE_NAME)\"
+	ASFLAGS 		+= -DDTB_ARRAY_SIZE=9400
+endef
+
+define add_dtb
+am62l_bl1: bl1 dtbs
+	./${PLAT_PATH}/common/drivers/lpddr4/am62l-bl1-dtb.sh ${BUILD_PLAT}/bl1/bl1.elf ${BUILD_PLAT}/fdts/$(DTB_FILE_NAME) ${BUILD_PLAT}/bl1.bin
+
+all: am62l_bl1
+endef
+
+$(eval $(call add_tfcflag))
+$(eval $(call add_asflag))
+$(eval $(call add_dtb))
+
+FDT_SOURCES	:= fdts/$(patsubst %.dtb,%.dts,$(DTB_FILE_NAME)) \
+
+PLAT_INCLUDES		+=	\
+				-I${PLAT_PATH}/common/drivers/lpddr4	\
+				-I${PLAT_PATH}/common/drivers/lpddr4/common	\
+				-I${PLAT_PATH}/common/drivers/lpddr4/16bit	\
+				-Iinclude/lib/libfdt	\
+
+K3_LPDDR4_SOURCES	+= 	\
+				${PLAT_PATH}/common/drivers/lpddr4/k3-ddrss.c \
+				${PLAT_PATH}/common/drivers/lpddr4/lpddr4_obj_if.c \
+				${PLAT_PATH}/common/drivers/lpddr4/lpddr4.c \
+				${PLAT_PATH}/common/drivers/lpddr4/lpddr4_16bit_ctl_regs_rw_masks.c \
+				${PLAT_PATH}/common/drivers/lpddr4/lpddr4_16bit.c \
+				${PLAT_PATH}/common/drivers/lpddr4/k3-ddr-dtb.S \
+
+
+BL1_SOURCES		+=	\
+				${PLAT_PATH}/common/k3_bl1_setup.c	\
+				${PLAT_PATH}/common/k3_helpers.S	\
+				${PLAT_PATH}/common/k3_topology.c	\
+				drivers/io/io_storage.c \
+				${K3_LPDDR4_SOURCES}			\
+				${K3_TI_SCI_TRANSPORT}	\
+
 
 K3_TI_SCI_TRANSPORT    =      ${PLAT_PATH}/common/drivers/mailbox/mailbox.c
 K3_PSCI_SOURCES		+=	\
