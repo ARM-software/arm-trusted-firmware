5227171c079d0c7d22a6d37b5cec079c8c5bb4b1
Vignesh Raghavendra <vigneshr@ti.com>
2025-02-03 14:50:32 +0530

    PENDING: fix(ti): am62l: Make am62l-bl1-dtb.sh more readable
    Don't depend on yet another tool "bc" for simple hex to dec conversion as
    not every build system may have access to such host tools.
    Instead just subtract dtb_array offset from text base and use printf
    for base conversion.
    While at that, silence dd output by adding status=none
    
    Signed-off-by: Vignesh Raghavendra <vigneshr@ti.com>

--- plat/ti/k3/common/drivers/lpddr4/am62l-bl1-dtb.sh
+++ plat/ti/k3/common/drivers/lpddr4/am62l-bl1-dtb.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-dtb_offset=$(readelf -s $1 | grep dtb_array | tr -s " " | cut -d " " -f 3 | cut -c12-16)
-dtb_seek=$(echo "ibase=16; $dtb_offset" | bc)
-dd if=$2 of=$3 bs=1 seek=$dtb_seek conv=notrunc
+dtb_array_addr=$(readelf -s $1 | grep dtb_array | tr -s " " | cut -d " " -f 3)
+text_addr=$(readelf -s $1 | grep "\.text" | tr -s " " | cut -d " " -f 3)
+dtb_seek=$(printf "%d" $((0x$dtb_array_addr - 0x$text_addr)))
+dd if=$2 of=$3 bs=1 seek=$dtb_seek conv=notrunc status=none
