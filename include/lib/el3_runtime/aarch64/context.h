c72200357aed49fd51dc21e45d4396f5402df811
Manish Pandey <manish.pandey2@arm.com>
2025-02-03 12:00:56 +0000

    fix(el3-runtime): replace CTX_ESR_EL3 with CTX_DOUBLE_FAULT_ESR
    ESR_EL3 value is updated when an exception is taken to EL3 and its value
    does not change until a new exception is taken to EL3. We need to save
    ESR in context memory only when we expect nested exception in EL3.
    
    The scenarios where we would expect nested EL3 execution are related
    with FFH_SUPPORT, namely
      1.Handling pending async EAs at EL3 boundry
        - It uses CTX_SAVED_ESR_EL3 to preserve origins esr_el3
      2.Double fault handling
        - Introduce an explicit storage (CTX_DOUBLE_FAULT_ESR) for esr_el3
          to take care of DobuleFault.
    
    As the ESR context has been removed, read the register directly instead
    of its context value in RD platform.
    
    Signed-off-by: Manish Pandey <manish.pandey2@arm.com>
    Change-Id: I7720c5f03903f894a77413a235e3cc05c86f9c17

--- include/lib/el3_runtime/aarch64/context.h
+++ include/lib/el3_runtime/aarch64/context.h
@@ -67,25 +67,25 @@
  ******************************************************************************/
 #define CTX_EL3STATE_OFFSET	(CTX_GPREGS_OFFSET + CTX_GPREGS_END)
 #define CTX_SCR_EL3		U(0x0)
-#define CTX_ESR_EL3		U(0x8)
-#define CTX_RUNTIME_SP		U(0x10)
-#define CTX_SPSR_EL3		U(0x18)
-#define CTX_ELR_EL3		U(0x20)
-#define CTX_PMCR_EL0		U(0x28)
-#define CTX_IS_IN_EL3		U(0x30)
-#define CTX_MDCR_EL3		U(0x38)
+#define CTX_RUNTIME_SP		U(0x8)
+#define CTX_SPSR_EL3		U(0x10)
+#define CTX_ELR_EL3		U(0x18)
+#define CTX_PMCR_EL0		U(0x20)
+#define CTX_IS_IN_EL3		U(0x28)
+#define CTX_MDCR_EL3		U(0x30)
 /* Constants required in supporting nested exception in EL3 */
-#define CTX_SAVED_ELR_EL3	U(0x40)
+#define CTX_SAVED_ELR_EL3	U(0x38)
 /*
  * General purpose flag, to save various EL3 states
  * FFH mode : Used to identify if handling nested exception
  * KFH mode : Used as counter value
  */
-#define CTX_NESTED_EA_FLAG	U(0x48)
+#define CTX_NESTED_EA_FLAG	U(0x40)
 #if FFH_SUPPORT
- #define CTX_SAVED_ESR_EL3	U(0x50)
- #define CTX_SAVED_SPSR_EL3	U(0x58)
- #define CTX_SAVED_GPREG_LR	U(0x60)
+ #define CTX_SAVED_ESR_EL3	U(0x48)
+ #define CTX_SAVED_SPSR_EL3	U(0x50)
+ #define CTX_SAVED_GPREG_LR	U(0x58)
+ #define CTX_DOUBLE_FAULT_ESR	U(0x60)
  #define CTX_EL3STATE_END	U(0x70) /* Align to the next 16 byte boundary */
 #else
  #define CTX_EL3STATE_END	U(0x50) /* Align to the next 16 byte boundary */
