1e0fca13d9a4d2a0791a572c9e42207ae53bc0ad
Govindraj Raja <govindraj.raja@arm.com>
2025-02-07 15:17:09 -0600

    fix(cpus): workaround for Cortex-X925 erratum 2963999
    Cortex-X925 erratum 2963999 that applies to r0p0 and is fixed in
    r0p1.
    
    In EL3, reads of MPIDR_EL1 and MIDR_EL1 might incorrectly virtualize
    which register to return when reading the value of
    MPIDR_EL1/VMPIDR_EL2 and MIDR_EL1/VPIDR_EL2, respectively.
    
    The workaround is to do an ISB prior to an MRS read to either
    MPIDR_EL1 and MIDR_EL1.
    
    SDEN documentation:
    https://developer.arm.com/documentation/109180/latest/
    
    Change-Id: I447fd359ea32e1d274e1245886e1de57d14f082c
    Signed-off-by: Govindraj Raja <govindraj.raja@arm.com>
    (cherry picked from commit 29bda258d5655d2ac24dda0f73e6b0ec93c3037e)

--- lib/cpus/cpu-ops.mk
+++ lib/cpus/cpu-ops.mk
@@ -903,6 +903,10 @@ CPU_FLAG_LIST += ERRATA_X4_3076789
 # of the Cortex-X4 cpu and is still open.
 CPU_FLAG_LIST += ERRATA_X4_3701758
 
+# Flag to apply erratum 2963999 workaround during reset. This erratum applies
+# to revisions r0p0 of the Cortex-X925 cpu and is fixed in r0p1.
+CPU_FLAG_LIST += ERRATA_X925_2963999
+
 # Flag to apply erratum 3701747 workaround during context save/restore of
 # ICH_VMCR_EL2 reg. This erratum applies to revisions r0p0, r0p1 of the
 # Cortex-X925 cpu and is still open.
