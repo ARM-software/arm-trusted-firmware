08963618c717fa808552d2d7b63f71d63f3ecb6a
Yann Gautier <yann.gautier@st.com>
2025-02-07 17:15:29 +0100

    refactor(arm): switch to rse_mbx_init
    The rse_comms_init() function will be removed. The new function to use
    is rse_mbx_init() for the MHU mailbox initialization.
    
    Change-Id: I1932500ef71b6e895f0ee164ee9c2b58becf4409
    Signed-off-by: Yann Gautier <yann.gautier@st.com>

--- plat/arm/board/tc/tc_rse_comms.c
+++ plat/arm/board/tc/tc_rse_comms.c
@@ -5,13 +5,19 @@
  */
 
 #include <common/debug.h>
+#include <drivers/arm/mhu.h>
 #include <drivers/arm/rse_comms.h>
 #include <platform_def.h>
 #include <tc_rse_comms.h>
+
+static const struct mhu_addr mhu_addresses = {
+	PLAT_RSE_AP_SND_MHU_BASE,
+	PLAT_RSE_AP_RCV_MHU_BASE
+};
 
 int plat_rse_comms_init(void)
 {
 	VERBOSE("Initializing the rse_comms now\n");
 	/* Initialize the communication channel between AP and RSE */
-	return rse_comms_init(PLAT_RSE_AP_SND_MHU_BASE, PLAT_RSE_AP_RCV_MHU_BASE);
+	return rse_mbx_init(&mhu_addresses);
 }
