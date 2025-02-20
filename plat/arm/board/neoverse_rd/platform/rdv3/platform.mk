fe488c3796e01187fb6cffdd27a1bee1a33e0931
Govindraj Raja <govindraj.raja@arm.com>
2025-02-20 17:19:46 -0600

    fix(rdv3): handle invalid build combination
    `CTX_INCLUDE_SVE_REGS` should not be enabled when building with
    SPD=spmd and SPMD_SPM_AT_SEL2=1 both been used.
    
    Unfortunately a check at top level makefile ignored this, now its been
    fixed at top level makefile. Ensure correct combination are handled,
    otherwise it will lead to build failures.
    
    Change-Id: Ib84fc0096c92d9b3d56366c0e1d77b6d83098221
    Signed-off-by: Govindraj Raja <govindraj.raja@arm.com>

--- plat/arm/board/neoverse_rd/platform/rdv3/platform.mk
+++ plat/arm/board/neoverse_rd/platform/rdv3/platform.mk
@@ -144,7 +144,7 @@ FDT_SOURCES	+=	${RDV3_BASE}/fdts/${PLAT}_fw_config.dts	\
 			${RDV3_BASE}/fdts/${PLAT}_nt_fw_config.dts
 
 ifeq (${SPMD_SPM_AT_SEL2}, 1)
-BL32_CONFIG_DTS                :=      ${RDV3_BASE}/fdts/${PLAT}_spmc_sp_manifest.dts
+BL32_CONFIG_DTS        :=      ${RDV3_BASE}/fdts/${PLAT}_spmc_sp_manifest.dts
 FDT_SOURCES            +=      ${BL32_CONFIG_DTS}
 TOS_FW_CONFIG          :=      ${BUILD_PLAT}/fdts/$(notdir $(basename ${BL32_CONFIG_DTS})).dtb
 endif
@@ -169,4 +169,14 @@ override ENABLE_FEAT_MTE2	:= 2
 
 # FEAT_SVE related flags
 override SVE_VECTOR_LEN		:= 128
-override CTX_INCLUDE_SVE_REGS	:= 1
+
+override CTX_INCLUDE_SVE_REGS   := 1
+
+# Enabling CTX_INCLUDE_SVE_REGS along with SPMD_SPM_AT_SEL2=1 is a invalid
+# combination and will lead to build failure, use them only when SPMD_SPM_AT_SEL2=0
+# In this combination its SPMC responsbility to save SVE regs.
+ifeq (${SPD},spmd)
+ifeq (${SPMD_SPM_AT_SEL2},1)
+override CTX_INCLUDE_SVE_REGS	:= 0
+endif
+endif
