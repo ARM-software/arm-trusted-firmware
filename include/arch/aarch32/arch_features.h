10ecd58093a34e95e2dfad65b1180610f29397cc
Boyan Karatotev <boyan.karatotev@arm.com>
2025-03-26 15:54:55 +0000

    feat(cpufeat): enable FEAT_BTI to FEAT_STATE_CHECKED
    Introduce the is_feat_bti_{supported, present}() helpers and replace
    checks for ENABLE_BTI with it. Also factor out the setting of
    SCTLR_EL3.BT out of the PAuth enablement and place it in the respective
    entrypoints where we initialise SCTLR_EL3. This makes PAuth
    self-contained and SCTLR_EL3 initialisation centralised.
    
    Change-Id: I0c0657ff1e78a9652cd2cf1603478283dc01f17b
    Signed-off-by: Boyan Karatotev <boyan.karatotev@arm.com>

--- include/arch/aarch32/arch_features.h
+++ include/arch/aarch32/arch_features.h
@@ -204,5 +204,7 @@ __attribute__((always_inline))
 static inline bool is_feat_ls64_accdata_present(void) { return false; }
 __attribute__((always_inline))
 static inline bool is_feat_mops_supported(void) { return false; }
+__attribute__((always_inline))
+static inline bool is_feat_bti_supported(void) { return false; }
 
 #endif /* ARCH_FEATURES_H */
