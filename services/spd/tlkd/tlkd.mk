c2388515a3a44d0a67c12750d8977ce6277078ed
Govindraj Raja <govindraj.raja@arm.com>
2025-04-10 10:38:24 -0500

    build(tlkd): add build error when building with FEAT_D128
    tlkd is not fully adopted to work with FEAT_D128, currently we avoid
    building tlkd when FEAT_D128 is enabled.
    
    Add a build error when we build tlkd with FEAT_D128.
    
    Change-Id: I6ebc0eb8b551701897a62e0d800e786ce81beadf
    Signed-off-by: Govindraj Raja <govindraj.raja@arm.com>

--- services/spd/tlkd/tlkd.mk
+++ services/spd/tlkd/tlkd.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (c) 2024, ARM Limited and Contributors. All rights reserved.
+# Copyright (c) 2024-2025, ARM Limited and Contributors. All rights reserved.
 #
 # SPDX-License-Identifier: BSD-3-Clause
 #
@@ -8,9 +8,11 @@ ifeq (${ERROR_DEPRECATED},0)
 SPD_INCLUDES		:=	-Iinclude/bl32/payloads
 endif
 
-ifeq (${ENABLE_FEAT_D128}, 0)
+ifneq (${ENABLE_FEAT_D128}, 0)
+        $(error spd=tlkd will not work with ENABLE_FEAT_D128 enabled.)
+endif
+
 SPD_SOURCES		:=	services/spd/tlkd/tlkd_common.c		\
 				services/spd/tlkd/tlkd_helpers.S	\
 				services/spd/tlkd/tlkd_main.c		\
 				services/spd/tlkd/tlkd_pm.c
-endif
