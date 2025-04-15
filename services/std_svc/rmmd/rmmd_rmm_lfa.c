f024c573042f7ad29c41e68d261c859b91c6dcde
Manish V Badarkhe <Manish.Badarkhe@arm.com>
2025-04-15 20:16:38 +0100

    feat(lfa): enable LFA of RMM
    Extend the LFA framework to handle the activation of RMM image.
    This allows RMM to be updated and re-initialized as part of
    LFA without requiring a system reset.
    
    Signed-off-by: Manish V Badarkhe <Manish.Badarkhe@arm.com>
    Change-Id: I6ca3e21f6fe07ac3ab0bec1656d295129b62b8f5

--- services/std_svc/rmmd/rmmd_rmm_lfa.c
+++ services/std_svc/rmmd/rmmd_rmm_lfa.c
@@ -4,18 +4,63 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
+#include <lib/el3_runtime/context_mgmt.h>
+#include <plat/common/platform.h>
+#include <services/lfa_holding_pen.h>
 #include <services/lfa_svc.h>
 #include <services/rmmd_rmm_lfa.h>
+#include <services/rmmd_svc.h>
 
 static int lfa_rmm_prime(struct lfa_component_status *activation)
 {
-	return LFA_WRONG_STATE;
+	/* RMM-specific PRIME actions. Currently, no action is required. */
+
+	return LFA_SUCCESS;
 }
 
 static int lfa_rmm_activate(struct lfa_component_status *activation,
 				uint64_t ep_address, uint64_t context_id)
 {
-	return LFA_WRONG_STATE;
+	int ret = LFA_SUCCESS;
+
+	if (lfa_holding_start()) {
+		/* Leader CPU */
+		INFO("LFA_ACTIVATE: Last CPU proceed with activation\n");
+
+		cm_el2_sysregs_context_save(NON_SECURE);
+		ret = rmmd_primary_activate();
+		cm_el2_sysregs_context_restore(NON_SECURE);
+
+		cm_set_next_eret_context(NON_SECURE);
+
+		if (ret == LFA_SUCCESS) {
+			VERBOSE("Successful activation of component %d\n",
+				activation->component_id);
+		} else {
+			ERROR("Failed to activate component %d\n", activation->component_id);
+			ret = LFA_BUSY;
+		}
+
+		lfa_holding_release(ret);
+
+		/**
+		 * TODO: avoid race condition trying to prime next firmware
+		 * before secondary activations are finished
+		 */
+		lfa_reset_activation();
+	} else {
+		ret = lfa_holding_wait();
+
+		if (ret == LFA_SUCCESS) {
+			cm_el2_sysregs_context_save(NON_SECURE);
+			ret = rmmd_secondary_activate();
+			cm_el2_sysregs_context_restore(NON_SECURE);
+
+			cm_set_next_eret_context(NON_SECURE);
+		}
+	}
+
+	return ret;
 }
 
 static struct lfa_component_ops rmm_activator = {
