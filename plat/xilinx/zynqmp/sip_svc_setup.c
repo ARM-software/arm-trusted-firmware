55f6ea4dadb20c52cf837fba2cc344547ac8baca
Prasad Kummari <prasad.kummari@amd.com>
2025-04-16 11:20:38 +0530

    refactor(zynqmp): refactor custom sip service
    Refactored the custom SIP service code and relocated it to a
    common directory to support additional platforms and enhance
    code readability. Renamed ZYNQMP_SIP_SVC_CUSTOM to a generic
    SOC_SIP_SVC_CUSTOM for better with other across SoCs
    
    Change-Id: Id114f717d0397e257a831c7d5b67104c2c766d09
    Signed-off-by: Prasad Kummari <prasad.kummari@amd.com>

--- plat/xilinx/zynqmp/sip_svc_setup.c
+++ plat/xilinx/zynqmp/sip_svc_setup.c
@@ -105,8 +105,8 @@ static uintptr_t sip_svc_smc_handler(uint32_t smc_fid,
 	case ZYNQMP_SIP_SVC_VERSION:
 		SMC_RET2(handle, SIP_SVC_VERSION_MAJOR, SIP_SVC_VERSION_MINOR);
 
-	case ZYNQMP_SIP_SVC_CUSTOM:
-	case ZYNQMP_SIP_SVC64_CUSTOM:
+	case SOC_SIP_SVC_CUSTOM:
+	case SOC_SIP_SVC64_CUSTOM:
 		return custom_smc_handler(smc_fid, x1, x2, x3, x4, cookie,
 					  handle, flags);
 
