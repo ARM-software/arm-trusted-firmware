3618d5aac05a9485336af96a1e0b1d396b98ac18
Release CI <ci@trustedfirmware.org>
2025-04-25 16:26:41 +0000

    docs(changelog): changelog for lts-v2.12.2 release
    Change-Id: Ifa34c36c9043d565d28fad21e5f65866f72e01f6
    Signed-off-by: Release CI <ci@trustedfirmware.org>

--- docs/conf.py
+++ docs/conf.py
@@ -14,8 +14,8 @@
 
 project = "Trusted Firmware-A"
 author = "Trusted Firmware-A contributors"
-version = "2.12.1"
-release = "2.12.1"
+version = "2.12.2"
+release = "2.12.2"
 
 # -- General configuration ---------------------------------------------------
 
