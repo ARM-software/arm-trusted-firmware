9c18c35cbb21632d817ed2650ce80e5d544cac3b
Lauren Wehrmeister <lauren.wehrmeister@arm.com>
2025-04-28 15:41:23 -0500

    feat(auth): add update of current_pk_oid in auth
    Adding the set of current_pk_oid during the authentication process,
    include the new file in auth makefile.
    
    Signed-off-by: Lauren Wehrmeister <lauren.wehrmeister@arm.com>
    Change-Id: I3e05b8607060b424e34642d23e4960d2ef0f71f0

--- drivers/auth/auth.mk
+++ drivers/auth/auth.mk
@@ -7,5 +7,6 @@
 AUTH_SRC_DIR		:= drivers/auth/
 
 AUTH_SOURCES		:= ${AUTH_SRC_DIR}auth_mod.c		\
+			   ${AUTH_SRC_DIR}auth_util.c		\
 			   ${AUTH_SRC_DIR}crypto_mod.c		\
 			   ${AUTH_SRC_DIR}img_parser_mod.c
