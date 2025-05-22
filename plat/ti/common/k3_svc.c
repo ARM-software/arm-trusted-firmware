db97e3a17de7642c72de27c98a6cb87663f3d132
Vignesh Raghavendra <vigneshr@ti.com>
2025-05-22 12:49:25 +0530

    PENDING: feat(ti): Add SiP SVC calls for fuse programming
    Add Silicon Vendor specific SVC calls to read and write to general
    purpose OTP fuses using TISCI calls underneath.
    
    These fuses may contain user keys etc and thus better handled in
    trusted environment (TF-A).
    
    Change-Id: I6dfd1d5e53e3a9de317a4407c27b67076938cc3d
    Signed-off-by: Vignesh Raghavendra <vigneshr@ti.com>
    Signed-off-by: Anshul Dalal <anshuld@ti.com>

--- plat/ti/common/k3_svc.c
+++ plat/ti/common/k3_svc.c
@@ -11,9 +11,10 @@
 #include <common/runtime_svc.h>
 #include <drivers/scmi-msg.h>
 
-#include <lib/mmio.h>
-#include <tools_share/uuid.h>
 #include <k3_sip_svc.h>
+#include <lib/mmio.h>
+#include <ti_sci.h>
+#include <tools_share/uuid.h>
 
 /* K3 SiP Service UUID */
 DEFINE_SVC_UUID2(ti_sip_svc_uid,
@@ -32,11 +33,12 @@ uintptr_t sip_smc_handler(uint32_t smc_fid,
 			  void *handle,
 			  u_register_t flags)
 {
+	int ret = SMC_UNK;
+
 	switch (smc_fid) {
 	case SIP_SVC_CALL_COUNT:
 		/* Return the number of TI K3 SiP Service Calls. */
-		SMC_RET1(handle,
-			 K3_COMMON_SIP_NUM_CALLS);
+		SMC_RET1(handle, K3_COMMON_SIP_NUM_CALLS);
 
 	case SIP_SVC_UID:
 		/* Return UID to the caller */
@@ -53,6 +55,42 @@ uintptr_t sip_smc_handler(uint32_t smc_fid,
 
 	case K3_SIP_OTP_WRITEBUFF:
 		SMC_RET1(handle, ti_fuse_writebuff_handler(x1));
+
+	case K3_SIP_OTP_READ:
+		uint32_t mmr_val = 0;
+
+		/*
+		 * 0x00 - 0xFE is reserved for user OTP, 0xFF is
+		 * reserved for bootmode OTP which doesnt support
+		 * readback
+		 */
+		if (x1 < 0xff)
+			ret = ti_sci_read_otp(x1, x2, &mmr_val);
+		else
+			ERROR("%s: (K3_SIP_OTP_READ) tried reading from invalid bank '0x%lx'\n",
+			      __func__, x1);
+
+		SMC_RET2(handle, ret, mmr_val);
+
+	case K3_SIP_OTP_WRITE:
+		/*
+		 * 0x00 - 0xFE is reserved for user OTP, 0xFF is
+		 * reserved for bootmode OTP programming
+		 */
+		if (x1 < 0xff) {
+			uint32_t row_val = 0;
+
+			ret = ti_sci_write_otp(x1, x2, x3, x4, &row_val);
+
+			SMC_RET2(handle, ret, row_val);
+		} else if (x1 == 0xff) {
+			SMC_RET2(handle, ti_sci_set_otp_bootmode(x2, x3), x3);
+		} else {
+			ERROR("%s: (K3_SIP_OTP_WRITE) tried writing to invalid bank '0x%lx'\n",
+			      __func__, x1);
+		}
+
+		SMC_RET1(handle, SMC_UNK);
 
 	default:
 		ERROR("%s: unhandled SMC (0x%x)\n", __func__, smc_fid);
