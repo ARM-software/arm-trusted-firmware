5a40ac16c0dbb658544b726cc92cf7679c1e5f00
Vignesh Raghavendra <vigneshr@ti.com>
2025-06-02 10:23:29 +0530

    PENDING: fix(ti): Flush OTP memory blob before handoff
    The OTP blob is ultimately consumed by the secure co-processor in the
    system and thus needs to be flushed to the point of cache coherency.
    These blobs are at most a couple of KBs in size, but flush the entire
    PAGE to be on the safer side.
    
    Without this change, the responsibility for flushing the cache for the
    blob being passed lies with the caller (like U-Boot), which isn't the
    right place since U-Boot has no knowledge of the backend implementation
    of the SVC call.
    
    Fixes: 7a76ef2f283d ("PENDING: feat(ti): add keywriter lite SMC handler")
    Signed-off-by: Vignesh Raghavendra <vigneshr@ti.com>

--- plat/ti/k3/common/k3_sip_handler.c
+++ plat/ti/k3/common/k3_sip_handler.c
@@ -4,6 +4,9 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
+#include <platform_def.h>
+
+#include <arch_helpers.h>
 #include <common/debug.h>
 #include <lib/smccc.h>
 #include <k3_sip_svc.h>
@@ -36,6 +39,13 @@ int ti_fuse_writebuff_handler(u_register_t x1)
 	k3_fuse_buff = (const struct k3_fuse_writebuff *)x1;
 	NOTICE("Writebuff version info: 0x%x\n", k3_fuse_buff->ver_info);
 	if (k3_fuse_buff->tisci_id == TISCI_MSG_KEY_WRITER_LITE) {
+
+		/*
+		 * Buffer needs to be visible to the TISCI co-processor, since
+		 * the size is unknown, flush the entire page
+		 */
+		flush_dcache_range((uintptr_t)k3_fuse_buff, PAGE_SIZE);
+
 		ret = ti_sci_keywriter_lite((unsigned long)&k3_fuse_buff->payload);
 		if (ret) {
 			ERROR("Keywriter Lite Failed: (%d)\n", ret);
