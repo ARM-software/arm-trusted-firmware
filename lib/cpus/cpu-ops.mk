b83bf0ad161f99a5264f5742bd0ffb9684fcf73a
John Powell <john.powell@arm.com>
2025-06-09 13:46:11 -0500

    fix(cpus): workaround for Cortex-A710 erratum 1916945
    Cortex-A710 erratum 1916945 is a Cat B erratum that applies
    to revisions r0p0 and r1p0 and is fixed in r2p0.
    
    The workaround is to set CPUECTLR_EL1[8]. This has a slight
    performance impact.
    
    SDEN documentation:
    https://developer.arm.com/documentation/SDEN1775101
    
    Change-Id: I54793492c527928d7f266165a31b8613de838e69
    Signed-off-by: John Powell <john.powell@arm.com>
    (cherry picked from commit df067c0a60b0c05554ebac51a37fb7d9ba866130)

--- lib/cpus/cpu-ops.mk
+++ lib/cpus/cpu-ops.mk
@@ -569,6 +569,10 @@ CPU_FLAG_LIST += ERRATA_V3_3701767
 # to revision r1p0 and is fixed in r2p0.
 CPU_FLAG_LIST += ERRATA_A710_1901946
 
+# Flag to apply erratum 1916945 workaround during reset. This erratum applies
+# to revisions r0p0 and r1p0 of the Cortex-A710 CPU and is fixed in r2p0.
+CPU_FLAG_LIST += ERRATA_A710_1916945
+
 # Flag to apply erratum 1987031 workaround during reset. This erratum applies
 # to revisions r0p0, r1p0 and r2p0 of the Cortex-A710 cpu and is still open.
 CPU_FLAG_LIST += ERRATA_A710_1987031
