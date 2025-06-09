d91c417716f260081e6a78f561224117088fc2e0
John Powell <john.powell@arm.com>
2025-06-09 14:32:39 -0500

    fix(cpus): workaround for Cortex-A710 erratum 1917258
    Cortex-A710 erratum 1917258 is a Cat B erratum that applies
    to revisions r0p0 and r1p0 and is fixed in r2p0.
    
    The workaround is to set CPUACTLR4_EL1[43]. This has no
    performance impact.
    
    SDEN documentation:
    https://developer.arm.com/documentation/SDEN1775101
    
    Change-Id: I1fae91a5e3a8ecea255f0f0a481bfd6196a7db51
    Signed-off-by: John Powell <john.powell@arm.com>

--- lib/cpus/cpu-ops.mk
+++ lib/cpus/cpu-ops.mk
@@ -569,6 +569,10 @@ CPU_FLAG_LIST += ERRATA_A710_1901946
 # to revisions r0p0 and r1p0 of the Cortex-A710 CPU and is fixed in r2p0.
 CPU_FLAG_LIST += ERRATA_A710_1916945
 
+# Flag to apply erratum 1917258 workaround during reset. This erratum applies
+# to revisions r0p0 and r1p0 of the Cortex-A710 CPU and is fixed in r2p0.
+CPU_FLAG_LIST += ERRATA_A710_1917258
+
 # Flag to apply erratum 1987031 workaround during reset. This erratum applies
 # to revisions r0p0, r1p0 and r2p0 of the Cortex-A710 cpu and is still open.
 CPU_FLAG_LIST += ERRATA_A710_1987031
