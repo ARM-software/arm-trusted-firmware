44fc77fef621d4ebc1aaf10b5da0c8c98a778e8a
Arvind Ram Prakash <arvind.ramprakash@arm.com>
2025-06-30 16:22:46 -0500

    fix(cpus): workaround for Cortex-X3 erratum 3692984
    Cortex-X3 erratum 3692984 is a Cat B erratum that applies to
    r0p0, r1p0, r1p1 and r1p2 and is still open.
    
    The erratum can be avoided by disabling the
    affected prefetcher setting CPUACTLR6_EL1[41].
    
    SDEN Documentation:
    https://developer.arm.com/documentation/SDEN-2055130/latest/
    
    Signed-off-by: Arvind Ram Prakash <arvind.ramprakash@arm.com>
    Change-Id: I054b47d33fd1ff7bde3ae12e8ee3d99e9203965f
    (cherry picked from commit f828efe258e148b2707249a65b2e31ab9718c4a6)

--- lib/cpus/cpu-ops.mk
+++ lib/cpus/cpu-ops.mk
@@ -846,6 +846,10 @@ CPU_FLAG_LIST += ERRATA_X3_3701769
 # to revisions r0p0, r1p0, r1p1 of the Cortex-X3 cpu, it is fixed in r1p2.
 CPU_FLAG_LIST += ERRATA_X3_2779509
 
+# Flag to apply erratum 3692984 workaround on reset. This erratum applies
+# to revisions r0p0, r1p0, r1p1, r1p2 of the Cortex-X3 cpu and is still open.
+CPU_FLAG_LIST += ERRATA_X3_3692984
+
 # Flag to apply erratum 2701112 workaround for platforms that do not use an
 # Arm interconnect IP. This erratum applies to revisions r0p0 of the Cortex-X4
 # cpu and is fixed in r0p1.
