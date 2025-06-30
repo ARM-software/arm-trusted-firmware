bdd1932c463f57fb066325b7b8392b97c06b69a1
Konrad Dybcio <konrad.dybcio@oss.qualcomm.com>
2025-06-30 18:12:39 +0200

    fix(qti): allow secure r/w to the EUD enable register
    The EUD (Embedded USB Debugger) is an IP block that can only be toggled
    from the secure world. Allow accessing its enable register through the
    SCM MMIO R/W calls.
    
    The remaining (non-qtiseclib) platforms don't feature this hardware.
    
    Compile-tested only, but should be simple enough.
    
    Change-Id: I2763fd89f2a9d068ec2b4662dc2de91d1d466666
    Signed-off-by: Konrad Dybcio <konrad.dybcio@oss.qualcomm.com>

--- plat/qti/qcs615/inc/qti_secure_io_cfg.h
+++ plat/qti/qcs615/inc/qti_secure_io_cfg.h
@@ -12,6 +12,7 @@
  * List of peripheral/IO memory areas that are protected from
  * non-secure world but not required to be secure.
  */
+#define EUD_MODE_MANAGER2_EN                 0x088E4000
 #define APPS_SMMU_TBU_PWR_STATUS             0x15002204
 #define APPS_SMMU_CUSTOM_CFG                 0x15002300
 #define APPS_SMMU_STATS_SYNC_INV_TBU_ACK     0x150025DC
@@ -19,6 +20,7 @@
 #define APPS_SMMU_MMU2QSS_AND_SAFE_WAIT_CNTR 0x15002648
 
 static const uintptr_t qti_secure_io_allowed_regs[] = {
+	EUD_MODE_MANAGER2_EN,
 	APPS_SMMU_TBU_PWR_STATUS,
 	APPS_SMMU_CUSTOM_CFG,
 	APPS_SMMU_STATS_SYNC_INV_TBU_ACK,
