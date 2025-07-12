4b559e72cf0cb7b20356feee3356d16eed25297e
John Powell <john.powell@arm.com>
2025-07-11 19:10:52 -0500

    fix(cpus): workaround for Cortex-X2 erratum 1901946
    Cortex-X2 erratum 1901946 is a Cat B erratum that applies to
    revision r1p0 and is fixed in r2p0.
    
    The workaround is to set CPUACTLR4_EL1[15]. This has a small
    performance impact.
    
    SDEN documentation:
    https://developer.arm.com/documentation/SDEN-1775100/latest
    
    Change-Id: I5a65db60f06982191994db49815419c4d72506cf
    Signed-off-by: John Powell <john.powell@arm.com>
    (cherry picked from commit ce64ea6ecb2b03eaf23fd4ec1460072ed472a92b)

--- lib/cpus/cpu-ops.mk
+++ lib/cpus/cpu-ops.mk
@@ -737,6 +737,10 @@ CPU_FLAG_LIST += ERRATA_N2_2779511
 # of the Neoverse N2 cpu and is still open.
 CPU_FLAG_LIST += ERRATA_N2_3701773
 
+# Flag to apply erratum 1901946 workaround during reset. This erratum applies
+# only to revision r1p0 of the Cortex-X2 cpu, it is fixed in r2p0.
+CPU_FLAG_LIST += ERRATA_X2_1901946
+
 # Flag to apply erratum 3699563 workaround during context save/restore of
 # ICH_VMCR_EL2 reg. This erratum applies to revision r0p0 of the Neoverse N3
 # cpu and is still open.
