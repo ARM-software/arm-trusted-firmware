ccee7fa879a4252b3e665b07c302a5fc47fd129d
John Powell <john.powell@arm.com>
2025-07-11 21:06:49 -0500

    fix(cpus): workaround for Cortex-X2 erratum 1917258
    Cortex-X2 erratum 1917258 is a Cat B erratum that applies to
    revisions r0p0 and r1p0 and is fixed in r2p0.
    
    The workaround is to set CPUACTLR4_EL1[43]. This has no
    performance impact.
    
    SDEN documentation:
    https://developer.arm.com/documentation/SDEN-1775100/latest
    
    Change-Id: Ic18a5179856f861701f09b2556906a6722db8150
    Signed-off-by: John Powell <john.powell@arm.com>

--- lib/cpus/cpu-ops.mk
+++ lib/cpus/cpu-ops.mk
@@ -750,6 +750,10 @@ CPU_FLAG_LIST += ERRATA_X2_1901946
 # to revisions r0p0 and r1p0 of the Cortex-X2 cpu, it is fixed in r2p0.
 CPU_FLAG_LIST += ERRATA_X2_1916945
 
+# Flag to apply erratum 1917258 workaround during reset. This erratum applies
+# to revisions r0p0 and r1p0 of the Cortex-X2 cpu, it is fixed in r2p0.
+CPU_FLAG_LIST += ERRATA_X2_1917258
+
 # Flag to apply erratum 2002765 workaround during reset. This erratum applies
 # to revisions r0p0, r1p0, and r2p0 of the Cortex-X2 cpu and is still open.
 CPU_FLAG_LIST += ERRATA_X2_2002765
