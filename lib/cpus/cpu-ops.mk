7054fa4e758e50841e620f2cd9b32c8bba2073e6
John Powell <john.powell@arm.com>
2025-07-11 19:27:15 -0500

    fix(cpus): workaround for Cortex-X2 erratum 1916945
    Cortex-X2 erratum 1916945 is a Cat B erratum that applies to
    revisions r0p0 and r1p0 and is fixed in r2p0.
    
    The workaround is to set CPUECTLR_EL1[8]. This has a small
    performance impact (<0.5%).
    
    SDEN documentation:
    https://developer.arm.com/documentation/SDEN-1775100/latest
    
    Change-Id: If810b1d0a07c43b3e1aa70d2ec88c1dcfa6f735f
    Signed-off-by: John Powell <john.powell@arm.com>
    (cherry picked from commit ff879c528056dd49b9fa18c544e832d6dc0dd75c)

--- lib/cpus/cpu-ops.mk
+++ lib/cpus/cpu-ops.mk
@@ -741,6 +741,10 @@ CPU_FLAG_LIST += ERRATA_N2_3701773
 # only to revision r1p0 of the Cortex-X2 cpu, it is fixed in r2p0.
 CPU_FLAG_LIST += ERRATA_X2_1901946
 
+# Flag to apply erratum 1916945 workaround during reset. This erratum applies
+# to revisions r0p0 and r1p0 of the Cortex-X2 cpu, it is fixed in r2p0.
+CPU_FLAG_LIST += ERRATA_X2_1916945
+
 # Flag to apply erratum 3699563 workaround during context save/restore of
 # ICH_VMCR_EL2 reg. This erratum applies to revision r0p0 of the Neoverse N3
 # cpu and is still open.
