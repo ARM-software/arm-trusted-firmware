47ddedb4d888991df3d797b6926c3b7922d2cca9
John Powell <john.powell@arm.com>
2025-07-11 21:26:00 -0500

    fix(cpus): workaround for Cortex-X2 erratum 1927200
    Cortex-X2 erratum 1927200 is a Cat B erratum that applies to
    revisions r0p0 and r1p0 and is fixed in r2p0.
    
    The workaround is to use instruction patching to insert a DMB ST
    before acquire atomic instructions without release semantics.
    
    SDEN documentation:
    https://developer.arm.com/documentation/SDEN-1775100/latest
    
    Change-Id: I8d9038df1907888b3c5b2520d06bc150665e74a1
    Signed-off-by: John Powell <john.powell@arm.com>
    (cherry picked from commit e236548455cc3f8e28cdd2daa3cffc6fae6ea73c)

--- lib/cpus/cpu-ops.mk
+++ lib/cpus/cpu-ops.mk
@@ -749,6 +749,10 @@ CPU_FLAG_LIST += ERRATA_X2_1916945
 # to revisions r0p0 and r1p0 of the Cortex-X2 cpu, it is fixed in r2p0.
 CPU_FLAG_LIST += ERRATA_X2_1917258
 
+# Flag to apply erratum 1927200 workaround during reset. This erratum applies
+# to revisions r0p0 and r1p0 of the Cortex-X2 cpu, it is fixed in r2p0.
+CPU_FLAG_LIST += ERRATA_X2_1927200
+
 # Flag to apply erratum 3699563 workaround during context save/restore of
 # ICH_VMCR_EL2 reg. This erratum applies to revision r0p0 of the Neoverse N3
 # cpu and is still open.
