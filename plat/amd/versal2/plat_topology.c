d2244f32f7773d7a7c8031fd4a95fbd3fbad7917
Prasad Kummari <prasad.kummari@amd.com>
2025-07-25 12:08:15 +0530

    fix(versal2): move plat_core_pos_by_mpidr to asm
    In the current implementation, plat_core_pos_by_mpidr() is defined in
    C.When BL31 is compiled with Armclang, a call to plat_core_pos_by_mpidr()
    from plat_my_core_pos() results in the return address stored in register
    x30 becoming invalid and register x9 (used later) ends up with the value
    0x0. Consequently, the CPU branches to address 0x0, triggering a
    synchronous exception. TF-A then invokes the BHB flush code before
    resuming execution. However, since the stack is not properly initialized
    at this stage, the system eventually enters plat_panic_handler().
    In the updated implementation, the platform_get_core_pos() function
    is redefined in assembly to provide tighter control during early boot
    stages. The MPIDR_EL1 register contains three affinity levels: Aff0
    (bits [0:7]), Aff1 (bits [8:15]), and Aff2 (bits [16:23]). In this
    assembly function, the core ID is extracted from Aff1
    (MPIDR_AFF1_SHIFT), and the cluster ID from Aff2 (MPIDR_AFF2_SHIFT).
    cluster/core ID calculation. The macro PLATFORM_MPIDR_AFFINITY_MASK
    introduced to mask MPIDR_EL1 register.
    
    Change-Id: Id532bbcd68f18e87ceba01c8f961d8c15962a1a3
    Signed-off-by: Prasad Kummari <prasad.kummari@amd.com>

--- plat/amd/versal2/plat_topology.c
+++ plat/amd/versal2/plat_topology.c
@@ -1,7 +1,7 @@
 /*
  * Copyright (c) 2018, Arm Limited and Contributors. All rights reserved.
  * Copyright (c) 2018-2022, Xilinx, Inc. All rights reserved.
- * Copyright (c) 2022, Advanced Micro Devices, Inc. All rights reserved.
+ * Copyright (c) 2022-2025, Advanced Micro Devices, Inc. All rights reserved.
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
@@ -32,37 +32,3 @@ const uint8_t *plat_get_power_domain_tree_desc(void)
 	return plat_power_domain_tree_desc;
 }
 
-/*******************************************************************************
- * This function implements a part of the critical interface between the psci
- * generic layer and the platform that allows the former to query the platform
- * to convert an MPIDR to a unique linear index. An error code (-1) is returned
- * in case the MPIDR is invalid.
- ******************************************************************************/
-int32_t plat_core_pos_by_mpidr(u_register_t mpidr)
-{
-	uint32_t cluster_id, cpu_id;
-	int32_t ret = 0;
-
-	mpidr &= MPIDR_AFFINITY_MASK;
-
-	cluster_id = MPIDR_AFFLVL2_VAL(mpidr);
-	cpu_id = MPIDR_AFFLVL1_VAL(mpidr);
-
-	if (cluster_id >= PLATFORM_CLUSTER_COUNT) {
-		ret = E_INVALID_CLUSTER_COUNT;
-		goto exit_label;
-	}
-
-	/*
-	 * Validate cpu_id by checking whether it represents a CPU in
-	 * one of the two clusters present on the platform.
-	 */
-	if (cpu_id >= PLATFORM_CORE_COUNT_PER_CLUSTER) {
-		ret = E_INVALID_CORE_COUNT;
-	} else {
-		ret = (cpu_id + (cluster_id * PLATFORM_CORE_COUNT_PER_CLUSTER));
-	}
-
-exit_label:
-	return ret;
-}
