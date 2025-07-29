3f446df45f1af055718d61459ee98019907d09af
Hope Wang <hope.wang@mediatek.corp-partner.google.com>
2025-07-29 11:31:26 +0800

    feat(mt8189): add support for PTP3
    Use common PTP3 driver to protect CPU from excessive voltage drop in CPU
    heavy loading.
    
    Signed-off-by: Hope Wang <hope.wang@mediatek.corp-partner.google.com>
    Change-Id: Iab4a77a6d1816a520f3fe112ef94efdc5789f6c8

---
+++ plat/mediatek/drivers/ptp3/mt8189/ptp3_plat.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2025, MediaTek Inc. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef PTP3_PLAT_H
+#define PTP3_PLAT_H
+
+#include <ptp3_plat_v2.h>
+
+#endif /* PTP3_PLAT_H */
