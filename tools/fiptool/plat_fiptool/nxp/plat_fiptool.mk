ba3668f1865b44635e8c7aa3a38d0d315850cec3
Khristine Andreea Barbulescu <khristineandreea.barbulescu@nxp.com>
2025-08-19 10:32:59 +0300

    fix(fiptool): skip Layerscape makefile for S32 build
    Skip Layerscape makefile for S32CC platform.
    This change ensures the `fiptool` Makefile
    recognizes and process the S32-specific Makefile
    correctly, instead of the default Layerscape one.
    
    Change-Id: I09b54a676d8db035c41ad77325b5b0e4b230e1ac
    Signed-off-by: Khristine Andreea Barbulescu <khristineandreea.barbulescu@nxp.com>

--- tools/fiptool/plat_fiptool/nxp/plat_fiptool.mk
+++ tools/fiptool/plat_fiptool/nxp/plat_fiptool.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (c) 2021, NXP. All rights reserved.
+# Copyright (c) 2021, 2025 NXP. All rights reserved.
 # Copyright (c) 2023-2025, Arm Limited. All rights reserved.
 #
 # SPDX-License-Identifier: BSD-3-Clause
@@ -8,6 +8,7 @@
 # Name of the platform defined source file name,
 # which contains platform defined UUID entries populated
 # in the plat_def_toc_entries[].
+ifeq (,$(findstring s32,${PLAT}))
 PLAT_DEF_UUID_CONFIG_FILE_NAME	:= plat_def_uuid_config
 
 PLAT_DEF_UUID_CONFIG_FILE_PATH := plat_fiptool/nxp/
@@ -27,4 +28,6 @@ endif
 ifeq (${PLAT_DEF_UUID},yes)
 FIPTOOL_DEFINES += PLAT_DEF_FIP_UUID
 FIPTOOL_SOURCES += ${PLAT_DEF_UUID_CONFIG_FILE_PATH}/${PLAT_DEF_UUID_CONFIG_FILE_NAME}.c
+endif
+
 endif
