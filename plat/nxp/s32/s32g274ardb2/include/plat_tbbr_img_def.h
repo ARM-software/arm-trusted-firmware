4d9ac8f0ae47fbc892b605d46d0f1f815e388a45
Khristine Andreea Barbulescu <khristineandreea.barbulescu@nxp.com>
2025-08-19 10:42:02 +0300

    feat(s32g274ardb): add custom DDR FW UUID entry
    Integrate support to add DDR FW to the final
    FIP image via `fiptool`.
    
    Add mechanism to allow platform specific image
    UUID. Add DDR FW entries to toc_entries and create
    `plat_fiptool.mk` in order to enable the integration
    of the DDR FW binary into the FIP image using
    `fiptool` command.
    
    Change-Id: I10c000025378206411ab70dbc5b2e745ffb01e5d
    Signed-off-by: Khristine Andreea Barbulescu <khristineandreea.barbulescu@nxp.com>

---
+++ plat/nxp/s32/s32g274ardb2/include/plat_tbbr_img_def.h
@@ -0,0 +1,18 @@
+/*
+ * Copyright 2025 NXP
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef PLAT_TBBR_IMG_DEF_H
+#define PLAT_TBBR_IMG_DEF_H
+
+#include <export/common/tbbr/tbbr_img_def_exp.h>
+
+#define DDR_FW_IMAGE_ID		MAX_IMG_IDS_WITH_SPMDS
+
+#define MAX_IMG_WITH_DDR_IDS		(MAX_IMG_IDS_WITH_SPMDS + 1U)
+
+#define MAX_NUMBER_IDS		MAX_IMG_WITH_DDR_IDS
+
+#endif	/* PLAT_TBBR_IMG_DEF_H */
