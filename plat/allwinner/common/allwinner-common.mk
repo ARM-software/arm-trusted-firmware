3d25bdfca3eceeba4ae2c1b24636d01114b1e854
Andre Przywara <andre.przywara@arm.com>
2025-09-01 01:06:49 +0100

    build(allwinner): disable unneeded CVE workarounds and MPAM
    There are a number of workarounds for CVEs related to sidechannel
    attacks on some CPU cores, most of them listed here:
    https://developer.arm.com/documentation/110280/latest/
    Also there are two other CVEs:
    https://developer.arm.com/documentation/110324/latest/
    https://developer.arm.com/documentation/110326/latest/
    
    As these page reveals, those workaround do not apply to the Cortex-A53
    (or A55) cores, so we can safely disable them in the Allwinner build
    recipes, since they only use those two cores so far.
    
    Also disable FEAT_MPAM, which is one of the only three later features
    that are enabled default, but are not enabled in Cortex-A53 or A55
    cores. Use the opportunity to group those options together and improve
    the comment.
    
    This decreases the code size by a few hundred bytes.
    
    Change-Id: Ibc52a4fc9b8f5d9b2b28a2ce13d3ab99b63e9640
    Signed-off-by: Andre Przywara <andre.przywara@arm.com>
    (cherry picked from commit d86ddcef6fee6d48285dd781f21dc44cdb3a8a45)

--- plat/allwinner/common/allwinner-common.mk
+++ plat/allwinner/common/allwinner-common.mk
@@ -91,6 +91,13 @@ ENABLE_SPE_FOR_LOWER_ELS	:=	0
 
 # Do not enable SVE (not supported on ARM v8.0).
 ENABLE_SVE_FOR_NS		:=	0
+ENABLE_FEAT_MPAM		:=	0
+
+WORKAROUND_CVE_2017_5715	:=	0
+WORKAROUND_CVE_2018_3639	:=	0
+WORKAROUND_CVE_2022_23960	:=	0
+WORKAROUND_CVE_2024_7881	:=	0
+WORKAROUND_CVE_2024_5660	:=	0
 
 # Enable workarounds for Cortex-A53 errata. Allwinner uses at least r0p4.
 ERRATA_A53_835769		:=	1
