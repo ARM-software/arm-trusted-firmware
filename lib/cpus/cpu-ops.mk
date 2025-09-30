af2b4d148a240d1dfa97bfc2ca0025ed52e7d566
John Powell <john.powell@arm.com>
2025-09-30 18:53:38 -0500

    fix(cpus): workaround for Cortex-A720 erratum 2729604
    Cortex-A720 erratum 2729604 is a Cat B erratum that applies to
    revisions r0p0 and r0p1, and is fixed in r0p2.
    
    This workaround might impact performance of workloads heavily
    relying on floating point division or square root operations.
    
    SDEN documentation:
    https://developer.arm.com/documentation/SDEN-2439421
    
    Change-Id: I4567d75ba9f17146d0d7bc5cdb622bb63efadc3c
    Signed-off-by: John Powell <john.powell@arm.com>
    (cherry picked from commit 217a79c4c3cfe714cb1ce4d083bd8f174d1be93c)

--- lib/cpus/cpu-ops.mk
+++ lib/cpus/cpu-ops.mk
@@ -1123,6 +1123,10 @@ CPU_FLAG_LIST += ERRATA_A715_2804830
 # of the Cortex-A715 cpu and is still open.
 CPU_FLAG_LIST += ERRATA_A715_3699560
 
+# Flag to apply erratum 2729604 workaround during reset. This erratum applies
+# to revisions r0p0 and r0p1. It is fixed in r0p2.
+CPU_FLAG_LIST += ERRATA_A720_2729604
+
 # Flag to apply erratum 2792132 workaround during reset. This erratum applies
 # to revisions r0p0 and r0p1. It is fixed in r0p2.
 CPU_FLAG_LIST += ERRATA_A720_2792132
