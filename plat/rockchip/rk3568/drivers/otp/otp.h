frank-w/u-boot

feat(rk3568): support SCMI for clock/reset domain
rockchip scmi clock controls clocks which only available in secure mode.

Change-Id: Ide3a8dac72512ce79331592c3cbb86577de7df70
Signed-off-by: Shengfei Xu <xsf@rock-chips.com>

plat/rockchip/rk3568/drivers/otp/otp.h
@@ -1,0 +1,77 @@
+/*
+ * Copyright (c) 2024, Rockchip Electronics Co., Ltd. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef RK_OTP_H
+#define RK_OTP_H
+
+#define WRITE_MASK			(16)
+
+#define OTP_MAX_SIZE			512
+#define OTP_MAX_BYTE_SIZE		(OTP_MAX_SIZE * 2)
+#define OTP_S_SIZE			448
+#define OTP_S_BYTE_SIZE			(OTP_S_SIZE * 2)
+#define OTP_NS_SIZE			64
+#define OTP_NS_BYTE_SIZE		(OTP_NS_SIZE * 2)
+#define OTP_PROG_MASK			0
+#define OTP_PROG_MASK_NUM		32
+#define OTP_READ_MASK			0x0100
+#define OTP_READ_MASK_NUM		32
+#define IS_READ_MASK			0
+#define IS_WRITE_MASK			1
+#define IS_DISBALE_MASK			0
+#define IS_ENABLE_MASK			1
+#define OTP_MASK_BYPASS			0x200
+
+/* CRU controller register */
+#define CLK_NS_OTP_USER_EN		(1 << 11)
+#define CLK_NS_OTP_SBPI_EN		(1 << 10)
+#define PCLK_NS_OTP_EN			(1 << 9)
+#define PCLK_PHY_OTP_EN			(1 << 13)
+#define OTP_PHY_SRSTN			(1u << 15)
+
+/* SCRU controller register */
+#define CLK_S_OTP_USER_EN		(1 << 7)
+#define CLK_S_OTP_SBPI_EN		(1 << 6)
+#define PCLK_S_OTP_EN			(1 << 5)
+
+/* SGRF controller register */
+#define SGRF_CON_OTP_CKE		(1 << 2)
+#define SGRF_CON_OTP_SECURE		(1 << 1)
+
+/* OTP controller register */
+#define REG_OTPC_SBPI_CTRL		(0x0020)
+#define SBPI_DEV_ID_SHIFT		(8)
+#define SBPI_DEV_ID_MASK		(0xFF)
+#define SBPI_CS_DEASSERT		(1 << 3)
+#define SBPI_CS_AUTO			(1 << 2)
+#define SBPI_SP				(1 << 1)
+#define SBPI_ENABLE			(1 << 0)
+#define REG_OTPC_SBPI_CMD_VALID_PRE	(0x0024)
+#define REG_OTPC_SBPI_CS_VALID_PRE	(0x0028)
+#define REG_OTPC_SBPI_STATUS		(0x002C)
+#define REG_OTPC_USER_CTRL		(0x0100)
+#define USER_PD				(1 << 1)
+#define USER_DCTRL			(1 << 0)
+#define REG_OTPC_USER_ADDR		(0x0104)
+#define REG_OTPC_USER_ENABLE		(0x0108)
+#define USER_ENABLE			(1 << 0)
+#define REG_OTPC_USER_STATUS		(0x0110)
+#define REG_OTPC_USER_QP		(0x0120)
+#define REG_OTPC_USER_Q			(0x0124)
+#define REG_OTPC_USER_QSR		(0x0128)
+#define REG_OTPC_USER_QRR		(0x012C)
+#define REG_OTPC_SBPI_CMD_BASE		(0x1000)
+#define REG_OTPC_SBPI_READ_DATA_BASE	(0x2000)
+#define REG_OTPC_INT_CON		(0x0300)
+#define REG_OTPC_INT_STATUS		(0x0304)
+
+#define REG_KEY_READER_CONFIG		0x00
+#define OTP_KEY_ACCESS_START		(1 << 0)
+
+int rk_otp_read(uint32_t addr, uint32_t length,
+		    uint16_t *buf, bool is_need_ecc);
+int rk_otp_ns_ecc_flag(uint32_t addr);
+#endif /* RK_OTP_H */

plat/rockchip/rk3568/drivers/scmi/rk3568_clk.h
@@ -1,0 +1,70 @@
+/*
+ * Copyright (c) 2024, Rockchip Electronics Co., Ltd. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef __CLOCK_H__
+#define __CLOCK_H__
+
+#define CRU_CLKGATE_CON0		0x300
+#define CRU_CLKGATES_CON(i)		(CRU_CLKGATE_CON0 + (i) * 4)
+
+#define SCRU_GATE_CON00			0x0180
+#define SCRU_GATE_CON01			0x0184
+#define SCRU_GATE_CON02			0x0188
+
+#define SGRF_SOC_CON2			0x0008
+
+#define MPLL_HZ				(800 * MHz)
+#define MPLL_REFDIV			3
+#define MPLL_FBDIV			200
+#define MPLL_POSTDIV1			2
+#define MPLL_POSTDIV2			1
+#define MPLL_DSMPD			1
+#define MPLL_FRAC			0
+
+#define PPLL_HZ				(200 * MHz)
+#define GPLL_HZ				(1188 * MHz)
+#define ACLK_SECURE_FLASH_S_HZ		(297 * MHz)
+#define HCLK_SECURE_FLASH_S_HZ		(148500000)
+#define PCLK_SECURE_FLASH_S_HZ		(99 * MHz)
+#define PCLK_TOP_S_HZ			(99 * MHz)
+#define HCLK_VO_S_HZ			(99 * MHz)
+#define PCLK_DDR_HZ			(99 * MHz)
+#define PCLK_PDPMU_S_HZ			(100 * MHz)
+#define HCLK_PDPMU_S_HZ			(100 * MHz)
+
+#define RK3568_PLL_MODE_CON		0x20
+#define RK3568_PLL_MODE_SHIFT		0
+#define RK3568_PLL_MODE_MASK		(0x3 << RK3568_PLL_MODE_SHIFT)
+#define RK3568_PLL_MODE_SLOWMODE	0
+#define RK3568_PLL_MODE_NORMAL		1
+#define RK3568_PLLCON(i)		(i * 0x4)
+#define RK3568_PLLCON0_FBDIV_MASK	0xfff
+#define RK3568_PLLCON0_FBDIV_SHIFT	0
+#define RK3568_PLLCON0_POSTDIV1_MASK	0x7
+#define RK3568_PLLCON0_POSTDIV1_SHIFT	12
+#define RK3568_PLLCON1_REFDIV_MASK	0x3f
+#define RK3568_PLLCON1_REFDIV_SHIFT	0
+#define RK3568_PLLCON1_POSTDIV2_MASK	0x7
+#define RK3568_PLLCON1_POSTDIV2_SHIFT	6
+#define RK3568_PLLCON1_LOCK_STATUS	(1 << 10)
+#define RK3568_PLLCON1_DSMPD_MASK	0x1
+#define RK3568_PLLCON1_DSMPD_SHIFT	12
+#define RK3568_PLLCON1_PWRDOWN		(1 << 13)
+
+#define RK3568_CLK_SEL(x)		((x) * 0x4 + 0x100)
+#define RK3568_PMUCLK_SEL(x)		((x) * 0x4 + 0x100)
+
+#define PMUCRU_MODE_CON00		0x0080
+#define PMUCRU_PMUCLKSEL_CON00		0x0100
+#define PMUCRU_PMUCLKSEL_CON03		0x010c
+#define PMUCRU_PMUGATE_CON01		0x0184
+#define PMUCRU_CLKGATES_CON(i)		(0x180 + (i) * 4)
+#define PMUCRU_CLKGATES_CON_CNT		3
+
+void pvtplls_suspend(void);
+void pvtplls_resume(void);
+void rockchip_clock_init(void);
+#endif

ARM-software/arm-trusted-firmware

plat/rockchip/rk3568/drivers/otp/otp.h
@@ -1,0 +1,78 @@
+/*
+ * Copyright (c) 2024-2025, Rockchip Electronics Co., Ltd. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef OTP_H
+#define OTP_H
+
+#define WRITE_MASK			(16)
+
+#define OTP_MAX_SIZE			512
+#define OTP_MAX_BYTE_SIZE		(OTP_MAX_SIZE * 2)
+#define OTP_S_SIZE			448
+#define OTP_S_BYTE_SIZE			(OTP_S_SIZE * 2)
+#define OTP_NS_SIZE			64
+#define OTP_NS_BYTE_SIZE		(OTP_NS_SIZE * 2)
+#define OTP_PROG_MASK			0
+#define OTP_PROG_MASK_NUM		32
+#define OTP_READ_MASK			0x0100
+#define OTP_READ_MASK_NUM		32
+#define IS_READ_MASK			0
+#define IS_WRITE_MASK			1
+#define IS_DISBALE_MASK			0
+#define IS_ENABLE_MASK			1
+#define OTP_MASK_BYPASS			0x200
+
+/* CRU controller register */
+#define CLK_NS_OTP_USER_EN		(1 << 11)
+#define CLK_NS_OTP_SBPI_EN		(1 << 10)
+#define PCLK_NS_OTP_EN			(1 << 9)
+#define PCLK_PHY_OTP_EN			(1 << 13)
+#define OTP_PHY_SRSTN			(1u << 15)
+
+/* SCRU controller register */
+#define CLK_S_OTP_USER_EN		(1 << 7)
+#define CLK_S_OTP_SBPI_EN		(1 << 6)
+#define PCLK_S_OTP_EN			(1 << 5)
+
+/* SGRF controller register */
+#define SGRF_CON_OTP_CKE		(1 << 2)
+#define SGRF_CON_OTP_SECURE		(1 << 1)
+
+/* OTP controller register */
+#define REG_OTPC_SBPI_CTRL		(0x0020)
+#define SBPI_DEV_ID_SHIFT		(8)
+#define SBPI_DEV_ID_MASK		(0xFF)
+#define SBPI_CS_DEASSERT		(1 << 3)
+#define SBPI_CS_AUTO			(1 << 2)
+#define SBPI_SP				(1 << 1)
+#define SBPI_ENABLE			(1 << 0)
+#define REG_OTPC_SBPI_CMD_VALID_PRE	(0x0024)
+#define REG_OTPC_SBPI_CS_VALID_PRE	(0x0028)
+#define REG_OTPC_SBPI_STATUS		(0x002C)
+#define REG_OTPC_USER_CTRL		(0x0100)
+#define USER_PD				(1 << 1)
+#define USER_DCTRL			(1 << 0)
+#define REG_OTPC_USER_ADDR		(0x0104)
+#define REG_OTPC_USER_ENABLE		(0x0108)
+#define USER_ENABLE			(1 << 0)
+#define REG_OTPC_USER_STATUS		(0x0110)
+#define REG_OTPC_USER_QP		(0x0120)
+#define REG_OTPC_USER_Q			(0x0124)
+#define REG_OTPC_USER_QSR		(0x0128)
+#define REG_OTPC_USER_QRR		(0x012C)
+#define REG_OTPC_SBPI_CMD_OFFSET(n)	(0x1000 + (n << 2))
+#define REG_OTPC_SBPI_READ_DATA_BASE	(0x2000)
+#define REG_OTPC_INT_CON		(0x0300)
+#define REG_OTPC_INT_STATUS		(0x0304)
+
+#define REG_KEY_READER_CONFIG		0x00
+#define OTP_KEY_ACCESS_START		(1 << 0)
+#define SBPI_VAILI_COMMAND(n)		(0xffff0000 | n)
+
+int rk_otp_read(uint32_t addr, uint32_t length,
+		    uint16_t *buf, bool is_need_ecc);
+int rk_otp_ns_ecc_flag(uint32_t addr);
+#endif /* OTP_H */

plat/rockchip/rk3568/drivers/scmi/rk3568_clk.h
@@ -1,0 +1,71 @@
+/*
+ * Copyright (c) 2024-2025, Rockchip Electronics Co., Ltd. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef RK3568_CLOCK_H
+#define RK3568_CLOCK_H
+
+#define CRU_CLKGATE_CON0		0x300
+#define CRU_CLKGATES_CON(i)		(CRU_CLKGATE_CON0 + (i) * 4)
+
+#define SCRU_GATE_CON00			0x0180
+#define SCRU_GATE_CON01			0x0184
+#define SCRU_GATE_CON02			0x0188
+
+#define SGRF_SOC_CON2			0x0008
+
+#define MPLL_HZ				(800 * MHz)
+#define MPLL_REFDIV			3
+#define MPLL_FBDIV			200
+#define MPLL_POSTDIV1			2
+#define MPLL_POSTDIV2			1
+#define MPLL_DSMPD			1
+#define MPLL_FRAC			0
+
+#define PPLL_HZ				(200 * MHz)
+#define GPLL_HZ				(1188 * MHz)
+#define ACLK_SECURE_FLASH_S_HZ		(297 * MHz)
+#define HCLK_SECURE_FLASH_S_HZ		(148500000)
+#define PCLK_SECURE_FLASH_S_HZ		(99 * MHz)
+#define PCLK_TOP_S_HZ			(99 * MHz)
+#define HCLK_VO_S_HZ			(99 * MHz)
+#define PCLK_DDR_HZ			(99 * MHz)
+#define PCLK_PDPMU_S_HZ			(100 * MHz)
+#define HCLK_PDPMU_S_HZ			(100 * MHz)
+
+#define RK3568_PLL_MODE_CON		0x20
+#define RK3568_PLL_MODE_SHIFT		0
+#define RK3568_PLL_MODE_MASK		(0x3 << RK3568_PLL_MODE_SHIFT)
+#define RK3568_PLL_MODE_SLOWMODE	0
+#define RK3568_PLL_MODE_NORMAL		1
+#define RK3568_PLLCON(i)		(i * 0x4)
+#define RK3568_PLLCON0_FBDIV_MASK	0xfff
+#define RK3568_PLLCON0_FBDIV_SHIFT	0
+#define RK3568_PLLCON0_POSTDIV1_MASK	0x7
+#define RK3568_PLLCON0_POSTDIV1_SHIFT	12
+#define RK3568_PLLCON1_REFDIV_MASK	0x3f
+#define RK3568_PLLCON1_REFDIV_SHIFT	0
+#define RK3568_PLLCON1_POSTDIV2_MASK	0x7
+#define RK3568_PLLCON1_POSTDIV2_SHIFT	6
+#define RK3568_PLLCON1_LOCK_STATUS	(1 << 10)
+#define RK3568_PLLCON1_DSMPD_MASK	0x1
+#define RK3568_PLLCON1_DSMPD_SHIFT	12
+#define RK3568_PLLCON1_PWRDOWN		(1 << 13)
+
+#define RK3568_CLK_SEL(x)		((x) * 0x4 + 0x100)
+#define RK3568_PMUCLK_SEL(x)		((x) * 0x4 + 0x100)
+
+#define PMUCRU_MODE_CON00		0x0080
+#define PMUCRU_PMUCLKSEL_CON00		0x0100
+#define PMUCRU_PMUCLKSEL_CON03		0x010c
+#define PMUCRU_PMUGATE_CON01		0x0184
+#define PMUCRU_CLKGATES_CON(i)		(0x180 + (i) * 4)
+#define PMUCRU_CLKGATES_CON_CNT		3
+
+void pvtplls_suspend(void);
+void pvtplls_resume(void);
+void rockchip_clock_init(void);
+
+#endif /* RK3568_CLOCK_H */