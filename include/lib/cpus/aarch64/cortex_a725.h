ba7716bb80d3b59e9d2fba6125c809dbaa4fbf10
Xialin Liu <xialin.liu@arm.com>
2025-11-10 14:48:16 -0600

    fix(cpus): workaround for Cortex-A725 erratum 3711914
    Cortex-A725 erratum 3711914 is a Cat B erratum that applies
    to revisions r0p0 and r0p1 and it is fixed in r0p2.
    
    This erratum can be avoided by inserting a DMB LD after each DSB ST instruction.
    
    SDEN documentation:
    https://developer.arm.com/documentation/SDEN-2832921/latest/
    
    Change-Id: If3b9d3a0f495b3a172d3e6e5ca7afa8c30aeb4ea
    Signed-off-by: Xialin Liu <xialin.liu@arm.com>

--- include/lib/cpus/aarch64/cortex_a725.h
+++ include/lib/cpus/aarch64/cortex_a725.h
@@ -27,6 +27,14 @@
 #define CORTEX_A725_CPUACTLR_EL1				S3_0_C15_C1_0
 #define CORTEX_A725_CPUACTLR2_EL1				S3_0_C15_C1_1
 
+/*******************************************************************************
+ * CPU Instruction Patch Control register specific definitions
+ ******************************************************************************/
+#define CORTEX_A725_CPUPSELR_EL3				S3_6_C15_C8_0
+#define CORTEX_A725_CPUPCR_EL3					S3_6_C15_C8_1
+#define CORTEX_A725_CPUPOR_EL3					S3_6_C15_C8_2
+#define CORTEX_A725_CPUPMR_EL3					S3_6_C15_C8_3
+
 #ifndef __ASSEMBLER__
 long check_erratum_cortex_a725_3699564(long cpu_rev);
 #endif /* __ASSEMBLER__ */
