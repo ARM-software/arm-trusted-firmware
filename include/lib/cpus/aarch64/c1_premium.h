83ad6baebe522430911e0cdf8961fab105e97d45
Arvind Ram Prakash <arvind.ramprakash@arm.com>
2025-11-14 14:47:35 -0600

    fix(security): add CVE-2024-7881 mitigation to C1-Premium CPU
    This patch mitigates Cat B erratum 3651221 [2] / CVE-2024-7881 [1]
    for C1-Premium CPU. This CVE applies to r0p0 and
    is fixed in r1p0 [2].
    
    This CVE can be mitigated by disabling the affected prefetcher
    setting CPUACTLR6_EL1[41].
    
    [1] https://developer.arm.com/documentation/110326/latest/
    [2] https://developer.arm.com/documentation/111078/latest/
    
    Signed-off-by: Arvind Ram Prakash <arvind.ramprakash@arm.com>
    Change-Id: I70b50700bc1618e0f8f4121efc9fe89e2742ed74

--- include/lib/cpus/aarch64/c1_premium.h
+++ include/lib/cpus/aarch64/c1_premium.h
@@ -20,4 +20,9 @@
 #define C1_PREMIUM_IMP_CPUPWRCTLR_EL1				S3_0_C15_C2_7
 #define C1_PREMIUM_IMP_CPUPWRCTLR_EL1_CORE_PWRDN_EN_BIT	U(1)
 
+/*******************************************************************************
+ * CPU Auxiliary control register 6 specific definitions
+ ******************************************************************************/
+#define C1_PREMIUM_CPUACTLR6_EL1				S3_0_C15_C8_1
+
 #endif /* C1_PREMIUM_H */
