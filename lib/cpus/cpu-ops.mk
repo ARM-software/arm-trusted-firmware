f8f6f39dc4ba6401c35e6931d6af039dc866ba04
Xialin Liu <xialin.liu@arm.com>
2025-12-08 10:50:08 -0600

    fix(cpus): workaround for C1-Ultra erratum 4102704
    C1-Ultra erratum 4102704 is a Cat B erratum that applies
    to revisions r0p0, r1p0 and is still open.
    
    The erratum can be avoided by setting CPUACTLR4_EL1[23] to 1.
    Overall expected performance degradation is ~1.36%, but
    isolated benchmark components might see higher or lower impact.
    
    SDEN documentation:
    https://developer.arm.com/documentation/111077/8-0
    
    Change-Id: I414df1af006484dd120f928bd8fdf9e6f4a513fd
    Signed-off-by: Xialin Liu <xialin.liu@arm.com>

--- lib/cpus/cpu-ops.mk
+++ lib/cpus/cpu-ops.mk
@@ -1251,6 +1251,10 @@ CPU_FLAG_LIST += ERRATA_C1ULTRA_3815514
 # applies to revisions r0p0 and r1p0 of C1-Ultra cpu and is still open.
 CPU_FLAG_LIST += ERRATA_C1ULTRA_3865171
 
+# Flag to apply erratum 4102704 workaround during reset. This erratum
+# applies to revisions r0p0 and r1p0 of C1-Ultra cpu and is still open.
+CPU_FLAG_LIST += ERRATA_C1ULTRA_4102704
+
 # Flag to apply DSU erratum 798953. This erratum applies to DSUs revision r0p0.
 # Applying the workaround results in higher DSU power consumption on idle.
 CPU_FLAG_LIST += ERRATA_DSU_798953
