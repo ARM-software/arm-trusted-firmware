0627cca37efeb725dece71dbc00701799e484b3c
Harsha Vardhan V M <h-vm@ti.com>
2026-02-24 17:29:20 +0530

    PENDING: feat(ti): mmu map fuse writebuff buffer address
    Fuse writebuff buffer is written from u-boot at address
    0x82000000. So, map this buffer address with a size of 0x1000
    to the bl31 plat mmu mapping. Increase the MAX_MMAP_REGIONS
    to 13 with the new entry.
    
    Change-Id: I3dd01aceb33a3144ac4cc1bb5f133c26343f190f
    Signed-off-by: Harsha Vardhan V M <h-vm@ti.com>

--- plat/ti/k3low/board/am62lx/include/board_def.h
+++ plat/ti/k3low/board/am62lx/include/board_def.h
@@ -16,4 +16,12 @@
 #define SCMI_SHMEM_ADDR		(0x70800000UL)
 #define SCMI_SHMEM_SIZE		(0x1000UL)	/* 4096 bytes */
 
+/*
+ * Fuse writebuff
+ * K3_FUSE_WRITEBUFF_BASE - buffer content base addr in memory
+ * K3_FUSE_WRITEBUFF_SIZE - Maximum allocated size for the buffer
+ */
+#define K3_FUSE_WRITEBUFF_BASE	(0x82000000UL)
+#define K3_FUSE_WRITEBUFF_SIZE	(0x1000UL)
+
 #endif /* BOARD_DEF_H */
