bccaa89b977a251e0b63da816bbb239bda23d67e
Xialin Liu <xialin.liu@arm.com>
2026-02-27 10:14:08 -0600

    fix(cpus): workaround for Cortex-A710 erratum 1785648
    Cortex-A710 erratum 1785648 is a Cat B erratum that applies to
    revision r0p0. It is fixed in r1p0.
    
    Set CPUACTLR2_EL1[2] to force atomic store operations to write-back
    memory to be performed in the L1 data cache.
    
    SDEN documentation:
    https://developer.arm.com/documentation/SDEN-1775101/latest
    
    Change-Id: I2efb410d6712a1265518daaa16e0caa2daa370da
    Signed-off-by: Xialin Liu <xialin.liu@arm.com>
    (cherry picked from commit 4de39787382683ae2f0e9cb35aa47737c60a917b)

--- lib/cpus/cpu-ops.mk
+++ lib/cpus/cpu-ops.mk
@@ -678,6 +678,10 @@ CPU_FLAG_LIST += ERRATA_V3_3864536
 # to revisions r0p0, r0p1 and r0p2 of the Neoverse V3 cpu and is still open.
 CPU_FLAG_LIST += ERRATA_V3_3878291
 
+# Flag to apply erratum 1785648 workaround during reset. This erratum applies to
+# revision r0p0 of the Cortex-A710 cpu and is fixed in r1p0.
+CPU_FLAG_LIST += ERRATA_A710_1785648
+
 # Flag to apply erratum 1901946 workaround during reset. This erratum applies
 # to revision r1p0 and is fixed in r2p0.
 CPU_FLAG_LIST += ERRATA_A710_1901946
