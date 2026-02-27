2bbb415136cada4dad0e7d8c9287e299f221504d
Xialin Liu <xialin.liu@arm.com>
2026-02-27 11:34:49 -0600

    fix(cpus): workaround for Cortex-X1 erratum 1492189
    Cortex-X1 erratum 1492189 is a Cat B erratum that applies to revision
    r0p0. It is fixed in r1p0.
    
    The workaround is to set CPUACTLR5_EL1[8] to 1'b1. The workaround
    might result in a small increase in core power consumption.
    
    SDEN documentation:
    https://developer.arm.com/documentation/SDEN-1401782/latest
    
    Change-Id: I288f88f092ee05c15cefb2e764663f4d17fc10a5
    Signed-off-by: Xialin Liu <xialin.liu@arm.com>
    (cherry picked from commit e168af34f99b5ebbebd3ec28192714b835ba0cdb)

--- lib/cpus/cpu-ops.mk
+++ lib/cpus/cpu-ops.mk
@@ -609,6 +609,10 @@ CPU_FLAG_LIST += ERRATA_A78C_4302974
 # revision r0p0 of the Cortex-X1 cpu and is fixed in r1p0.
 CPU_FLAG_LIST += ERRATA_X1_1467580
 
+# Flag to apply erratum 1492189 workaround during reset. This erratum applies to
+# revision r0p0 of the Cortex-X1 cpu and is fixed in r1p0.
+CPU_FLAG_LIST += ERRATA_X1_1492189
+
 # Flag to apply erratum 1821534 workaround during reset. This erratum applies
 # to revisions r0p0 - r1p0 of the X1 cpu and fixed in r1p1.
 CPU_FLAG_LIST += ERRATA_X1_1821534
