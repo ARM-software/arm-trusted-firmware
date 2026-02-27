ded6b6ccf9363a49a1b18d93d1df02b12adf4b69
Xialin Liu <xialin.liu@arm.com>
2026-02-27 09:51:32 -0600

    fix(cpus): workaround for Cortex-X4 erratum 2631888
    Cortex-X4 erratum 2631888 is a Cat B erratum that applies to revision
    r0p0. It is fixed in r0p1.
    
    Disable the affected L1 data cache prefetcher by setting
    CPUACTLR6_EL1[41] to 1. Doing so will incur a performance penalty of
    approximately 1%. Contact Arm for an alternate workaround that impacts
    power.
    
    SDEN documentation:
    https://developer.arm.com/documentation/109148/latest
    
    Change-Id: If04390071ff514167218d68663cfabd91a93284f
    Signed-off-by: Xialin Liu <xialin.liu@arm.com>
    (cherry picked from commit 06c1aaf0456753eb9a1eec82479a9ddfdfd30ab2)

--- lib/cpus/cpu-ops.mk
+++ lib/cpus/cpu-ops.mk
@@ -1059,6 +1059,10 @@ CPU_FLAG_LIST += ERRATA_X4_2302507
 # revision r0p0 of the Cortex-X4 cpu and is fixed in r0p1.
 CPU_FLAG_LIST += ERRATA_X4_2620954
 
+# Flag to apply erratum 2631888 workaround during reset. This erratum applies to
+# revision r0p0 of the Cortex-X4 cpu and is fixed in r0p1.
+CPU_FLAG_LIST += ERRATA_X4_2631888
+
 # Flag to apply erratum 2701112 workaround for platforms that do not use an
 # Arm interconnect IP. This erratum applies to revisions r0p0 of the Cortex-X4
 # cpu and is fixed in r0p1.
