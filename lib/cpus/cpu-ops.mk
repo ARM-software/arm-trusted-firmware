74532dfc1487e8988f8be45dd1660dd2780e141e
Xialin Liu <xialin.liu@arm.com>
2026-02-27 10:34:39 -0600

    fix(cpus): workaround for Cortex-A78AE erratum 1827431
    Cortex-A78AE erratum 1827431 is a Cat B erratum that applies to
    revision r0p0. It is fixed in r0p1.
    
    This erratum can be avoided by setting CPUECTLR_EL1[53] to 1, which
    disables the allocation of splintered pages in the L2 TLB.
    
    SDEN documentation:
    https://developer.arm.com/documentation/SDEN-1707912/latest
    
    Change-Id: I05158d1dfcc4b70f4a96656c26f040aba9e74d70
    Signed-off-by: Xialin Liu <xialin.liu@arm.com>
    (cherry picked from commit 9f2fdca5ea6bda7cb0b3b355b539c97b952e697f)

--- lib/cpus/cpu-ops.mk
+++ lib/cpus/cpu-ops.mk
@@ -450,6 +450,10 @@ CPU_FLAG_LIST += ERRATA_A78_3888017
 # revisions r0p0, r1p0, r1p1, r1p2 of the Cortex-A78 cpu and is still open.
 CPU_FLAG_LIST += ERRATA_A78_4302972
 
+# Flag to apply erratum 1827431 workaround during reset. This erratum applies to
+# revision r0p0 of the Cortex-A78AE cpu and is fixed in r0p1.
+CPU_FLAG_LIST += ERRATA_A78_AE_1827431
+
 # Flag to apply erratum 1941500 workaround during reset. This erratum applies
 # to revisions r0p0 and r0p1 of the A78 AE cpu. It is still open.
 CPU_FLAG_LIST += ERRATA_A78_AE_1941500
