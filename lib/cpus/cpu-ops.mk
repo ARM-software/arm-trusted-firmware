159e301757120b30b664f427ea804b92414f76ac
Xialin Liu <xialin.liu@arm.com>
2026-02-27 10:48:53 -0600

    fix(cpus): workaround for Cortex-A77 erratum 1204882
    Cortex-A77 erratum 1204882 is a Cat B erratum that applies to revision
    r0p0. It is fixed in r1p0.
    
    This erratum can be avoided if software sets CPUACTLR2_EL1[11] to 1.
    
    SDEN documentation:
    https://developer.arm.com/documentation/SDEN-1152370/latest
    
    Change-Id: Id1ffbff103644966e657eff8a7cd5ab83532816d
    Signed-off-by: Xialin Liu <xialin.liu@arm.com>
    (cherry picked from commit 98311b0ec1519f3196042351926accc985de54ac)

--- lib/cpus/cpu-ops.mk
+++ lib/cpus/cpu-ops.mk
@@ -341,6 +341,10 @@ CPU_FLAG_LIST += ERRATA_A76AE_3888014
 # revision r0p0 of the Cortex-A77 cpu and is fixed in r1p0.
 CPU_FLAG_LIST += ERRATA_A77_1160841
 
+# Flag to apply erratum 1204882 workaround during reset. This erratum applies to
+# revision r0p0 of the Cortex-A77 cpu and is fixed in r1p0.
+CPU_FLAG_LIST += ERRATA_A77_1204882
+
 # Flag to apply erratum 1508412 workaround during reset. This erratum applies
 # only to revision <= r1p0 of the Cortex A77 cpu.
 CPU_FLAG_LIST += ERRATA_A77_1508412
