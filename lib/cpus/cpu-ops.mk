2adc274c6648e8a82df044feec0e20bb5e5007c8
Xialin Liu <xialin.liu@arm.com>
2026-02-27 10:36:17 -0600

    fix(cpus): workaround for Cortex-A510 erratum 1942494
    Cortex-A510 erratum 1942494 is a Cat B erratum that applies to
    revision r0p0. It is fixed in r0p1.
    
    This erratum can be avoided by setting IMP_CPUACTLR_EL1[15] = 1. This
    increases the best case L2 hit latency by a cycle, incurring a small
    but not negligible performance cost. This erratum affects configurations
    where the complex is configured with an L2 cache (configuration
    parameter L2_CACHE is TRUE).
    
    SDEN documentation:
    https://developer.arm.com/documentation/SDEN1873351/latest
    
    Change-Id: I1956f92457f91815bdb8220c4668cf2f721d1a57
    Signed-off-by: Xialin Liu <xialin.liu@arm.com>
    (cherry picked from commit f663b0e10d68e81154ca400bbca998f58c5a1aaa)

--- lib/cpus/cpu-ops.mk
+++ lib/cpus/cpu-ops.mk
@@ -1377,6 +1377,10 @@ CPU_FLAG_LIST += ERRATA_A510_1910738
 # revision r0p0 of the Cortex-A510 cpu and is fixed in r0p1.
 CPU_FLAG_LIST += ERRATA_A510_1937669
 
+# Flag to apply erratum 1942494 workaround during reset. This erratum applies to
+# revision r0p0 of the Cortex-A510 cpu and is fixed in r0p1.
+CPU_FLAG_LIST += ERRATA_A510_1942494
+
 # Flag to apply erratum 2008766 workaround during reset. This erratum applies
 # to revisions r0p0, r0p1, r0p2, r0p3, r1p0, r1p1, r1p2 and r1p3 of the
 # Cortex-A510 cpu and is still open.
