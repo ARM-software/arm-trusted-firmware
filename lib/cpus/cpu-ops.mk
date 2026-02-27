4a4b7a95ae683d5031e4fc91b5b3e2a7d5a3d246
Xialin Liu <xialin.liu@arm.com>
2026-02-27 11:34:49 -0600

    fix(cpus): workaround for Cortex-X1 erratum 2395406
    Cortex-X1 erratum 2395406 is a Cat B erratum that applies to revisions
    r0p0, r1p0, r1p1, r1p2. It is still open.
    
    Disable folding of demand requests into older prefetches with L2 miss
    requests outstanding by setting CPUACTLR2_EL1[40] to 1.
    
    SDEN documentation:
    https://developer.arm.com/documentation/SDEN-1401782/latest
    
    Change-Id: I8571c610a8701fbafbcbe4599d2f3272ad38efb5
    Signed-off-by: Xialin Liu <xialin.liu@arm.com>
    (cherry picked from commit 02bfb158838e1f0565ec27593e3833d1376955ba)

--- lib/cpus/cpu-ops.mk
+++ lib/cpus/cpu-ops.mk
@@ -649,6 +649,10 @@ CPU_FLAG_LIST += ERRATA_X1_1952683
 # revisions r0p0, r1p0, r1p1, r1p2 of the Cortex-X1 cpu and is still open.
 CPU_FLAG_LIST += ERRATA_X1_2376745
 
+# Flag to apply erratum 2395406 workaround during reset. This erratum applies to
+# revisions r0p0, r1p0, r1p1, r1p2 of the Cortex-X1 cpu and is still open.
+CPU_FLAG_LIST += ERRATA_X1_2395406
+
 # Flag to apply erratum 925373 workaround during reset. This erratum applies to
 # revision r0p0 of the Neoverse N1 cpu and is fixed in r1p0.
 CPU_FLAG_LIST += ERRATA_N1_925373
