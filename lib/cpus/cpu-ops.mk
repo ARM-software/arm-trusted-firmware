744e6a4c15daa9617e11d020a30c087afd1585e5
Xialin Liu <xialin.liu@arm.com>
2026-02-27 11:34:49 -0600

    fix(cpus): workaround for Cortex-X1 erratum 1952683
    Cortex-X1 erratum 1952683 is a Cat B erratum that applies to revision
    r0p0. It is fixed in r1p0.
    
    To avoid this erratum, serialize before all direct reads and writes to
    the FPSR (AArch64) and FPSCR (AArch32), with the exception of "VMRS
    APSR_nzcv, FPSCR" in AArch32 execution state. This can be done through
    an EL3 patch sequence.
    
    SDEN documentation:
    https://developer.arm.com/documentation/SDEN-1401782/latest
    
    Change-Id: I84b5fd6d703c49914f15d219a86b58bfcebefa77
    Signed-off-by: Xialin Liu <xialin.liu@arm.com>
    (cherry picked from commit 025f274152b0dec7bb3fa28f243cc988bbc01a33)

--- lib/cpus/cpu-ops.mk
+++ lib/cpus/cpu-ops.mk
@@ -641,6 +641,10 @@ CPU_FLAG_LIST += ERRATA_X1_1941498
 # revisions r0p0, r1p0, r1p1 of the Cortex-X1 cpu and is fixed in r1p2.
 CPU_FLAG_LIST += ERRATA_X1_1951500
 
+# Flag to apply erratum 1952683 workaround during reset. This erratum applies to
+# revision r0p0 of the Cortex-X1 cpu and is fixed in r1p0.
+CPU_FLAG_LIST += ERRATA_X1_1952683
+
 # Flag to apply erratum 925373 workaround during reset. This erratum applies to
 # revision r0p0 of the Neoverse N1 cpu and is fixed in r1p0.
 CPU_FLAG_LIST += ERRATA_N1_925373
