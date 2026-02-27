a459a760c3ba9373f42ca144cd84ec0915205a30
Xialin Liu <xialin.liu@arm.com>
2026-02-27 10:14:08 -0600

    fix(cpus): workaround for Cortex-A710 erratum 1887102
    Cortex-A710 erratum 1887102 is a Cat B erratum that applies to
    revisions r0p0, r1p0. It is fixed in r2p0.
    
    To avoid this erratum, software should either set CPUACTLR2_EL1[27] to
    1 to allow the injected DSB to invalidate translation table entries
    caching translations for TRBE, or execute an Inner Shareable TLBI by
    VA or TLBI by VMID before the DSB.
    
    SDEN documentation:
    https://developer.arm.com/documentation/SDEN-1775101/latest
    
    Change-Id: I9a98074af3147639b3b4b910cf2ee1b3638b65d5
    Signed-off-by: Xialin Liu <xialin.liu@arm.com>
    (cherry picked from commit 58d1e97c7cc0e0c6d467acf38ad12ccd4aaa9689)

--- lib/cpus/cpu-ops.mk
+++ lib/cpus/cpu-ops.mk
@@ -689,6 +689,10 @@ CPU_FLAG_LIST += ERRATA_A710_1793423
 # revision r0p0 of the Cortex-A710 cpu and is fixed in r1p0.
 CPU_FLAG_LIST += ERRATA_A710_1847092
 
+# Flag to apply erratum 1887102 workaround during reset. This erratum applies to
+# revisions r0p0, r1p0 of the Cortex-A710 cpu and is fixed in r2p0.
+CPU_FLAG_LIST += ERRATA_A710_1887102
+
 # Flag to apply erratum 1901946 workaround during reset. This erratum applies
 # to revision r1p0 and is fixed in r2p0.
 CPU_FLAG_LIST += ERRATA_A710_1901946
