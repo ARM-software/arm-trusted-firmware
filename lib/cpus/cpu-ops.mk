d580e9f10f20427e3ad2580b3af6287eb1e04b58
Xialin Liu <xialin.liu@arm.com>
2026-02-27 10:36:17 -0600

    fix(cpus): workaround for Cortex-A510 erratum 1966377
    Cortex-A510 erratum 1966377 is a Cat B erratum that applies to
    revision r0p0. It is fixed in r0p1.
    
    This erratum can be avoided by setting IMP_CPUACTLR2_EL1[29] to 1 to
    disable a power optimization feature. This will have no impact on
    performance, but will slightly increase power consumption.
    
    SDEN documentation:
    https://developer.arm.com/documentation/SDEN1873351/latest
    
    Change-Id: I9b0fb03f7e8b9626a19d3152e4f5047c3bbec1ad
    Signed-off-by: Xialin Liu <xialin.liu@arm.com>
    (cherry picked from commit 6e4321abe5ddc01bb529182df1afe3e348887364)

--- lib/cpus/cpu-ops.mk
+++ lib/cpus/cpu-ops.mk
@@ -1385,6 +1385,10 @@ CPU_FLAG_LIST += ERRATA_A510_1942494
 # revision r0p0 of the Cortex-A510 cpu and is fixed in r0p1.
 CPU_FLAG_LIST += ERRATA_A510_1952872
 
+# Flag to apply erratum 1966377 workaround during reset. This erratum applies to
+# revision r0p0 of the Cortex-A510 cpu and is fixed in r0p1.
+CPU_FLAG_LIST += ERRATA_A510_1966377
+
 # Flag to apply erratum 2008766 workaround during reset. This erratum applies
 # to revisions r0p0, r0p1, r0p2, r0p3, r1p0, r1p1, r1p2 and r1p3 of the
 # Cortex-A510 cpu and is still open.
