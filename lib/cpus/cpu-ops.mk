76e5e5ade5a60ce7bf204ed93e001465d727c58f
Xialin Liu <xialin.liu@arm.com>
2026-02-27 10:58:58 -0600

    fix(cpus): workaround for Cortex-A57 erratum 817171
    Cortex-A57 erratum 817171 is a Cat B erratum that applies to revisions
    r0p0, r0p1. It is fixed in r1p0.
    
    Set L2ACTLR_EL1[26] to 1'b1 to disable L2 regional clock gating.
    
    SDEN documentation:
    https://developer.arm.com/documentation/epm049219/latest
    
    Change-Id: I3cf8e70a6abcab8f51098fb22a1d383e123a53d6
    Signed-off-by: Xialin Liu <xialin.liu@arm.com>
    (cherry picked from commit 5c53650378844a6965219ad8fbda2740536a6384)

--- lib/cpus/cpu-ops.mk
+++ lib/cpus/cpu-ops.mk
@@ -171,6 +171,10 @@ CPU_FLAG_LIST += ERRATA_A57_814670
 # applies only to revision <= r0p1 of the Cortex A57 cpu.
 CPU_FLAG_LIST += ERRATA_A57_817169
 
+# Flag to apply erratum 817171 workaround during reset. This erratum applies to
+# revisions r0p0, r0p1 of the Cortex-A57 cpu and is fixed in r1p0.
+CPU_FLAG_LIST += ERRATA_A57_817171
+
 # Flag to apply erratum 826974 workaround during reset. This erratum applies
 # only to revision <= r1p1 of the Cortex A57 cpu.
 CPU_FLAG_LIST += ERRATA_A57_826974
