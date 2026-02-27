59646e58ca2a9c6b6c0dba6273b049917d8a0cf8
Xialin Liu <xialin.liu@arm.com>
2026-02-27 11:34:49 -0600

    fix(cpus): workaround for Cortex-X1 erratum 2376745
    Cortex-X1 erratum 2376745 is a Cat B erratum that applies to revisions
    r0p0, r1p0, r1p1, r1p2. It is still open.
    
    Set CPUACTLR2_EL1[0] to 1 to force PLDW or PRFM PST to behave like PLD
    or PRFM LD and not cause invalidations to other PE caches. There might
    be a small performance degradation to this workaround for certain
    workloads that share data.
    
    SDEN documentation:
    https://developer.arm.com/documentation/SDEN-1401782/latest
    
    Change-Id: Ia2c7855d341a01180c9d0a55472369996e416b8d
    Signed-off-by: Xialin Liu <xialin.liu@arm.com>
    (cherry picked from commit c8d0d08cbbd2b0bcd3d0d5cc6ea91978edcaa5e8)

--- lib/cpus/cpu-ops.mk
+++ lib/cpus/cpu-ops.mk
@@ -645,6 +645,10 @@ CPU_FLAG_LIST += ERRATA_X1_1951500
 # revision r0p0 of the Cortex-X1 cpu and is fixed in r1p0.
 CPU_FLAG_LIST += ERRATA_X1_1952683
 
+# Flag to apply erratum 2376745 workaround during reset. This erratum applies to
+# revisions r0p0, r1p0, r1p1, r1p2 of the Cortex-X1 cpu and is still open.
+CPU_FLAG_LIST += ERRATA_X1_2376745
+
 # Flag to apply erratum 925373 workaround during reset. This erratum applies to
 # revision r0p0 of the Neoverse N1 cpu and is fixed in r1p0.
 CPU_FLAG_LIST += ERRATA_N1_925373
