48b4828aef9a2f142d9ef46ea91b8fb5b48cb027
Xialin Liu <xialin.liu@arm.com>
2026-02-27 09:52:50 -0600

    fix(cpus): workaround for Neoverse N1 erratum 3888013
    Neoverse N1 erratum 3888013 is a Cat B erratum that applies to
    revisions r0p0, r1p0, r2p0, r3p0, r3p1, r4p0, r4p1. It is still open.
    
    The erratum can be avoided by setting CPUACTLR2_EL1[22] to 1 to
    disable linking multiple Non-Cacheable or Device GRE loads to the same
    cache line read request; this may significantly reduce Non-Cacheable
    and Device GRE read bandwidth for streaming workloads.
    
    SDEN documentation:
    https://developer.arm.com/documentation/SDEN-885747/latest
    
    Change-Id: I69ce57376458649f164e88e482a7c59e1edf4fab
    Signed-off-by: Xialin Liu <xialin.liu@arm.com>
    (cherry picked from commit f6b03b69f677825803147b85293735838e9483e9)

--- lib/cpus/cpu-ops.mk
+++ lib/cpus/cpu-ops.mk
@@ -594,6 +594,11 @@ CPU_FLAG_LIST += ERRATA_N1_2743102
 # applies to all revisions <= r4p1 of the Neoverse N1 cpu and is still open.
 CPU_FLAG_LIST += ERRATA_N1_3324349
 
+# Flag to apply erratum 3888013 workaround during reset. This erratum applies to
+# revisions r0p0, r1p0, r2p0, r3p0, r3p1, r4p0, r4p1 of the Neoverse N1 cpu and is
+# still open.
+CPU_FLAG_LIST += ERRATA_N1_3888013
+
 # Flag to apply erratum 1542436 workaround during reset. This erratum applies to
 # revision r0p0 of the Neoverse V1 cpu and is fixed in r1p0.
 CPU_FLAG_LIST += ERRATA_V1_1542436
