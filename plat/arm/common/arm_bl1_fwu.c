822dc56a576c787e1eb07578922ad46173c405b2
Manish V Badarkhe <Manish.Badarkhe@arm.com>
2026-03-05 11:52:18 +0000

    fix(arm): harden FWU address range checks
    Validate FWU ranges before computing end addresses to avoid
    integer overflow and malformed map entries bypassing bounds
    checks, which could lead to out-of-range accesses.
    
    Change-Id: I141752164935157ffa1b1488b1b90bb00fc559f8
    Signed-off-by: Manish V Badarkhe <Manish.Badarkhe@arm.com>
    (cherry picked from commit 08c7f0ede044e42e285f8507e22eacfd429687da)

--- plat/arm/common/arm_bl1_fwu.c
+++ plat/arm/common/arm_bl1_fwu.c
@@ -1,22 +1,32 @@
 /*
- * Copyright (c) 2015-2021, ARM Limited and Contributors. All rights reserved.
+ * Copyright (c) 2015-2026, Arm Limited and Contributors. All rights reserved.
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
 #include <assert.h>
 #include <errno.h>
+#include <inttypes.h>
 
 #include <platform_def.h>
 
 #include <bl1/tbbr/tbbr_img_desc.h>
 #include <common/bl_common.h>
 #include <common/debug.h>
+#include <lib/cassert.h>
 #include <lib/utils.h>
 #include <plat/arm/common/plat_arm.h>
 #include <plat/common/platform.h>
 
 #pragma weak bl1_plat_get_image_desc
+
+/* Compile-time checks for static FWU map entries. */
+CASSERT(!check_uptr_overflow(ARM_SHARED_RAM_BASE, ARM_SHARED_RAM_SIZE),
+	assert_fwu_secure_ram_overflow);
+CASSERT(!check_uptr_overflow(ARM_NS_DRAM1_BASE, ARM_NS_DRAM1_SIZE),
+	assert_fwu_ns_dram1_overflow);
+CASSERT(!check_uptr_overflow(PLAT_ARM_NVM_BASE, PLAT_ARM_NVM_SIZE),
+	assert_fwu_ns_nvm_overflow);
 
 /* Struct to keep track of usable memory */
 typedef struct bl1_mem_info {
@@ -24,7 +34,7 @@ typedef struct bl1_mem_info {
 	unsigned int mem_size;
 } bl1_mem_info_t;
 
-static bl1_mem_info_t fwu_addr_map_secure[] = {
+static const bl1_mem_info_t fwu_addr_map_secure[] = {
 	{
 		.mem_base = ARM_SHARED_RAM_BASE,
 		.mem_size = ARM_SHARED_RAM_SIZE
@@ -34,7 +44,7 @@ static bl1_mem_info_t fwu_addr_map_secure[] = {
 	}
 };
 
-static bl1_mem_info_t fwu_addr_map_non_secure[] = {
+static const bl1_mem_info_t fwu_addr_map_non_secure[] = {
 	{
 		.mem_base = ARM_NS_DRAM1_BASE,
 		.mem_size = ARM_NS_DRAM1_SIZE
@@ -53,31 +63,37 @@ int bl1_plat_mem_check(uintptr_t mem_base,
 		unsigned int flags)
 {
 	unsigned int index = 0;
-	bl1_mem_info_t *mmap;
+	const bl1_mem_info_t *mmap;
+	uintptr_t end;
+	uintptr_t region_end;
 
-	assert(mem_base);
-	assert(mem_size);
 	/*
 	 * The caller of this function is responsible for checking upfront that
-	 * the end address doesn't overflow. We double-check this in debug
-	 * builds.
+	 * the end address doesn't overflow builds. We double check this.
 	 */
-	assert(!check_uptr_overflow(mem_base, mem_size - 1));
+	if ((mem_base == 0U) || (mem_size == 0U) ||
+	    check_uptr_overflow(mem_base, mem_size)) {
+		return -ENOMEM;
+	}
+
+	end = mem_base + mem_size;
 
 	/*
 	 * Check the given image source and size.
 	 */
-	if (GET_SECURITY_STATE(flags) == SECURE)
+	if (GET_SECURITY_STATE(flags) == SECURE) {
 		mmap = fwu_addr_map_secure;
-	else
+	} else {
 		mmap = fwu_addr_map_non_secure;
+	}
 
-	while (mmap[index].mem_size) {
+	while (mmap[index].mem_size > 0U) {
+		region_end = mmap[index].mem_base + mmap[index].mem_size;
+
 		if ((mem_base >= mmap[index].mem_base) &&
-			((mem_base + mem_size)
-			<= (mmap[index].mem_base +
-			mmap[index].mem_size)))
+		    (end <= region_end)) {
 			return 0;
+		}
 
 		index++;
 	}
