524527e1dfdf4aaca330a57e26e53c0e1922a956
Yann Gautier <yann.gautier@st.com>
2026-04-02 15:04:33 +0200

    fix(armada): mv_ddr path may not be a git repo
    TF-A CI uses an archive for Marvell DDR code, and not a git repository.
    Remove the check for the git repository.
    The same change has already been done for a8k [1].
    
    [1]: 40929c29bffd ("fix(a8k): mv_ddr path may not be a git repo")
    
    Change-Id: I7b5c2367d4ad8ddb6a2ff0b20a5206d972e42e4d
    Signed-off-by: Yann Gautier <yann.gautier@st.com>
    (cherry picked from commit 47e2d0bd1aea2abf9384acd6bdaed39b132d7037)

--- plat/marvell/armada/a3k/common/a3700_common.mk
+++ plat/marvell/armada/a3k/common/a3700_common.mk
@@ -164,7 +164,6 @@ $(TIMDDRTOOL): FORCE
 #	Do not remove! Following checks are required to ensure correct TF-A builds, removing these checks leads to broken TF-A builds
 	$(if $(value MV_DDR_PATH),,$(error "Platform '${PLAT}' for ddr tool requires MV_DDR_PATH. Please set MV_DDR_PATH to point to the right directory"))
 	$(if $(wildcard $(value MV_DDR_PATH)/*),,$(error "'MV_DDR_PATH=$(value MV_DDR_PATH)' was specified, but '$(value MV_DDR_PATH)' directory does not exist"))
-	$(if $(shell git -C $(value MV_DDR_PATH) rev-parse --show-cdup 2>&1),$(error "'MV_DDR_PATH=$(value MV_DDR_PATH)' was specified, but '$(value MV_DDR_PATH)' does not contain valid mv-ddr-marvell git repository"))
 	$(q)$(MAKE) --no-print-directory -C $(WTP) MV_DDR_PATH=$(MV_DDR_PATH) DDR_TOPOLOGY=$(DDR_TOPOLOGY) mv_ddr
 
 $(BUILD_PLAT)/$(UART_IMAGE): $(BUILD_PLAT)/$(BOOT_IMAGE) $(BUILD_PLAT)/wtmi.bin $(TBB) $(TIMBUILD) $(TIMDDRTOOL) | $(BUILD_PLAT)/$(BUILD_UART)/ $$(@D)/
