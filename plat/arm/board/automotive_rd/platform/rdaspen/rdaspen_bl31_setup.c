07deb4f76876bb4801ac4ff3e4063a6abcf38b42
Divin Raj <divin.raj@arm.com>
2026-04-14 09:49:39 +0100

    feat(rdaspen): run PFDI OoR on secondaries
    Enable PFDI OoR tests on secondary cores. Power a secondary core off
    from the RD-Aspen post_run hook after its test completes, and panic if
    the CPU_OFF request unexpectedly returns.
    
    Override rdaspen_pwr_domain_on() to prevent normal secondary-core boot
    after an OoR test failure.
    
    Signed-off-by: Ziad Elhanafy <ziad.elhanafy@arm.com>
    Signed-off-by: Divin Raj <divin.raj@arm.com>
    Change-Id: Icfa9150d12c690aaec63e33d28dc13a3c2632c5c

--- plat/arm/board/automotive_rd/platform/rdaspen/rdaspen_bl31_setup.c
+++ plat/arm/board/automotive_rd/platform/rdaspen/rdaspen_bl31_setup.c
@@ -6,10 +6,14 @@
 
 #include <assert.h>
 #include <drivers/arm/css/css_mhu_doorbell.h>
+#include <drivers/arm/css/css_scp.h>
 #include <drivers/arm/css/scmi.h>
 #include <drivers/arm/dsu.h>
+#include <lib/psci/psci.h>
 #include <plat/arm/common/plat_arm.h>
 #include <plat/common/platform.h>
+#include <services/pfdi.h>
+
 #include <rdaspen_ras.h>
 
 static scmi_channel_plat_info_t plat_rd_scmi_info[] = {
@@ -28,8 +32,58 @@ scmi_channel_plat_info_t *plat_css_get_scmi_info(unsigned int channel_id)
 	return &plat_rd_scmi_info[channel_id];
 }
 
+#if PFDI_SUPPORT
+void pfdi_enable(void)
+{
+	int cpu_num;
+	int psci_rc;
+	pfdi_status_t rc;
+
+	cpu_num = plat_my_core_pos();
+	rc = pfdi_pe_oor_test_run();
+	if (rc != 0) {
+		ERROR("PFDI: OoR tests on core %d failed.\n", cpu_num);
+	}
+
+	/* This hook runs only on secondary warm boot. Power the PE back off. */
+	psci_rc = psci_cpu_off();
+	ERROR("PFDI: Failed to power off core %d (%d).\n", cpu_num, psci_rc);
+	panic();
+}
+
+u_register_t plat_pfdi_mpidr_by_core_pos(unsigned int core_pos)
+{
+	unsigned int cluster_id = core_pos / PLAT_MAX_CPUS_PER_CLUSTER;
+	unsigned int core_id = core_pos % PLAT_MAX_CPUS_PER_CLUSTER;
+
+	return ((u_register_t)((core_id & MPIDR_AFFLVL_MASK) << MPIDR_AFF1_SHIFT) |
+	       ((u_register_t)((cluster_id & MPIDR_AFFLVL_MASK) << MPIDR_AFF2_SHIFT)));
+}
+
+static int rdaspen_pwr_domain_on(u_register_t mpidr)
+{
+	uint64_t ft_id;
+	uint64_t cpu_num = plat_core_pos_by_mpidr(mpidr);
+	pfdi_status_t pfdi_status = pfdi_func_desc.result(cpu_num, &ft_id);
+	/*
+	 * The core can only boot if the OoR PFDI tests have not failed
+	 * or if the OoR PFDI tests have not been run yet.
+	 */
+	if ((pfdi_status != 0) && (pfdi_status != PFDI_RET_NOT_RUN)) {
+		return PSCI_E_INTERN_FAIL;
+	}
+
+	css_scp_on(mpidr);
+
+	return PSCI_E_SUCCESS;
+}
+#endif /* PFDI_SUPPORT */
+
 const plat_psci_ops_t *plat_arm_psci_override_pm_ops(plat_psci_ops_t *ops)
 {
+#if PFDI_SUPPORT
+	ops->pwr_domain_on = rdaspen_pwr_domain_on;
+#endif /* PFDI_SUPPORT */
 	ops->pwr_domain_on_finish = rdaspen_css_pwr_domain_on_finish;
 	return css_scmi_override_pm_ops(ops);
 }
