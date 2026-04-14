9b00479dae65d3b12ab481c768548e86efbdd3ed
Harrison Mutai <harrison.mutai@arm.com>
2026-04-14 14:12:05 +0000

    feat(bl31): add GPF-tolerant memcpy helpers
    Introduce EL3-safe memcpy helpers to copy data between EL3 and lower
    exception levels while tolerating Granule Protection Faults (GPFs).
    
    The implementation provides:
    - rme_gpf_safe_read/write for bulk 8-byte aligned transfers
    - GPF trap entries to safely recover and return failure on fault
    
    On fault, the helpers return false and may leave the destination buffer
    partially updated.
    
    Change-Id: I3bb7acefceba98699ade5718892a4b87f9e581cc
    Signed-off-by: Harrison Mutai <harrison.mutai@arm.com>

--- include/lib/gpt_rme/gpc_fault.h
+++ include/lib/gpt_rme/gpc_fault.h
@@ -11,7 +11,16 @@
 #define	GPC_GPF_TRAP_PC_RESUME_OFFSET	U(8)
 #define	GPC_GPF_TRAP_SIZE		U(16)
 
-#ifndef __ASSEMBLER__
+#ifdef __ASSEMBLER__
+#define RME_GPF_TRAP_ENTRY(_fault_pc, _resume_pc)	\
+	.pushsection .el3_gpf_traps, "a", %progbits;	\
+	.balign 8;					\
+	.quad _fault_pc;				\
+	.quad _resume_pc;				\
+	.popsection
+#else
+#include <stddef.h>
+#include <stdint.h>
 
 struct gpc_gpf_trap_entry {
 	/* fault_pc is expected to fault */
@@ -20,5 +29,40 @@ struct gpc_gpf_trap_entry {
 	uintptr_t resume_pc;
 };
 
+/******************************************************************************/
+/* GPF Tolerant APIs                                                          */
+/******************************************************************************/
+
+/**
+ * rme_gpf_safe_read() - Copy bytes from a lower-EL buffer into an EL3 buffer
+ * @dest: EL3 destination buffer
+ * @lower_el_src: Source buffer owned by a lower exception level
+ * @size: Number of bytes to copy
+ *
+ * Attempts to copy @size bytes from @lower_el_src into @dest. If a GPF is
+ * raised while accessing the lower-EL source buffer, the function returns the
+ * number of bytes copied before the fault.
+ *
+ * @dest, @lower_el_src and @size must be 8-byte aligned.
+ *
+ * Return: @size on success, less than @size if the lower-EL access faults
+ */
+size_t rme_gpf_safe_read(void *dest, const void *lower_el_src, size_t size);
+
+/**
+ * rme_gpf_safe_write() - Copy bytes from an EL3 buffer into a lower-EL buffer
+ * @lower_el_dest: Destination buffer owned by a lower exception level
+ * @src: EL3 source buffer
+ * @size: Number of bytes to copy
+ *
+ * Attempts to copy @size bytes from @src into @lower_el_dest. If a GPF is
+ * raised while accessing the lower-EL destination buffer, the function returns
+ * the number of bytes copied before the fault.
+ *
+ * @lower_el_dest, @src and @size must be 8-byte aligned.
+ *
+ * Return: @size on success, less than @size if the lower-EL access faults
+ */
+size_t rme_gpf_safe_write(void *lower_el_dest, const void *src, size_t size);
 #endif /* __ASSEMBLER__ */
 #endif /* GPC_FAULT_H */
