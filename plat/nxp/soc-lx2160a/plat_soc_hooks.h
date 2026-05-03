54cb11a9fd29c0cf762df6236a0d03b46dc8f32b
Vincent Jardin <vjardin@free.fr>
2026-05-03 09:06:17 +0200

    feat(lx2160): add plat_soc_early_init_hook
    The LX2160A boards can add their board-specific initialisation.
    
    The existing LX2160A boards (lx2160ardb, lx2160aqds,
    lx2162aqds) opt in to the no-op default and are functionally
    unaffected.
    
    Test: make CROSS_COMPILE=aarch64-linux-gnu- PLAT=lx2160ardb \
          BOOT_MODE=flexspi_nor bl2 bl31
    
    Change-Id: Ife24969fe52150e7bd2424a1d8462f8d780b3b92
    Signed-off-by: Vincent Jardin <vjardin@free.fr>

---
+++ plat/nxp/soc-lx2160a/plat_soc_hooks.h
@@ -0,0 +1,12 @@
+/*
+ * SPDX-License-Identifier: BSD-3-Clause
+ * Copyright 2026 Free Mobile - Vincent Jardin
+ */
+
+#ifndef PLAT_SOC_HOOKS_H
+#define PLAT_SOC_HOOKS_H
+
+/* Platform extension */
+void plat_soc_early_init_hook(void);
+
+#endif /* PLAT_SOC_HOOKS_H */
---
+++ plat/nxp/soc-lx2160a/plat_soc_hooks_default.c
@@ -0,0 +1,10 @@
+/*
+ * SPDX-License-Identifier: BSD-3-Clause
+ * Copyright 2026 Free Mobile - Vincent Jardin
+ */
+
+#include "plat_soc_hooks.h"
+
+void plat_soc_early_init_hook(void)
+{
+}
