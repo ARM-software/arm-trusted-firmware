02941b288bfbd8a03bf2f247178200d6b151a20d
Boyan Karatotev <boyan.karatotev@arm.com>
2026-05-12 18:24:09 +0100

    fix(aarch32): remove unnecessary feature helpers
    Feature helpers are only needed when they are present in generic code.
    Such features are few and far between and there is no need to keep a
    list of a bunch that noone needs.
    
    Change-Id: Ifc7e844d6572f450fa553825c2b16d9968bb5301
    Signed-off-by: Boyan Karatotev <boyan.karatotev@arm.com>

--- include/arch/aarch32/arch_features.h
+++ include/arch/aarch32/arch_features.h
@@ -63,92 +63,14 @@ CREATE_FEATURE_FUNCS(feat_mtpmu, id_dfr1, ID_DFR1_MTPMU,
  * for AArch32.
  */
 __attribute__((always_inline))
-static inline bool is_feat_twed_supported(void) { return false; }
-__attribute__((always_inline))
-static inline bool is_feat_ecv_supported(void) { return false; }
-__attribute__((always_inline))
-static inline bool is_feat_ecv_v2_supported(void) { return false; }
-__attribute__((always_inline))
-static inline bool is_feat_csv2_2_supported(void) { return false; }
-__attribute__((always_inline))
-static inline bool is_feat_csv2_3_supported(void) { return false; }
-__attribute__((always_inline))
-static inline bool is_feat_ras_supported(void) { return false; }
-
-/* The following features are supported in AArch64 only. */
-__attribute__((always_inline))
-static inline bool is_feat_vhe_supported(void) { return false; }
-__attribute__((always_inline))
-static inline bool is_feat_sel2_supported(void) { return false; }
-__attribute__((always_inline))
-static inline bool is_feat_fgt_supported(void) { return false; }
-__attribute__((always_inline))
-static inline bool is_feat_tcr2_supported(void) { return false; }
-__attribute__((always_inline))
-static inline bool is_feat_spe_supported(void) { return false; }
-__attribute__((always_inline))
-static inline bool is_feat_rng_supported(void) { return false; }
-__attribute__((always_inline))
-static inline bool is_feat_gcs_supported(void) { return false; }
-__attribute__((always_inline))
-static inline bool is_feat_mte2_supported(void) { return false; }
-__attribute__((always_inline))
-static inline bool is_feat_mpam_supported(void) { return false; }
-__attribute__((always_inline))
-static inline bool is_feat_hcx_supported(void) { return false; }
-__attribute__((always_inline))
-static inline bool is_feat_sve_supported(void) { return false; }
-__attribute__((always_inline))
-static inline bool is_feat_brbe_supported(void) { return false; }
-__attribute__((always_inline))
-static inline bool is_feat_trbe_supported(void) { return false; }
-__attribute__((always_inline))
-static inline bool is_feat_nv2_supported(void) { return false; }
-__attribute__((always_inline))
-static inline bool is_feat_sme_supported(void) { return false; }
-__attribute__((always_inline))
-static inline bool is_feat_sme2_supported(void) { return false; }
-__attribute__((always_inline))
-static inline bool is_feat_s2poe_supported(void) { return false; }
-__attribute__((always_inline))
-static inline bool is_feat_s1poe_supported(void) { return false; }
-__attribute__((always_inline))
-static inline bool is_feat_sxpoe_supported(void) { return false; }
-__attribute__((always_inline))
-static inline bool is_feat_s2pie_supported(void) { return false; }
-__attribute__((always_inline))
-static inline bool is_feat_s1pie_supported(void) { return false; }
-__attribute__((always_inline))
-static inline bool is_feat_sxpie_supported(void) { return false; }
-__attribute__((always_inline))
-static inline bool is_feat_uao_present(void) { return false; }
-__attribute__((always_inline))
-static inline bool is_feat_nmi_present(void) { return false; }
-__attribute__((always_inline))
-static inline bool is_feat_ebep_present(void) { return false; }
-__attribute__((always_inline))
-static inline bool is_feat_sebep_present(void) { return false; }
-__attribute__((always_inline))
-static inline bool is_feat_d128_present(void) { return false; }
-__attribute__((always_inline))
-static inline bool is_feat_ls64_accdata_present(void) { return false; }
-__attribute__((always_inline))
-static inline bool is_feat_mops_supported(void) { return false; }
-__attribute__((always_inline))
 static inline bool is_feat_bti_supported(void) { return false; }
 __attribute__((always_inline))
 static inline bool is_feat_pauth_supported(void) { return false; }
-__attribute__((always_inline))
-static inline bool is_feat_lse_supported(void) { return false; }
 __attribute__((always_inline))
 static inline bool is_feat_morello_supported(void) { return false; }
 __attribute__((always_inline))
 static inline bool is_feat_crypto_supported(void) { return false; }
 __attribute__((always_inline))
-static inline bool is_feat_step2_supported(void) { return false; }
-__attribute__((always_inline))
 static inline bool is_feat_rme_supported(void) { return false; }
-__attribute__((always_inline))
-static inline bool is_feat_rme_present(void) { return false; }
 
 #endif /* ARCH_FEATURES_H */
