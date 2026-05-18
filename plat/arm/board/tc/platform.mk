92e4f52e52cbf1fff22a4966bc1a334854ca11ff
Manish Pandey <manish.pandey2@arm.com>
2026-05-18 10:00:20 +0100

    feat(tc): enable workaround for CVE-2026-0995
    Signed-off-by: Manish Pandey <manish.pandey2@arm.com>
    Change-Id: I3712f7d6f7c08a58897091a1e5549fd3fbc864ab
    (cherry picked from commit 0bc52ef2a0588e29865ef06e6a9094a37aedcf2c)

--- plat/arm/board/tc/platform.mk
+++ plat/arm/board/tc/platform.mk
@@ -140,6 +140,11 @@ ifeq (${TARGET_PLATFORM}, 4)
 TC_CPU_SOURCES	+=	lib/cpus/aarch64/c1_pro.S \
 			lib/cpus/aarch64/c1_nano.S \
 			lib/cpus/aarch64/c1_ultra.S
+
+ifeq (${WORKAROUND_CVE_2026_0995},1)
+BL31_SOURCES	+=	lib/cpus/aarch64/c1_pro_pubsub.c \
+			${CPU_SVC_SRCS}
+endif
 endif
 
 INTERCONNECT_SOURCES	:=	${TC_BASE}/tc_interconnect.c \
