5e47ce2aad15ae470f8a6284727a2ce957feaffa
Antal Bánovics <antal.banovics@arm.com>
2026-06-29 16:13:42 +0200

    feat(fvp): enable kernel dt convention
    Enable USE_KERNEL_DT_CONVENTION for the FVP platform to pass the DT blob
    (DTB) in x0 to BL33. This aligns with the Linux boot protocol as
    described in Documentation/arm64/booting.rst.
    
    In addition:
    
    - Clean up legacy ARM_LINUX_KERNEL_AS_BL33 handling since
      USE_KERNEL_DT_CONVENTION now implies this mode for DT handoff.
    - Override args.arg0 for BL33 to point to ARM_PRELOADED_DTB_BASE in
      RESET_TO_BL31.
    - Skip setting the primary MPID in x0 when using this convention.
    
    Change-Id: Ieea8cfe68104b82038b9311613abf13afe7b48f1
    Signed-off-by: Harrison Mutai <harrison.mutai@arm.com>
    Signed-off-by: Antal Bánovics <antal.banovics@arm.com>
    (cherry picked from commit 8946bb03c6c473497a29eba1340e60ca10cb0c6c)

--- plat/arm/common/arm_bl31_setup.c
+++ plat/arm/common/arm_bl31_setup.c
@@ -256,7 +256,7 @@ void __init arm_bl31_early_platform_setup(void *from_bl2, uintptr_t soc_fw_confi
 	bl33_image_ep_info.args.arg1 = 0U;
 	bl33_image_ep_info.args.arg2 = 0U;
 	bl33_image_ep_info.args.arg3 = 0U;
-# endif
+#endif
 }
 
 void bl31_early_platform_setup2(u_register_t arg0, u_register_t arg1,
