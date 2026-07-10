4391742ba5f87f9aa0aabb2265b6e46fb9ba3a32
tomas alvarez vanoli <tomiav97@gmail.com>
2026-07-10 10:12:30 +0200

    fix(nxp): move PLAT_FUSE_FIP_* to plat_default_def
    In all files that include fuse_io.h, platform_def.h is included after
    it.
    
    The comment in fuse_io.h states that you can override the
    PLAT_FUSE_FIP_OFFSET and MAX_SIZE, however this is impossible given the
    include order. The compiler will complain about the macros being defined
    already.
    
    To fix this, move this declaration to plat_default_def.h, which is
    included in the platform defines for all the existing boards, and
    already contains the offset and max size for the fip binary.
    
    Change-Id: I6777e342a439d484ea52ea851beb15afb2dc6a78
    Signed-off-by: tomas alvarez vanoli <tomiav97@gmail.com>
    LTS-candidate: yes
    (cherry picked from commit b919024a966dcfb0a6693444a2382e1453d90b06)

--- plat/nxp/common/fip_handler/fuse_fip/fuse_io.h
+++ plat/nxp/common/fip_handler/fuse_fip/fuse_io.h
@@ -9,15 +9,6 @@
 
 #include <drivers/io/io_driver.h>
 
-/* Can be overridden from platform_def.h file.
- */
-#ifndef PLAT_FUSE_FIP_OFFSET
-#define PLAT_FUSE_FIP_OFFSET	0x880000
-#endif
-#ifndef PLAT_FUSE_FIP_MAX_SIZE
-#define PLAT_FUSE_FIP_MAX_SIZE	0x80000
-#endif
-
 int fip_fuse_provisioning(uintptr_t image_buf, uint32_t size);
 int fuse_fip_setup(const io_dev_connector_t *fip_dev_con, unsigned int boot_dev);
 int plat_get_fuse_image_source(unsigned int image_id,
