3c9e3bc71328bb8cf5438ec7e266e761760b6f32
Michal Simek <michal.simek@amd.com>
2026-07-28 15:02:24 +0200

    feat(versal2): support up to 32 custom SMCs
    Custom SMCs are going to be used in SCMI server where every agent has own SMC that's why increase number of custom SMC to 32.
    
    Signed-off-by: Michal Simek <michal.simek@amd.com>
    Change-Id: I8e85389757d1b2e0195ba62195ac00e188b5d612

--- plat/xilinx/common/include/custom_svc.h
+++ plat/xilinx/common/include/custom_svc.h
@@ -8,6 +8,7 @@
 
 #define SOC_SIP_SVC_CUSTOM   U(0x82002000)
 #define SOC_SIP_SVC64_CUSTOM U(0xC2002000)
+#define SOC_SIP_CUSTOM_SIZE U(32)
 
 uint64_t custom_smc_handler(uint32_t smc_fid, uint64_t x1, uint64_t x2,
 			    uint64_t x3, uint64_t x4, void *cookie,
