10ebfdb8f529f5e1bb9104f5397847ece8b26ea0
Release CI <ci@trustedfirmware.org>
2026-07-31 13:38:36 +0000

    docs(changelog): changelog for lts-v2.12.14 release
    Change-Id: I863637bed4181895da16a24aa990e30d497e1850
    Signed-off-by: Release CI <ci@trustedfirmware.org>

--- docs/conf.py
+++ docs/conf.py
@@ -14,8 +14,8 @@
 
 project = "Trusted Firmware-A"
 author = "Trusted Firmware-A contributors"
-version = "2.12.13"
-release = "2.12.13"
+version = "2.12.14"
+release = "2.12.14"
 
 # -- General configuration ---------------------------------------------------
 
