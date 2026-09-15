TrustedFirmware-A/trusted-firmware-a

refactor(cpus): factor out Neoverse V3AE from the base Neoverse V3 file
The two cores' SDENs have diverged. The only way we have to track the
difference is via separate CPU drivers. So separate them as exact copies
with all the names changed.

Most errata share the same number, so also share the flag.

Change-Id: Ic658273bd4502c28281a7963a8c862c26da644b8
Signed-off-by: Boyan Karatotev <boyan.karatotev@arm.com>

plat/arm/board/fvp/platform.mk
@@ -198,6 +198,7 @@ else
 					lib/cpus/aarch64/neoverse_n3.S		\
 					lib/cpus/aarch64/neoverse_v2.S		\
 					lib/cpus/aarch64/neoverse_v3.S		\
+					lib/cpus/aarch64/neoverse_v3ae.S	\
 					lib/cpus/aarch64/c1_pro.S		\
 					lib/cpus/aarch64/c1_nano.S		\
 					lib/cpus/aarch64/c1_ultra.S		\

ARM-software/arm-trusted-firmware

(cherry picked from commit b1b2165e0c8b992e39214dc79eca4ad5830f3998)

plat/arm/board/fvp/platform.mk
@@ -238,7 +238,8 @@ ifeq (${ENABLE_ERRATA_ALL},1)
 				lib/cpus/aarch64/cortex_x925.S          \
 				lib/cpus/aarch64/neoverse_n3.S          \
 				lib/cpus/aarch64/neoverse_v2.S          \
-				lib/cpus/aarch64/neoverse_v3.S
+				lib/cpus/aarch64/neoverse_v3.S		\
+				lib/cpus/aarch64/neoverse_v3ae.S
 endif
 
 #Build AArch64-only CPUs with no FVP model yet.