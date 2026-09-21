4ms/tf-a-stm32mp25

feat(st): add a helper to get OSPI memory mapped size
This patch adds a SYSCFG helper to get the memory map size
assigned to an OSPI instance.

Change-Id: I47b8b63185f114f50802043d27fc5401e7c74620
Signed-off-by: Christophe Kerello <christophe.kerello@foss.st.com>
Reviewed-on: https://gerrit.st.com/c/mpu/oe/st/tf-a/+/248421
Reviewed-by: CITOOLS <MDG-smet-aci-reviews@list.st.com>
Reviewed-by: Christophe KERELLO <christophe.kerello@st.com>
Reviewed-by: Yann GAUTIER <yann.gautier@foss.st.com>
Tested-by: Christophe KERELLO <christophe.kerello@st.com>

plat/st/stm32mp2/include/stm32mp2_private.h
@@ -14,6 +14,7 @@ void stm32mp2_security_setup(void);
 
 uint32_t stm32mp2_syscfg_get_chip_version(void);
 uint32_t stm32mp2_syscfg_get_chip_dev_id(void);
+size_t stm32mp2_syscfg_get_mm_size(uint8_t bank);
 
 /* Get RISAF platform instance ID from peripheral IO memory base address */
 int stm32_risaf_get_instance(uintptr_t base);

ARM-software/arm-trusted-firmware

plat/st/stm32mp2/include/stm32mp2_private.h
@@ -10,6 +10,7 @@
 void configure_mmu(void);
 
 uint32_t stm32mp_syscfg_get_chip_dev_id(void);
+size_t stm32mp_syscfg_get_mm_size(uint8_t bank);
 
 /* Get RISAF platform instance ID from peripheral IO memory base address */
 int stm32_risaf_get_instance(uintptr_t base);